6179ac513b0e40006e80fda0cdc101064eb9bbb5
haykh <hayk.hakopyan@gmail.com>
2021-05-02 16:19:29 -0400

    input.h/cpp for command line args

--- lib/io/input.h
+++ lib/io/input.h
@@ -1,6 +1,23 @@
 #ifndef IO_INPUT_H
 #define IO_INPUT_H
 
+#include <string_view>
+#include <string>
+#include <vector>
 
+namespace io {
+  class CommandLineArguments {
+  private:
+    bool _initialized = false;
+    std::vector<std::string_view> _args;
+  public:
+    void readCommandLineArguments(int argc, char *argv[]);
+    std::string_view getArgument (std::string_view key, std::string_view def);
+    std::string_view getArgument (std::string_view key);
+    bool isSpecified(std::string_view key);
+  };
+  extern CommandLineArguments cl_args;
+  extern std::string_view InputFile;
+}
 
 #endif
