f6af1a89490bd98d13402c35967cb54fdf9fceb8
haykh <hayk.hakopyan@gmail.com>
2021-05-02 16:18:59 -0400

    generic main

--- ntt/main.cpp
+++ ntt/main.cpp
@@ -1,16 +1,11 @@
 #include "global.h"
 #include "constants.h"
+#include "input.h"
 #include "arrays.h"
 
 #include <iostream>
 #include <cassert>
 
-int main() {
-  ntt::real_t a { 2.0 };
-  std::cout << sizeof(a) << std::endl;
-  std::cout << ntt::sim.title << std::endl;
-  ntt::sim.setTitle("NOTHING");
-  std::cout << ntt::sim.title << std::endl;
-  std::cout << ntt::sim.precision << std::endl;
+int main(int argc, char *argv[]) {
   return 0;
 }
