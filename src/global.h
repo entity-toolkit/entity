3be4816a46156cb377d1296211a711cc679de50b
Hayk Hakobyan <haykh.mfs+github@gmail.com>
2021-08-30 19:28:29 -0400

    added simulation type specifier

--- src/global.h
+++ src/global.h
@@ -13,8 +13,9 @@ using real_t = double;
 
 inline constexpr std::size_t N_GHOSTS{2};
 
+enum SimulationType { PIC_SIM, FORCE_FREE_SIM, MHD_SIM };
 enum Dimension { ONE_D, TWO_D, THREE_D };
-enum CoordinateSystem { CARTESIAN, POLAR, SPHERICAL, LOG_SPHERICAL, CUSTOM };
+enum CoordinateSystem { CARTESIAN_COORD, POLAR_COORD, SPHERICAL_COORD, LOG_SPHERICAL_COORD, CUSTOM_COORD };
 
 enum ParticlePusher { BORIS_PUSHER, VAY_PUSHER };
 
--- src/templates.cpp
+++ src/templates.cpp
@@ -1,6 +1,10 @@
 #include "arrays.h"
 #include "arrays.cpp"
 
+template class ntt::arrays::Array<int>;
+template class ntt::arrays::Array<float>;
+template class ntt::arrays::Array<double>;
+
 template class ntt::arrays::OneDArray<int>;
 template class ntt::arrays::OneDArray<float>;
 template class ntt::arrays::OneDArray<double>;
