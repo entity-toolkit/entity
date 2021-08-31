adf260432dd83309ccc20ef65cad10b671509bdd
Hayk Hakobyan <haykh.mfs+github@gmail.com>
2021-08-31 17:47:23 -0400

    minor

--- src/global.h
+++ src/global.h
@@ -4,6 +4,8 @@
 #include <cstddef>
 #include <string_view>
 
+#define UNUSED(x) (void)(x)
+
 namespace ntt {
 #ifdef SINGLE_PRECISION
 using real_t = float;
@@ -13,11 +15,11 @@ using real_t = double;
 
 inline constexpr std::size_t N_GHOSTS{2};
 
-enum SimulationType { PIC_SIM, FORCE_FREE_SIM, MHD_SIM };
-enum Dimension { ONE_D, TWO_D, THREE_D };
-enum CoordinateSystem { CARTESIAN_COORD, POLAR_COORD, SPHERICAL_COORD, LOG_SPHERICAL_COORD, CUSTOM_COORD };
+enum SimulationType { UNDEFINED_SIM, PIC_SIM, FORCE_FREE_SIM, MHD_SIM };
+enum Dimension { UNDEFINED_D, ONE_D, TWO_D, THREE_D };
+enum CoordinateSystem { UNDEFINED_COORD, CARTESIAN_COORD, POLAR_COORD, SPHERICAL_COORD, LOG_SPHERICAL_COORD, CUSTOM_COORD };
 
-enum ParticlePusher { BORIS_PUSHER, VAY_PUSHER };
+enum ParticlePusher { UNDEFINED_PUSHER, BORIS_PUSHER, VAY_PUSHER };
 
 // defaults
 constexpr std::string_view DEF_input_filename{"input"};
