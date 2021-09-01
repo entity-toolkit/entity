2f43d8abb963aa29095b77579647b6df66269637
Hayk Hakobyan <haykh.mfs+github@gmail.com>
2021-09-01 15:23:10 -0400

    level of abstraction for simulation

--- src/simulation/sim.h
+++ src/simulation/sim.h
@@ -12,7 +12,18 @@
 #include <stdexcept>
 
 namespace ntt {
-class Simulation {
+class AbstractSimulation {
+public:
+  AbstractSimulation() = default;
+  ~AbstractSimulation() = default;
+  virtual void parseInput(int argc, char *argv[]) = 0;
+  virtual void run() = 0;
+  virtual void printDetails(std::ostream&) = 0;
+  virtual void printDetails() = 0;
+};
+
+
+class Simulation : public AbstractSimulation {
 protected:
   std::string m_title;
 
@@ -27,11 +38,9 @@ protected:
   toml::value m_inputdata;
 
   std::vector<int> m_resolution;
-  std::vector<real_t> m_dimensions;
+  std::vector<real_t> m_size;
   real_t m_runtime;
   real_t m_timestep;
-
-  arrays::Array<real_t> *ex1;
 
 public:
   Simulation(Dimension dim, CoordinateSystem coord_sys, SimulationType sim_type);
@@ -42,21 +51,39 @@ public:
   [[nodiscard]] auto get_coord_system() const -> CoordinateSystem { return m_coord_system; }
   [[nodiscard]] auto get_simulation_type() const -> SimulationType { return m_simulation_type; }
 
-  void parseInput(int argc, char *argv[]);
+  void parseInput(int argc, char *argv[]) override;
+  void run() override;
+  void printDetails(std::ostream& os) override;
+  void printDetails() override;
 
   template <typename T> auto readFromInput(const std::string &blockname, const std::string &variable) -> T;
   template <typename T> auto readFromInput(const std::string &blockname, const std::string &variable, const T &defval) -> T;
 
-  void run() {};
-
   void initialize();
 
-  void printDetails(std::ostream& os);
-  void printDetails();
+
   // virtual void restart() = 0;
   // virtual void mainloop() = 0;
   // virtual void finalize() = 0;
 };
+
+class PICSimulation : public Simulation {
+protected:
+  ParticlePusher m_pusher { UNDEFINED_PUSHER };
+public:
+  PICSimulation(Dimension dim, CoordinateSystem coord_sys, ParticlePusher pusher) : Simulation{dim, coord_sys, PIC_SIM}, m_pusher(pusher) {};
+  PICSimulation(Dimension dim, CoordinateSystem coord_sys) : Simulation{dim, coord_sys, PIC_SIM} {};
+  ~PICSimulation() = default;
+  void printDetails(std::ostream& os);
+};
+
+class PICSimulation1D : public PICSimulation {
+public:
+  PICSimulation1D(CoordinateSystem coord_sys, ParticlePusher pusher) : PICSimulation{ONE_D, coord_sys, pusher} {};
+  PICSimulation1D(CoordinateSystem coord_sys) : PICSimulation{ONE_D, coord_sys} {};
+  ~PICSimulation1D() = default;
+};
+
 } // namespace ntt
 
 #endif
