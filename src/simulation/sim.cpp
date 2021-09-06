e7b4000435e9542953d64dfcbb0a5ba8c8f47a8b
haykh <haykh.astro@gmail.com>
2021-09-06 13:17:35 -0400

    minor

--- src/simulation/sim.cpp
+++ src/simulation/sim.cpp
@@ -97,84 +97,17 @@ void Simulation::initialize() {
   assert(m_inputparsed);
   m_initialized = true;
 }
+void Simulation::verify() {
+  assert(is_inputparsed());
+  assert(is_initialized());
+  assert(get_simulation_type() != UNDEFINED_SIM);
+  assert(get_dimension() != UNDEFINED_D);
+  assert(get_coord_system() != UNDEFINED_COORD);
+}
+void Simulation::mainloop() {}
 void Simulation::finalize() {
   assert(m_initialized);
   m_initialized = false;
-}
-void Simulation::mainloop() {
-  assert(m_inputparsed);
-  assert(m_initialized);
-}
-
-void PICSimulation::printDetails(std::ostream &os) {
-  Simulation::printDetails(os);
-  os << "   particle pusher: " << stringifyParticlePusher(m_pusher) << "\n";
-}
-
-void PICSimulation1D::initialize() {
-  ex1.allocate(m_domain.nx1());
-  ex2.allocate(m_domain.nx1());
-  ex3.allocate(m_domain.nx1());
-  bx1.allocate(m_domain.nx1());
-  bx2.allocate(m_domain.nx1());
-  bx3.allocate(m_domain.nx1());
-  Simulation::initialize();
-}
-
-void PICSimulation2D::initialize() {
-  ex1.allocate(m_domain.nx1(), m_domain.nx2());
-  ex2.allocate(m_domain.nx1(), m_domain.nx2());
-  ex3.allocate(m_domain.nx1(), m_domain.nx2());
-  bx1.allocate(m_domain.nx1(), m_domain.nx2());
-  bx2.allocate(m_domain.nx1(), m_domain.nx2());
-  bx3.allocate(m_domain.nx1(), m_domain.nx2());
-  Simulation::initialize();
-}
-
-void PICSimulation3D::initialize() {
-  ex1.allocate(m_domain.nx1(), m_domain.nx2(), m_domain.nx3());
-  ex2.allocate(m_domain.nx1(), m_domain.nx2(), m_domain.nx3());
-  ex3.allocate(m_domain.nx1(), m_domain.nx2(), m_domain.nx3());
-  bx1.allocate(m_domain.nx1(), m_domain.nx2(), m_domain.nx3());
-  bx2.allocate(m_domain.nx1(), m_domain.nx2(), m_domain.nx3());
-  bx3.allocate(m_domain.nx1(), m_domain.nx2(), m_domain.nx3());
-  Simulation::initialize();
-}
-
-// explicitly calling all the destructors
-void PICSimulation1D::finalize() {
-  ex1.arrays::OneDArray<real_t>::~OneDArray<real_t>();
-  ex2.arrays::OneDArray<real_t>::~OneDArray<real_t>();
-  ex3.arrays::OneDArray<real_t>::~OneDArray<real_t>();
-  bx1.arrays::OneDArray<real_t>::~OneDArray<real_t>();
-  bx2.arrays::OneDArray<real_t>::~OneDArray<real_t>();
-  bx3.arrays::OneDArray<real_t>::~OneDArray<real_t>();
-  Simulation::finalize();
-}
-
-void PICSimulation2D::finalize() {
-  ex1.arrays::TwoDArray<real_t>::~TwoDArray<real_t>();
-  ex2.arrays::TwoDArray<real_t>::~TwoDArray<real_t>();
-  ex3.arrays::TwoDArray<real_t>::~TwoDArray<real_t>();
-  bx1.arrays::TwoDArray<real_t>::~TwoDArray<real_t>();
-  bx2.arrays::TwoDArray<real_t>::~TwoDArray<real_t>();
-  bx3.arrays::TwoDArray<real_t>::~TwoDArray<real_t>();
-  Simulation::finalize();
-}
-
-void PICSimulation3D::finalize() {
-  ex1.arrays::ThreeDArray<real_t>::~ThreeDArray<real_t>();
-  ex2.arrays::ThreeDArray<real_t>::~ThreeDArray<real_t>();
-  ex3.arrays::ThreeDArray<real_t>::~ThreeDArray<real_t>();
-  bx1.arrays::ThreeDArray<real_t>::~ThreeDArray<real_t>();
-  bx2.arrays::ThreeDArray<real_t>::~ThreeDArray<real_t>();
-  bx3.arrays::ThreeDArray<real_t>::~ThreeDArray<real_t>();
-  Simulation::finalize();
-}
-
-void PICSimulation::mainloop() {
-  Simulation::mainloop();
-  // ...
 }
 
 } // namespace ntt
