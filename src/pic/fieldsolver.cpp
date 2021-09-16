989600f301691ef76184d1664f2fcfa65481c0b2
Hayk Hakobyan <haykh.mfs+github@gmail.com>
2021-09-16 11:09:22 -0400

    upd pic stuff

--- src/pic/fieldsolver.cpp
+++ src/pic/fieldsolver.cpp
@@ -1,44 +1,44 @@
 #include "global.h"
 #include "picsim.h"
 
-#include <iostream>
+#include <plog/Log.h>
 
 namespace ntt {
 
 void PICSimulation1D::faradayHalfsubstep(const real_t &time) {
-  std::cout << stringifyDimension(m_domain.m_dimension) << " faraday " << time << "\n";
+  PLOGD << time;
 }
 
 void PICSimulation2D::faradayHalfsubstep(const real_t &time) {
-  std::cout << stringifyDimension(m_domain.m_dimension) << " faraday " << time << "\n";
+  PLOGD << time;
 }
 
 void PICSimulation3D::faradayHalfsubstep(const real_t &time) {
-  std::cout << stringifyDimension(m_domain.m_dimension) << " faraday " << time << "\n";
+  PLOGD << time;
 }
 
 void PICSimulation1D::ampereSubstep(const real_t &time) {
-  std::cout << stringifyDimension(m_domain.m_dimension) << " ampere " << time << "\n";
+  PLOGD << time;
 }
 
 void PICSimulation2D::ampereSubstep(const real_t &time) {
-  std::cout << stringifyDimension(m_domain.m_dimension) << " ampere " << time << "\n";
+  PLOGD << time;
 }
 
 void PICSimulation3D::ampereSubstep(const real_t &time) {
-  std::cout << stringifyDimension(m_domain.m_dimension) << " ampere " << time << "\n";
+  PLOGD << time;
 }
 
 void PICSimulation1D::addCurrentsSubstep(const real_t &time) {
-  std::cout << stringifyDimension(m_domain.m_dimension) << " currents " << time << "\n";
+  PLOGD << time;
 }
 
 void PICSimulation2D::addCurrentsSubstep(const real_t &time) {
-  std::cout << stringifyDimension(m_domain.m_dimension) << " currents " << time << "\n";
+  PLOGD << time;
 }
 
 void PICSimulation3D::addCurrentsSubstep(const real_t &time) {
-  std::cout << stringifyDimension(m_domain.m_dimension) << " currents " << time << "\n";
+  PLOGD << time;
 }
 
 
