e86284513c5669597ae986cfdf99e1b95a86cddf
haykh <haykh.astro@gmail.com>
2021-09-28 23:48:05 -0400

    field solver as functor

--- src/pic/fieldsolver.cpp
+++ src/pic/fieldsolver.cpp
@@ -1,7 +1,11 @@
 #include "global.h"
+#include "meshblock.h"
 #include "simulation.h"
+#include "faraday.hpp"
 
 #include <plog/Log.h>
+
+#include <Kokkos_Core.hpp>
 
 namespace ntt {
 
@@ -10,33 +14,27 @@ void Simulation<One_D>::faradayHalfsubstep(const real_t &time) {
   UNUSED(time);
   PLOGD << "1D faraday";
   const real_t testval {-2.0};
-  Meshblock<One_D> &mblock = m_meshblock;
-  Kokkos::parallel_for("faraday",
-    NTTRange(N_GHOSTS, m_sim_params.m_resolution[0] - N_GHOSTS),
-    Lambda (index_t i) {
-      mblock.ex1(i) = testval;
-      // update e,b
-    }
-  );
-  real_t sum {0.0};
-  Kokkos::parallel_reduce("faraday2",
-    NTTRange(N_GHOSTS, m_sim_params.m_resolution[0] - N_GHOSTS),
-    Lambda (index_t i, real_t & s) {
-      s += mblock.ex1(i);
-      // update e,b
-    }, sum
-  );
-  PLOGI << sum;
+  auto range = NTT1DRange({N_GHOSTS}, {m_sim_params.m_resolution[0] - N_GHOSTS});
+  // if cartesian:
+  Kokkos::parallel_for("faraday", range, Faraday1DHalfstep_Cartesian(m_meshblock, testval));
 }
 template <>
 void Simulation<Two_D>::faradayHalfsubstep(const real_t &time) {
   UNUSED(time);
   PLOGD << "2D faraday";
+  const real_t testval {-2.0};
+  auto range = NTT2DRange({N_GHOSTS, N_GHOSTS}, {m_sim_params.m_resolution[0] - N_GHOSTS, m_sim_params.m_resolution[1] - N_GHOSTS});
+  // if cartesian:
+  Kokkos::parallel_for("faraday", range, Faraday2DHalfstep_Cartesian(m_meshblock, testval));
 }
 template <>
 void Simulation<Three_D>::faradayHalfsubstep(const real_t &time) {
   UNUSED(time);
   PLOGD << "3D faraday";
+  const real_t testval {-2.0};
+  auto range = NTT3DRange({N_GHOSTS, N_GHOSTS, N_GHOSTS}, {m_sim_params.m_resolution[0] - N_GHOSTS, m_sim_params.m_resolution[1] - N_GHOSTS, m_sim_params.m_resolution[2] - N_GHOSTS});
+  // if cartesian:
+  Kokkos::parallel_for("faraday", range, Faraday3DHalfstep_Cartesian(m_meshblock, testval));
 }
 
 template <>
