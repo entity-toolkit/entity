9ddca4938271919dff849d2394881530acaac2ca
haykh <haykh.astro@gmail.com>
2021-09-28 23:47:09 -0400

    user init has to be written as a class
    Former-commit-id: bddf84f4618eef89372a4b0e27368073eb8f0cf2
    Former-commit-id: 207e7a0347852a3b91be2ca6bdff331e75fd99ba

--- ntt/pgen/ntt_example.cpp
+++ ntt/pgen/ntt_example.cpp
@@ -9,37 +9,26 @@
 
 namespace ntt {
 
-ProblemGenerator::ProblemGenerator(SimulationParams &sim_params) {
-  UNUSED(sim_params);
-  // auto timestep = readFromInput<real_t>(sim_params.m_inputdata, "algorithm", "timestep");
-  // PLOGD << timestep << "\n";
-}
+// class UserInitFields {
+//   NTTArray<real_t*> ex1, ex2, ex3;
+//   NTTArray<real_t*> bx1, bx2, bx3;
+//   using size_type = NTTArray<real_t*>::size_type;
+//   real_t coeff;
+// public:
+//   Faraday1DHalfstep (const NTTArray<real_t*>& ex1_,
+//                      const NTTArray<real_t*>& ex2_,
+//                      const NTTArray<real_t*>& ex3_,
+//                      const NTTArray<real_t*>& bx1_,
+//                      const NTTArray<real_t*>& bx2_,
+//                      const NTTArray<real_t*>& bx3_,
+//                      const real_t& coeff_) :
+//                      ex1(ex1_), ex2(ex2_), ex3(ex3_),
+//                      bx1(bx1_), bx2(bx2_), bx3(bx3_),
+//                      coeff(coeff_) {}
+//   Inline void operator() (const size_type i) const {
+//     ex1(i) = 0.0;
+//   }
+// };
 
-template <>
-void ProblemGenerator::userInitFields<One_D>(SimulationParams &sim_params, Meshblock<One_D> &mblock) {
-  Kokkos::parallel_for("userInit", sim_params.m_resolution[0],
-    Lambda (index_t i) {
-      mblock.ex1(i) = 1.0 / static_cast<real_t>(i + 1);
-      mblock.bx2(i) = 1.0 / static_cast<real_t>(i + 1);
-    }
-  );
-  real_t sum {0.0};
-  Kokkos::parallel_reduce("checkAll", sim_params.m_resolution[0],
-    Lambda (index_t i, real_t & sum_) {
-      sum_ += mblock.ex1(i);
-    }, sum
-  );
-  PLOGD << sum << " < result";
-}
-
-// TODO: this has to be done better
-template <>
-void ProblemGenerator::userInitFields<Two_D>(SimulationParams &, Meshblock<Two_D>&) {}
-template <>
-void ProblemGenerator::userInitFields<Three_D>(SimulationParams &, Meshblock<Three_D>&) {}
-
-template void ProblemGenerator::userInitFields<One_D>(SimulationParams &, Meshblock<One_D>&);
-template void ProblemGenerator::userInitFields<Two_D>(SimulationParams &, Meshblock<Two_D>&);
-template void ProblemGenerator::userInitFields<Three_D>(SimulationParams &, Meshblock<Three_D>&);
 
 }
