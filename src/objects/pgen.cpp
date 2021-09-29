3ed94abe359c7f4ce9dd2c4b40b8500beb3859b0
haykh <haykh.astro@gmail.com>
2021-09-28 23:47:58 -0400

    minor
    Former-commit-id: 6d28615d7d265134d60fbfbfd3dbe0d149bf41d1
    Former-commit-id: 3fa821e36542d054dfc4d0b81a88721617905ca0

---
+++ src/objects/pgen.cpp
@@ -0,0 +1,40 @@
+#include "global.h"
+#include "pgen.h"
+#include "sim_params.h"
+#include "meshblock.h"
+
+#include "input.h"
+
+#include <plog/Log.h>
+#include <Kokkos_Core.hpp>
+
+namespace ntt {
+
+ProblemGenerator::ProblemGenerator(SimulationParams &sim_params) {
+  UNUSED(sim_params);
+  // auto timestep = readFromInput<real_t>(sim_params.m_inputdata, "algorithm", "timestep");
+  // PLOGD << timestep << "\n";
+}
+
+template <>
+void ProblemGenerator::userInitFields<One_D>(SimulationParams &sim_params, Meshblock<One_D> &mblock) {
+  // Kokkos::parallel_for("userInit", sim_params.m_resolution[0],
+  //   Lambda (index_t i) {
+  //     mblock.ex1(i) = 1.0 / static_cast<real_t>(i + 1);
+  //     mblock.bx2(i) = 1.0 / static_cast<real_t>(i + 1);
+  //   }
+  // );
+}
+
+// TODO: this has to be done better
+template <>
+void ProblemGenerator::userInitFields<Two_D>(SimulationParams &, Meshblock<Two_D>&) {}
+template <>
+void ProblemGenerator::userInitFields<Three_D>(SimulationParams &, Meshblock<Three_D>&) {}
+
+template void ProblemGenerator::userInitFields<One_D>(SimulationParams &, Meshblock<One_D>&);
+template void ProblemGenerator::userInitFields<Two_D>(SimulationParams &, Meshblock<Two_D>&);
+template void ProblemGenerator::userInitFields<Three_D>(SimulationParams &, Meshblock<Three_D>&);
+
+
+}
