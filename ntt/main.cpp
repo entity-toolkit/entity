603330af04f51353119b6cb529b3e4040aa35fcf
haykh <haykh.astro@gmail.com>
2021-09-28 23:47:09 -0400

    user init has to be written as a class

--- ntt/main.cpp
+++ ntt/main.cpp
@@ -1,14 +1,20 @@
 #include "global.h"
+#include "cargs.h"
+#include "input.h"
 #include "simulation.h"
 
-#include <Kokkos_Core.hpp>
+#include <toml/toml.hpp>
 
 #include <plog/Log.h>
 #include <plog/Init.h>
 #include <plog/Formatters/FuncMessageFormatter.h>
 #include <plog/Appenders/ColorConsoleAppender.h>
 
-#include <iostream>
+#include <Kokkos_Core.hpp>
+
+#include <type_traits>
+#include <cassert>
+#include <vector>
 
 using plog_t = plog::ColorConsoleAppender<plog::FuncMessageFormatter>;
 
@@ -28,12 +34,40 @@ auto main(int argc, char *argv[]) -> int {
 
   Kokkos::initialize();
   try {
-    ntt::Simulation<ntt::One_D> sim(argc, argv);
-    sim.initialize();
-    sim.verify();
-    sim.printDetails();
-    sim.mainloop();
-    sim.finalize();
+    ntt::CommandLineArguments cl_args;
+    cl_args.readCommandLineArguments(argc, argv);
+    auto inputfilename = cl_args.getArgument("-input", ntt::DEF_input_filename);
+    auto outputpath = cl_args.getArgument("-output", ntt::DEF_output_path);
+    auto inputdata = toml::parse(static_cast<std::string>(inputfilename));
+    short res = ntt::readFromInput<std::vector<std::size_t>>(inputdata, "domain", "resolution").size();
+    assert((res > 0) && (res < 4));
+
+    // TODO: make this prettier
+    if (res == 1) {
+      ntt::Simulation<ntt::One_D> sim(inputdata);
+      sim.setIO(inputfilename, outputpath);
+      sim.initialize();
+      sim.verify();
+      sim.printDetails();
+      sim.mainloop();
+      sim.finalize();
+    } else if (res == 2) {
+      ntt::Simulation<ntt::Two_D> sim(inputdata);
+      sim.setIO(inputfilename, outputpath);
+      sim.initialize();
+      sim.verify();
+      sim.printDetails();
+      sim.mainloop();
+      sim.finalize();
+    } else {
+      ntt::Simulation<ntt::Three_D> sim(inputdata);
+      sim.setIO(inputfilename, outputpath);
+      sim.initialize();
+      sim.verify();
+      sim.printDetails();
+      sim.mainloop();
+      sim.finalize();
+    }
   } catch (std::exception &err) {
     std::cerr << err.what() << std::endl;
     Kokkos::finalize();
