015172b8ccf81749d790843c0456e7b2d960cc71
haykh <haykh.mfs+github@gmail.com>
2021-10-01 11:47:05 -0400

    collection of timers + assert -> throw
    Former-commit-id: 319f37b656ec0d66c9f1b9ca97599f3c92f3f6aa
    Former-commit-id: 62c56f09644ba22bca57fa662f681b1efc910116

--- src/pic/mainloop.cpp
+++ src/pic/mainloop.cpp
@@ -1,4 +1,5 @@
 #include "global.h"
+#include "timer.h"
 #include "simulation.h"
 
 #include <plog/Log.h>
@@ -7,12 +8,14 @@ namespace ntt {
 
 template <template <typename T> class D> void Simulation<D>::mainloop() {
   PLOGD << "Simulation mainloop started.";
+  TimerCollection timers({"Field_Solver", "Curr_Deposit", "Prtl_Pusher"});
   unsigned long timax{static_cast<unsigned long>(m_sim_params.m_runtime / m_sim_params.m_timestep)};
   real_t time{0.0};
   for (unsigned long ti{0}; ti < timax; ++ti) {
     PLOGD << "t = " << time;
+    timers.start(1);
     faradayHalfsubstep(time);
-
+    timers.stop(1);
     // BC b-fields
 
     // particlePushSubstep(time);
@@ -22,11 +25,14 @@ template <template <typename T> class D> void Simulation<D>::mainloop() {
     // BC particles
     // BC currents
 
+    timers.start(1);
     faradayHalfsubstep(time);
     ampereSubstep(time);
     addCurrentsSubstep(time);
     resetCurrentsSubstep(time);
+    timers.stop(1);
 
+    timers.printAll(millisecond);
     time += m_sim_params.m_timestep;
   }
   PLOGD << "Simulation mainloop finished.";
