aa87e6701361d521e66148ba41aa77d6f56d019d
haykh <haykh.astro@gmail.com>
2021-10-02 14:41:03 -0400

    bug with ranges for bc
    Former-commit-id: 1a6082a3558bb49737ffc99b04bdac397549b8e8
    Former-commit-id: 5aa273c5fd508539fe26a3e18fb7087918ccb008

--- src/pic/boundaries.cpp
+++ src/pic/boundaries.cpp
@@ -16,7 +16,7 @@ void Simulation<One_D>::fieldBoundaryConditions(const real_t& time) {
   if (m_sim_params.m_boundaries[0] == PERIODIC_BC) {
     auto range_m = NTT1DRange({0}, {N_GHOSTS});
     auto range_p
-        = NTT1DRange({m_meshblock.get_imax() + 1}, {m_meshblock.get_imax() + 1 + N_GHOSTS});
+        = NTT1DRange({m_meshblock.get_imax()}, {m_meshblock.get_imax() + N_GHOSTS});
     Kokkos::parallel_for("1d_periodic_bc_x1m", range_m, BC1D_PeriodicX1m(m_meshblock, nx1));
     Kokkos::parallel_for("1d_periodic_bc_x1p", range_p, BC1D_PeriodicX1p(m_meshblock, nx1));
   } else {
@@ -30,8 +30,8 @@ void Simulation<Two_D>::fieldBoundaryConditions(const real_t& time) {
   auto nx1 = m_meshblock.get_n1();
   if (m_sim_params.m_boundaries[0] == PERIODIC_BC) {
     auto range_m = NTT2DRange({0, m_meshblock.get_jmin()}, {N_GHOSTS, m_meshblock.get_jmax()});
-    auto range_p = NTT2DRange({m_meshblock.get_imax() + 1, m_meshblock.get_jmin()},
-                              {m_meshblock.get_imax() + 1 + N_GHOSTS, m_meshblock.get_jmax()});
+    auto range_p = NTT2DRange({m_meshblock.get_imax(), m_meshblock.get_jmin()},
+                              {m_meshblock.get_imax() + N_GHOSTS, m_meshblock.get_jmax()});
     Kokkos::parallel_for("2d_periodic_bc_x1m", range_m, BC2D_PeriodicX1m(m_meshblock, nx1));
     Kokkos::parallel_for("2d_periodic_bc_x1p", range_p, BC2D_PeriodicX1p(m_meshblock, nx1));
   } else {
@@ -42,8 +42,8 @@ void Simulation<Two_D>::fieldBoundaryConditions(const real_t& time) {
   if (m_sim_params.m_boundaries[1] == PERIODIC_BC) {
     auto range_m = NTT2DRange({0, 0}, {m_meshblock.get_imax() + N_GHOSTS, N_GHOSTS});
     auto range_p
-        = NTT2DRange({0, m_meshblock.get_jmax() + 1},
-                     {m_meshblock.get_imax() + N_GHOSTS, m_meshblock.get_jmax() + 1 + N_GHOSTS});
+        = NTT2DRange({0, m_meshblock.get_jmax()},
+                     {m_meshblock.get_imax() + N_GHOSTS, m_meshblock.get_jmax() + N_GHOSTS});
     Kokkos::parallel_for("2d_periodic_bc_x2m", range_m, BC2D_PeriodicX2m(m_meshblock, nx2));
     Kokkos::parallel_for("2d_periodic_bc_x2p", range_p, BC2D_PeriodicX2p(m_meshblock, nx2));
   } else {
