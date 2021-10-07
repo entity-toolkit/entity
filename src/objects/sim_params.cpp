989edb55bf6f24ad0c1defabd1ae681232e5219a
haykh <haykh.astro@gmail.com>
2021-10-07 18:58:47 -0400

    added particle templates (fixed a few bugs)
    Former-commit-id: e90bf11768db9e889c654b73c443c68e78791f59
    Former-commit-id: 0c2720ee847ca7472755850436f863e20da2040c

--- src/objects/sim_params.cpp
+++ src/objects/sim_params.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 
 namespace ntt {
+
 SimulationParams::SimulationParams(const toml::value& inputdata, Dimension dim) {
   m_inputdata = inputdata;
 
@@ -37,7 +38,7 @@ SimulationParams::SimulationParams(const toml::value& inputdata, Dimension dim) 
     } else if (pusher_str == "Boris") {
       pusher = BORIS_PUSHER;
     }
-    // m_species.emplace_back(ParticleSpecies(label, mass, charge, maxnpart, pusher));
+    m_species.emplace_back(ParticleSpecies(label, mass, charge, maxnpart, pusher));
   }
 
   // TODO: for now only PIC
@@ -106,9 +107,85 @@ SimulationParams::SimulationParams(const toml::value& inputdata, Dimension dim) 
   // real_t maxtstep {}
   auto cfl = readFromInput<real_t>(m_inputdata, "algorithm", "CFL", 0.45);
   assert(cfl > 0);
-  // TODO: other coord systems
-  auto cell_size = (m_extent[1] - m_extent[0]) / static_cast<real_t>(m_resolution[0]);
-  m_timestep = cell_size * cfl;
+  m_timestep = get_cell_size() * cfl;
+}
+
+auto SimulationParams::get_cell_size() -> real_t {
+  // TODO: modify this
+  if (m_coord_system == CARTESIAN_COORD) {
+    return (m_extent[1] - m_extent[0]) / static_cast<real_t>(m_resolution[0]);
+  } else {
+    throw std::logic_error("ERROR: Cannot determine the cell size.");
+  }
+}
+
+void SimulationParams::verify() {
+  if (m_simtype == UNDEFINED_SIM) {
+    throw std::logic_error("ERROR: simulation type unspecified.");
+  }
+  if (m_coord_system == UNDEFINED_COORD) {
+    throw std::logic_error("ERROR: coordinate system unspecified.");
+  }
+  for (auto& b : m_boundaries) {
+    if (b == UNDEFINED_BC) { throw std::logic_error("ERROR: boundary conditions unspecified."); }
+  }
+  // TODO: maybe some other tests
+}
+
+void SimulationParams::printDetails() {
+  PLOGI << "[Simulation details]";
+  PLOGI << "   title: " << m_title;
+  PLOGI << "   type: " << stringifySimulationType(m_simtype);
+  PLOGI << "   total runtime: " << m_runtime;
+  PLOGI << "   dt: " << m_timestep << " ["
+        << static_cast<int>(m_runtime / m_timestep) << " steps]";
+
+  auto dim = static_cast<short>(m_resolution.size());
+  PLOGI << "[domain]";
+  PLOGI << "   dimension: " << dim << "D";
+  PLOGI << "   coordinate system: "
+        << stringifyCoordinateSystem(m_coord_system, dim);
+
+  std::string bc{"   boundary conditions: { "};
+  for (auto& b : m_boundaries) {
+    bc += stringifyBoundaryCondition(b) + " x ";
+  }
+  bc.erase(bc.size() - 3);
+  bc += " }";
+  PLOGI << bc;
+
+  std::string res{"   resolution: { "};
+  for (auto& r : m_resolution) {
+    res += std::to_string(r) + " x ";
+  }
+  res.erase(res.size() - 3);
+  res += " }";
+  PLOGI << res;
+
+  std::string ext{"   extent: "};
+  for (std::size_t i{0}; i < m_extent.size(); i += 2) {
+    ext += "{" + std::to_string(m_extent[i]) + ", "
+         + std::to_string(m_extent[i + 1]) + "} ";
+  }
+  PLOGI << ext;
+
+  std::string cell{"   cell size: "};
+  real_t effective_dx{0.0};
+  if (m_coord_system == CARTESIAN_COORD) {
+    cell += "{" + std::to_string(get_cell_size()) + "}";
+    effective_dx = get_cell_size();
+  }
+  PLOGI << cell;
+
+  PLOGI << "[fiducial parameters]";
+  PLOGI << "   ppc0: " << m_ppc0;
+  PLOGI << "   rho0: " << m_larmor0 << " [" << m_larmor0 / effective_dx
+        << " dx]";
+  PLOGI << "   c_omp0: " << m_skindepth0 << " ["
+        << m_skindepth0 / effective_dx << " dx]";
+  PLOGI << "   sigma0: " << m_sigma0;
+  PLOGI << "   q0: " << m_charge0;
+  PLOGI << "   B0: " << m_B0;
 }
 
 } // namespace ntt
