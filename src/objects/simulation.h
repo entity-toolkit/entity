8da1d754cc79da699abd83c6d346be2a94360d27
haykh <haykh.mfs+github@gmail.com>
2021-10-07 01:22:39 -0400

    update
    Former-commit-id: 9529e01dc869584eba17be8415e0a5a133dacfe3
    Former-commit-id: 3a110decf8bdbf8c462955977df5d4b8399915d0

--- src/objects/simulation.h
+++ src/objects/simulation.h
@@ -12,6 +12,7 @@
 
 namespace ntt {
 
+// TODO!: there has to be a better way (e.g. `using`, or `typedef`)
 template <template <typename T = std::nullptr_t> class D>
 class Simulation {
   D<> m_dim;
