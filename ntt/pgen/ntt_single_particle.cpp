18b12820cd92f5e30c50609f125a0c9e17431f74
haykh <haykh.mfs+github@gmail.com>
2021-10-10 21:31:50 -0400

    automatically mapping particle array pointers for plotting
    Former-commit-id: e76dba9e0b8a162272028fcf5dcb1f0bbcf05a6c
    Former-commit-id: b6a313ff7bac3b3f4811063ce230648dd81b6c8d

--- ntt/pgen/ntt_single_particle.cpp
+++ ntt/pgen/ntt_single_particle.cpp
@@ -44,12 +44,19 @@ void ProblemGenerator<TWO_D>::userInitParticles(SimulationParams& sim_params,
       mblock.particles[0].m_x2(p) = -1.4;
       mblock.particles[0].m_ux1(p) = 1.0;
       // positron
-      mblock.particles[1].m_x1(p) = -1.4;
-      mblock.particles[1].m_x2(p) = -1.4;
-      mblock.particles[1].m_ux1(p) = 1.0;
+      mblock.particles[1].m_x1(p) = -0.2;
+      mblock.particles[1].m_x2(p) = 0.3;
+      mblock.particles[1].m_ux1(p) = -0.5;
+      mblock.particles[1].m_ux2(p) = 0.5;
+      // ion
+      mblock.particles[2].m_x1(p) = 0.6;
+      mblock.particles[2].m_x2(p) = 1.5;
+      mblock.particles[2].m_ux1(p) = 2.0;
+      mblock.particles[2].m_ux2(p) = 2.0;
   });
   mblock.particles[0].m_npart = 1;
   mblock.particles[1].m_npart = 1;
+  mblock.particles[2].m_npart = 1;
 
 }
 
