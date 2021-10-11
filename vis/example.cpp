f11959fe18c70aa1d9f2bee5c77c29e8caf16617
haykh <haykh.mfs+github@gmail.com>
2021-10-10 21:31:50 -0400

    automatically mapping particle array pointers for plotting
    Former-commit-id: ed62a8a23682b85c3625284b6a6a9caba96b22bf
    Former-commit-id: 19806607787b59f1890dbae0931791bd0f12b996

--- vis/example.cpp
+++ vis/example.cpp
@@ -26,10 +26,12 @@ public:
 
   ntt::real_t m_time;
 
-  nttiny::Data<float> electrons_x;
-  nttiny::Data<float> electrons_y;
-  nttiny::Data<float> positrons_x;
-  nttiny::Data<float> positrons_y;
+  std::vector<std::unique_ptr<nttiny::Data<float>>> prtl_pointers;
+
+  // nttiny::Data<float> electrons_x;
+  // nttiny::Data<float> electrons_y;
+  // nttiny::Data<float> positrons_x;
+  // nttiny::Data<float> positrons_y;
 
   NTTSimulationVis(ntt::Simulation2D& sim)
     : nttiny::SimulationAPI<float>{0, 0}, m_sim(sim) {
@@ -56,26 +58,21 @@ public:
       f.second->set_dimension(2);
     }
 
-    particles.insert({{"electrons",
-                      {&(this->electrons_x),
-                       &(this->electrons_y)}
-                     },{
-                       "positrons",
-                       {&(this->positrons_x),
-                        &(this->positrons_y)}
-                     }});
-    // particle data is simply pointing at raw pointers of Kokkos arrays
-    // ... (will not work if data is on GPU, use mirror?)
-    particles["electrons"].first->m_data = m_sim.get_meshblock().particles[0].m_x1.data();
-    particles["electrons"].second->m_data = m_sim.get_meshblock().particles[0].m_x2.data();
-    particles["positrons"].first->m_data = m_sim.get_meshblock().particles[1].m_x1.data();
-    particles["positrons"].second->m_data = m_sim.get_meshblock().particles[1].m_x2.data();
-
-    particles["electrons"].first->set_size(0, m_sim.get_meshblock().particles[0].get_npart());
-    particles["electrons"].second->set_size(0, m_sim.get_meshblock().particles[0].get_npart());
-    particles["positrons"].first->set_size(0, m_sim.get_meshblock().particles[1].get_npart());
-    particles["positrons"].second->set_size(0, m_sim.get_meshblock().particles[1].get_npart());
-
+    int i{0};
+    for (auto& species : m_sim.get_meshblock().particles) {
+      auto x_prtl {std::make_unique<nttiny::Data<float>>()};
+      auto y_prtl {std::make_unique<nttiny::Data<float>>()};
+      x_prtl->m_data = m_sim.get_meshblock().particles[i / 2].m_x1.data();
+      y_prtl->m_data = m_sim.get_meshblock().particles[i / 2].m_x2.data();
+      x_prtl->set_size(0, m_sim.get_meshblock().particles[i / 2].get_npart());
+      y_prtl->set_size(0, m_sim.get_meshblock().particles[i / 2].get_npart());
+      this->prtl_pointers.push_back(std::move(x_prtl));
+      this->prtl_pointers.push_back(std::move(y_prtl));
+      this->particles.insert({species.m_label,
+                              {(this->prtl_pointers[i].get()),
+                               (this->prtl_pointers[i + 1].get())}});
+      i += 2;
+    }
     setData();
   }
   ~NTTSimulationVis() = default;
