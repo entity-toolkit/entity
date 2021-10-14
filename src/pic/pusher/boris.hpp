514a2041d33edf851ad06af52297ff0cca86cacd
haykh <haykh.mfs+github@gmail.com>
2021-10-14 16:50:47 -0400

    refactored mover

--- src/pic/pusher/boris.hpp
+++ src/pic/pusher/boris.hpp
@@ -9,10 +9,11 @@
 
 namespace ntt {
 
+// this depends on coord system
+
 template <Dimension D>
 class Boris : public Pusher<D> {
-  using index_t = NTTArray<real_t*>::size_type;
-
+  using index_t = typename Pusher<D>::index_t;
 public:
   Boris(
       const Meshblock<D>& m_meshblock_,
@@ -21,8 +22,6 @@ public:
       const real_t& dt_)
       : Pusher<D> {m_meshblock_, m_particles_, coeff_, dt_} {}
 
-  Inline void operator()(const index_t p) const;
-
   void velocityUpdate(
       const index_t& p,
       real_t& e0_x1,
@@ -30,7 +29,7 @@ public:
       real_t& e0_x3,
       real_t& b0_x1,
       real_t& b0_x2,
-      real_t& b0_x3) const {
+      real_t& b0_x3) const override {
     real_t COEFF {this->coeff};
 
     e0_x1 *= COEFF;
@@ -60,67 +59,6 @@ public:
     this->m_particles.m_ux3(p) = u0_x3;
   }
 };
-
-template <>
-Inline void Boris<ONE_D>::operator()(const index_t p) const {
-  // TESTPERF: this routine vs explicit call
-  auto [i, dx1] = convert_x1TOidx1(m_meshblock, m_particles.m_x1(p));
-  // dummy fields (no interp)
-  real_t e0_x1 {m_meshblock.ex1(i)}, e0_x2 {m_meshblock.ex2(i)}, e0_x3 {m_meshblock.ex3(i)};
-  real_t b0_x1 {m_meshblock.bx1(i)}, b0_x2 {m_meshblock.bx2(i)}, b0_x3 {m_meshblock.bx3(i)};
-  // TESTPERF: this vs explicit call
-  velocityUpdate(p, e0_x1, e0_x2, e0_x3, b0_x1, b0_x2, b0_x3);
-  // TESTPERF: faster sqrt?
-  real_t inv_gamma0 {
-      static_cast<real_t>(1.0)
-      / std::sqrt(
-          static_cast<real_t>(1.0) + m_particles.m_ux1(p) * m_particles.m_ux1(p)
-          + m_particles.m_ux2(p) * m_particles.m_ux2(p)
-          + m_particles.m_ux3(p) * m_particles.m_ux3(p))};
-  m_particles.m_x1(p) += dt * m_particles.m_ux1(p) * inv_gamma0;
-}
-
-template <>
-Inline void Boris<TWO_D>::operator()(const index_t p) const {
-  auto [i, dx1] = convert_x1TOidx1(m_meshblock, m_particles.m_x1(p));
-  auto [j, dx2] = convert_x2TOjdx2(m_meshblock, m_particles.m_x2(p));
-  // dummy fields (no interp)
-  real_t e0_x1 {m_meshblock.ex1(i, j)}, e0_x2 {m_meshblock.ex2(i, j)},
-      e0_x3 {m_meshblock.ex3(i, j)};
-  real_t b0_x1 {m_meshblock.bx1(i, j)}, b0_x2 {m_meshblock.bx2(i, j)},
-      b0_x3 {m_meshblock.bx3(i, j)};
-  velocityUpdate(p, e0_x1, e0_x2, e0_x3, b0_x1, b0_x2, b0_x3);
-  real_t inv_gamma0 {
-      static_cast<real_t>(1.0)
-      / std::sqrt(
-          static_cast<real_t>(1.0) + m_particles.m_ux1(p) * m_particles.m_ux1(p)
-          + m_particles.m_ux2(p) * m_particles.m_ux2(p)
-          + m_particles.m_ux3(p) * m_particles.m_ux3(p))};
-  m_particles.m_x1(p) += dt * m_particles.m_ux1(p) * inv_gamma0;
-  m_particles.m_x2(p) += dt * m_particles.m_ux2(p) * inv_gamma0;
-}
-
-template <>
-Inline void Boris<THREE_D>::operator()(const index_t p) const {
-  auto [i, dx1] = convert_x1TOidx1(m_meshblock, m_particles.m_x1(p));
-  auto [j, dx2] = convert_x2TOjdx2(m_meshblock, m_particles.m_x2(p));
-  auto [k, dx3] = convert_x3TOkdx3(m_meshblock, m_particles.m_x3(p));
-  // dummy fields (no interp)
-  real_t e0_x1 {m_meshblock.ex1(i, j, k)}, e0_x2 {m_meshblock.ex2(i, j, k)},
-      e0_x3 {m_meshblock.ex3(i, j, k)};
-  real_t b0_x1 {m_meshblock.bx1(i, j, k)}, b0_x2 {m_meshblock.bx2(i, j, k)},
-      b0_x3 {m_meshblock.bx3(i, j, k)};
-  velocityUpdate(p, e0_x1, e0_x2, e0_x3, b0_x1, b0_x2, b0_x3);
-  real_t inv_gamma0 {
-      static_cast<real_t>(1.0)
-      / std::sqrt(
-          static_cast<real_t>(1.0) + m_particles.m_ux1(p) * m_particles.m_ux1(p)
-          + m_particles.m_ux2(p) * m_particles.m_ux2(p)
-          + m_particles.m_ux3(p) * m_particles.m_ux3(p))};
-  m_particles.m_x1(p) += dt * m_particles.m_ux1(p) * inv_gamma0;
-  m_particles.m_x2(p) += dt * m_particles.m_ux2(p) * inv_gamma0;
-  m_particles.m_x3(p) += dt * m_particles.m_ux3(p) * inv_gamma0;
-}
 
 } // namespace ntt
 
