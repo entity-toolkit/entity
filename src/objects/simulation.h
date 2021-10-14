8da4adbfbe1635b17043a3fee4858f13075b187b
haykh <haykh.mfs+github@gmail.com>
2021-10-14 14:46:27 -0400

    template simulation class
    Former-commit-id: f8984b40819051bebf33cdbd8ef9a610e1cc5345
    Former-commit-id: 25bca96826d05185dc42036117dbc5c2f8db461a

--- src/objects/simulation.h
+++ src/objects/simulation.h
@@ -36,64 +36,19 @@ public:
   void run(std::string_view, std::string_view);
 
   // fields
-  virtual void faradayHalfsubstep(const real_t&) {}
-  virtual void ampereSubstep(const real_t&) {}
-  virtual void addCurrentsSubstep(const real_t&) {}
-  virtual void resetCurrentsSubstep(const real_t&) {}
+  void faradayHalfsubstep(const real_t&);
+  void ampereSubstep(const real_t&);
+  void addCurrentsSubstep(const real_t&);
+  void resetCurrentsSubstep(const real_t&);
   // particles
-  virtual void pushParticlesSubstep(const real_t&) {}
-  virtual void depositSubstep(const real_t&) {}
+  void pushParticlesSubstep(const real_t&);
+  void depositSubstep(const real_t&);
   // boundaries
-  virtual void fieldBoundaryConditions(const real_t&) {}
-  virtual void particleBoundaryConditions(const real_t&) {}
+  void fieldBoundaryConditions(const real_t&);
+  void particleBoundaryConditions(const real_t&);
 
   [[nodiscard]] auto get_params() const -> const SimulationParams& { return m_sim_params; }
   [[nodiscard]] auto get_meshblock() const -> const Meshblock<D>& { return m_meshblock; }
-};
-
-struct Simulation1D : public Simulation<ONE_D> {
-  Simulation1D(const toml::value& inputdata) : Simulation<ONE_D> {inputdata} {}
-
-  void faradayHalfsubstep(const real_t& time) override;
-  void ampereSubstep(const real_t& time) override;
-  void addCurrentsSubstep(const real_t& time) override;
-  void resetCurrentsSubstep(const real_t& time) override;
-
-  void pushParticlesSubstep(const real_t&) override;
-  void depositSubstep(const real_t& time) override;
-
-  void fieldBoundaryConditions(const real_t& time) override;
-  void particleBoundaryConditions(const real_t& time) override;
-};
-
-struct Simulation2D : public Simulation<TWO_D> {
-  Simulation2D(const toml::value& inputdata) : Simulation<TWO_D> {inputdata} {}
-
-  void faradayHalfsubstep(const real_t& time) override;
-  void ampereSubstep(const real_t& time) override;
-  void addCurrentsSubstep(const real_t& time) override;
-  void resetCurrentsSubstep(const real_t& time) override;
-
-  void pushParticlesSubstep(const real_t&) override;
-  void depositSubstep(const real_t& time) override;
-
-  void fieldBoundaryConditions(const real_t& time) override;
-  void particleBoundaryConditions(const real_t& time) override;
-};
-
-struct Simulation3D : public Simulation<THREE_D> {
-  Simulation3D(const toml::value& inputdata) : Simulation<THREE_D> {inputdata} {}
-
-  void faradayHalfsubstep(const real_t& time) override;
-  void ampereSubstep(const real_t& time) override;
-  void addCurrentsSubstep(const real_t& time) override;
-  void resetCurrentsSubstep(const real_t& time) override;
-
-  void pushParticlesSubstep(const real_t&) override;
-  void depositSubstep(const real_t& time) override;
-
-  void fieldBoundaryConditions(const real_t& time) override;
-  void particleBoundaryConditions(const real_t& time) override;
 };
 
 } // namespace ntt
