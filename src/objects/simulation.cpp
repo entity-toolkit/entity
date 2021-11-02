7eb9a94ceda93881a802084e63f34c4d2e3c76bd
Hayk Hakobyan <haykh.mfs+github@gmail.com>
2021-11-02 17:04:36 -0400

    cfl condition (for xyz) fixed
    Former-commit-id: 995b2c881a5f4940d610fd4773dd99fd921a4df2
    Former-commit-id: 38882b50091a8de10184233421f960f159cbcc81

--- src/objects/simulation.cpp
+++ src/objects/simulation.cpp
@@ -7,6 +7,7 @@
 #include <plog/Log.h>
 #include <toml/toml.hpp>
 
+#include <cmath>
 #include <stdexcept>
 
 namespace ntt {
@@ -18,6 +19,24 @@ Simulation<D>::Simulation(const toml::value& inputdata)
       m_meshblock {m_sim_params.m_resolution, m_sim_params.m_species} {
   m_meshblock.set_extent(m_sim_params.m_extent);
   m_meshblock.set_coord_system(m_sim_params.m_coord_system);
+
+  // find timestep and effective cell size
+  if (m_sim_params.m_coord_system == CARTESIAN_COORD) {
+    if (m_dim == ONE_D) {
+      real_t dx1 = m_meshblock.get_dx1();
+      m_sim_params.m_min_cell_size = dx1;
+    } else if (m_dim == TWO_D) {
+      real_t dx1 = m_meshblock.get_dx1();
+      real_t dx2 = m_meshblock.get_dx2();
+      m_sim_params.m_min_cell_size = 1.0 / std::sqrt(1.0 / (dx1 * dx1) + 1.0 / (dx2 * dx2));
+    } else if (m_dim == TWO_D) {
+      real_t dx1 = m_meshblock.get_dx1();
+      real_t dx2 = m_meshblock.get_dx2();
+      real_t dx3 = m_meshblock.get_dx3();
+      m_sim_params.m_min_cell_size = 1.0 / std::sqrt(1.0 / (dx1 * dx1) + 1.0 / (dx2 * dx2) + 1.0 / (dx3 * dx3));
+    }
+  }
+  m_sim_params.m_timestep = m_sim_params.m_cfl * m_sim_params.m_min_cell_size;
 }
 
 template <Dimension D>
@@ -43,8 +62,71 @@ void Simulation<D>::verify() {
 
 template <Dimension D>
 void Simulation<D>::printDetails() {
-  m_sim_params.printDetails();
-  m_meshblock.printDetails();
+  PLOGI << "[Simulation details]";
+  PLOGI << "   title: " << m_sim_params.m_title;
+  PLOGI << "   type: " << stringifySimulationType(m_sim_params.m_simtype);
+  PLOGI << "   total runtime: " << m_sim_params.m_runtime;
+  PLOGI << "   dt: " << m_sim_params.m_timestep << " [" << static_cast<int>(m_sim_params.m_runtime / m_sim_params.m_timestep) << " steps]";
+
+  PLOGI << "[domain]";
+  PLOGI << "   dimension: " << m_dim << "D";
+  PLOGI << "   coordinate system: " << stringifyCoordinateSystem(m_sim_params.m_coord_system, m_dim);
+
+  std::string bc {"   boundary conditions: { "};
+  for (auto& b : m_sim_params.m_boundaries) {
+    bc += stringifyBoundaryCondition(b) + " x ";
+  }
+  bc.erase(bc.size() - 3);
+  bc += " }";
+  PLOGI << bc;
+
+  std::string res {"   resolution: { "};
+  for (auto& r : m_sim_params.m_resolution) {
+    res += std::to_string(r) + " x ";
+  }
+  res.erase(res.size() - 3);
+  res += " }";
+  PLOGI << res;
+
+  std::string ext {"   extent: "};
+  for (std::size_t i {0}; i < m_sim_params.m_extent.size(); i += 2) {
+    ext += "{" + std::to_string(m_sim_params.m_extent[i]) + ", " + std::to_string(m_sim_params.m_extent[i + 1]) + "} ";
+  }
+  PLOGI << ext;
+
+  std::string cell {"   cell size: "};
+  cell += "{" + std::to_string(m_meshblock.get_dx1());
+  if ((m_dim == TWO_D) || (m_dim == THREE_D)) {
+    cell += ", " + std::to_string(m_meshblock.get_dx2());
+  }
+  if (m_dim == THREE_D) {
+    cell += ", " + std::to_string(m_meshblock.get_dx3());
+  }
+  cell += "}";
+  PLOGI << cell;
+
+  PLOGI << "[fiducial parameters]";
+  PLOGI << "   ppc0: " << m_sim_params.m_ppc0;
+  PLOGI << "   rho0: " << m_sim_params.m_larmor0 << " [" << m_sim_params.m_larmor0 / m_sim_params.m_min_cell_size << " d_min]";
+  PLOGI << "   c_omp0: " << m_sim_params.m_skindepth0 << " [" << m_sim_params.m_skindepth0 / m_sim_params.m_min_cell_size << " d_min]";
+  PLOGI << "   sigma0: " << m_sim_params.m_sigma0;
+  PLOGI << "   q0: " << m_sim_params.m_charge0;
+  PLOGI << "   B0: " << m_sim_params.m_B0;
+
+  if (m_meshblock.particles.size() > 0) {
+    PLOGI << "[particles]";
+    for (std::size_t i {0}; i < m_meshblock.particles.size(); ++i) {
+      PLOGI << "   [species #" << i + 1 << "]";
+      PLOGI << "      label: " << m_meshblock.particles[i].get_label();
+      PLOGI << "      mass: " << m_meshblock.particles[i].get_mass();
+      PLOGI << "      charge: " << m_meshblock.particles[i].get_charge();
+      PLOGI << "      pusher: " << stringifyParticlePusher(m_meshblock.particles[i].get_pusher());
+      PLOGI << "      maxnpart: " << m_meshblock.particles[i].get_maxnpart() << " (" << m_meshblock.particles[i].get_npart()
+            << ")";
+    }
+  } else {
+    PLOGI << "[no particles]";
+  }
 }
 
 template <Dimension D>
