c94bba0141a08567f1e53e535695bbccdd9ccc85
haykh <haykh.mfs+github@gmail.com>
2021-11-10 10:06:40 -0500

    curvilinear added + restruct meshblock
    Former-commit-id: 4aa7142a54b3b8eece777a57e6f2bb7cd60f7ad7
    Former-commit-id: 9d28f175009c729311d574b763060f0f0751846d

---
+++ src/objects/fields.cpp
@@ -0,0 +1,26 @@
+#include "global.h"
+#include "fields.h"
+
+#include <vector>
+
+namespace ntt {
+
+template <>
+Fields<ONE_D>::Fields(std::vector<std::size_t> res)
+    : em_fields {"EM", res[0] + 2 * N_GHOSTS}, j_fields {"J", res[0] + 2 * N_GHOSTS} {}
+
+template <>
+Fields<TWO_D>::Fields(std::vector<std::size_t> res)
+    : em_fields {"EM", res[0] + 2 * N_GHOSTS, res[1] + 2 * N_GHOSTS},
+      j_fields {"J", res[0] + 2 * N_GHOSTS, res[1] + 2 * N_GHOSTS} {}
+
+template <>
+Fields<THREE_D>::Fields(std::vector<std::size_t> res)
+    : em_fields {"EM", res[0] + 2 * N_GHOSTS, res[1] + 2 * N_GHOSTS, res[2] + 2 * N_GHOSTS},
+      j_fields {"J", res[0] + 2 * N_GHOSTS, res[1] + 2 * N_GHOSTS, res[2] + 2 * N_GHOSTS} {}
+
+} // namespace ntt
+
+template struct ntt::Fields<ntt::ONE_D>;
+template struct ntt::Fields<ntt::TWO_D>;
+template struct ntt::Fields<ntt::THREE_D>;
