e73b713f92e1bd82aae99db58873644e2f1eeb16
haykh <haykh.mfs+github@gmail.com>
2021-11-10 18:17:25 -0500

    faraday 1d/2d curvilinear done
    Former-commit-id: d19ab5de010080365c080687027dccc3a09cc52b
    Former-commit-id: 8e34e6d1930a9c87b08afdecbf4cf083d6321176

--- src/pic/fieldsolver/faraday.hpp
+++ src/pic/fieldsolver/faraday.hpp
@@ -25,47 +25,163 @@ public:
   Inline void operator()(const index_t, const index_t, const index_t) const;
 };
 
+// clang-format off
+
+#ifdef HARDCODE_FLAT_COORDS
+// flat cartesian system
+
 template <>
 Inline void Faraday<ONE_D>::operator()(const index_t i) const {
-  m_mblock.em_fields(i, fld::bx2)
-      += coeff_x1 * (m_mblock.em_fields(i + 1, fld::ex3) - m_mblock.em_fields(i, fld::ex3));
-  m_mblock.em_fields(i, fld::bx3)
-      += coeff_x1 * (-m_mblock.em_fields(i + 1, fld::ex2) + m_mblock.em_fields(i, fld::ex2));
+  m_mblock.em_fields(i, fld::bx2) += coeff_x1 * (
+                m_mblock.em_fields(i + 1, fld::ex3) -
+                m_mblock.em_fields(i, fld::ex3)
+              );
+  m_mblock.em_fields(i, fld::bx3) += coeff_x1 * (
+                m_mblock.em_fields(i, fld::ex2) -
+                m_mblock.em_fields(i + 1, fld::ex2)
+              );
 }
 
 template <>
 Inline void Faraday<TWO_D>::operator()(const index_t i, const index_t j) const {
-  m_mblock.em_fields(i, j, fld::bx1)
-      += coeff_x2 * (-m_mblock.em_fields(i, j + 1, fld::ex3) + m_mblock.em_fields(i, j, fld::ex3));
-  m_mblock.em_fields(i, j, fld::bx2)
-      += coeff_x1 * (m_mblock.em_fields(i + 1, j, fld::ex3) - m_mblock.em_fields(i, j, fld::ex3));
-  m_mblock.em_fields(i, j, fld::bx3)
-      += coeff_x2 * (m_mblock.em_fields(i, j + 1, fld::ex1) - m_mblock.em_fields(i, j, fld::ex1))
-         + coeff_x1
-               * (-m_mblock.em_fields(i + 1, j, fld::ex2) + m_mblock.em_fields(i, j, fld::ex2));
+  m_mblock.em_fields(i, j, fld::bx1) += coeff_x2 * (
+                m_mblock.em_fields(i, j, fld::ex3) -
+                m_mblock.em_fields(i, j + 1, fld::ex3)
+              );
+  m_mblock.em_fields(i, j, fld::bx2) += coeff_x1 * (
+                m_mblock.em_fields(i + 1, j, fld::ex3) -
+                m_mblock.em_fields(i, j, fld::ex3)
+              );
+  m_mblock.em_fields(i, j, fld::bx3) += coeff_x2 * (
+                m_mblock.em_fields(i, j + 1, fld::ex1) -
+                m_mblock.em_fields(i, j, fld::ex1)
+              ) + coeff_x1 * (
+                m_mblock.em_fields(i, j, fld::ex2) -
+                m_mblock.em_fields(i + 1, j, fld::ex2)
+              );
 }
 
 template <>
 Inline void Faraday<THREE_D>::operator()(const index_t i, const index_t j, const index_t k) const {
-  m_mblock.em_fields(i, j, k, fld::bx1)
-      += coeff_x3
-             * (m_mblock.em_fields(i, j, k + 1, fld::ex2) - m_mblock.em_fields(i, j, k, fld::ex2))
-         + coeff_x2
-               * (-m_mblock.em_fields(i, j + 1, k, fld::ex3)
-                  + m_mblock.em_fields(i, j, k, fld::ex3));
-  m_mblock.em_fields(i, j, k, fld::bx2)
-      += coeff_x1
-             * (m_mblock.em_fields(i + 1, j, k, fld::ex3) - m_mblock.em_fields(i, j, k, fld::ex3))
-         + coeff_x3
-               * (-m_mblock.em_fields(i, j, k + 1, fld::ex1)
-                  + m_mblock.em_fields(i, j, k, fld::ex1));
-  m_mblock.em_fields(i, j, k, fld::bx3)
-      += coeff_x2
-             * (m_mblock.em_fields(i, j + 1, k, fld::ex1) - m_mblock.em_fields(i, j, k, fld::ex1))
-         + coeff_x1
-               * (-m_mblock.em_fields(i + 1, j, k, fld::ex2)
-                  + m_mblock.em_fields(i, j, k, fld::ex2));
+  m_mblock.em_fields(i, j, k, fld::bx1) += coeff_x3 * (
+                m_mblock.em_fields(i, j, k + 1, fld::ex2) -
+                m_mblock.em_fields(i, j, k, fld::ex2)
+              ) + coeff_x2 * (
+                m_mblock.em_fields(i, j, k, fld::ex3) -
+                m_mblock.em_fields(i, j + 1, k, fld::ex3)
+              );
+  m_mblock.em_fields(i, j, k, fld::bx2) += coeff_x1 * (
+                m_mblock.em_fields(i + 1, j, k, fld::ex3) -
+                m_mblock.em_fields(i, j, k, fld::ex3)
+              ) + coeff_x3 * (
+                m_mblock.em_fields(i, j, k, fld::ex1) -
+                m_mblock.em_fields(i, j, k + 1, fld::ex1)
+              );
+  m_mblock.em_fields(i, j, k, fld::bx3) += coeff_x2 * (
+                m_mblock.em_fields(i, j + 1, k, fld::ex1) -
+                m_mblock.em_fields(i, j, k, fld::ex1)
+              ) + coeff_x1 * (
+                m_mblock.em_fields(i, j, k, fld::ex2) -
+                m_mblock.em_fields(i + 1, j, k, fld::ex2)
+              );
 }
+
+#else
+// curvilinear coordinate system
+
+template <>
+Inline void Faraday<ONE_D>::operator()(const index_t i) const {
+  real_t x1 {m_mblock.convert_iTOx1(i)};
+  real_t dx1 {(m_extent[1] - m_extent[0]) / static_cast<real_t>(m_resolution[0])};
+
+  real_t hx1_iP {{m_mblock.Jacobian_h1(x1 + 0.5 * dx1)}};
+  real_t hx2_iP {{m_mblock.Jacobian_h2(x1 + 0.5 * dx1)}};
+  real_t hx3_iP {{m_mblock.Jacobian_h3(x1 + 0.5 * dx1)}};
+  real_t hx2_i {{m_mblock.Jacobian_h2(x1)}};
+  real_t hx2_iP1 {{m_mblock.Jacobian_h2(x1 + dx1)}};
+  real_t hx3_i {{m_mblock.Jacobian_h3(x1)}};
+  real_t hx3_iP1 {{m_mblock.Jacobian_h3(x1 + dx1)}};
+
+  m_mblock.em_fields(i, fld::bx2) += coeff_x1 * (
+                hx3_iP1 * m_mblock.em_fields(i + 1, fld::ex3) -
+                hx3_i * m_mblock.em_fields(i, fld::ex3)
+              ) / (hx1_iP * hx3_iP);
+  m_mblock.em_fields(i, fld::bx3) += coeff_x1 * (
+                hx2_i * m_mblock.em_fields(i, fld::ex2) -
+                hx2_iP1 * m_mblock.em_fields(i + 1, fld::ex2)
+              ) / (hx1_iP * hx2_iP);
+}
+
+template <>
+Inline void Faraday<TWO_D>::operator()(const index_t i, const index_t j) const {
+  real_t x1 {m_mblock.convert_iTOx1(i)};
+  real_t x2 {m_mblock.convert_jTOx2(j)};
+
+  real_t dx1 {(m_extent[1] - m_extent[0]) / static_cast<real_t>(m_resolution[0])};
+  real_t dx2 {(m_extent[3] - m_extent[2]) / static_cast<real_t>(m_resolution[1])};
+
+  real_t hx2_ijP {m_mblock.Jacobian_h2(x1, x2 + 0.5 * dx2)};
+  real_t hx3_ijP {m_mblock.Jacobian_h3(x1, x2 + 0.5 * dx2)};
+  real_t hx1_iPj {m_mblock.Jacobian_h1(x1 + 0.5 * dx1, x2)};
+  real_t hx3_iPj {m_mblock.Jacobian_h3(x1 + 0.5 * dx1, x2)};
+  real_t hx1_iPjP {m_mblock.Jacobian_h1(x1 + 0.5 * dx1, x2 + 0.5 * dx2)};
+  real_t hx2_iPjP {m_mblock.Jacobian_h2(x1 + 0.5 * dx1, x2 + 0.5 * dx2)};
+
+  real_t hx3_ij {m_mblock.Jacobian_h3(x1, x2)};
+  real_t hx3_ijP1 {m_mblock.Jacobian_h3(x1, x2 + dx2)};
+  real_t hx3_iP1j {m_mblock.Jacobian_h3(x1 + dx1, x2)};
+  real_t hx1_iPjP1 {m_mblock.Jacobian_h1(x1 + 0.5 * dx1, x2 + dx2)};
+
+  real_t hx2_iP1jP {m_mblock.Jacobian_h2(x1 + dx1, x2 + 0.5 * dx2)};
+  real_t hx2_ijP {m_mblock.Jacobian_h2(x1, x2 + 0.5 * dx2)}
+
+  m_mblock.em_fields(i, j, fld::bx1) += coeff_x2 * (
+                hx3_ij * m_mblock.em_fields(i, j, fld::ex3) -
+                hx3_ijP1 * m_mblock.em_fields(i, j + 1, fld::ex3)
+              ) / (hx2_ijP * hx3_ijP);
+  m_mblock.em_fields(i, j, fld::bx2) += coeff_x1 * (
+                hx3_iP1j * m_mblock.em_fields(i + 1, j, fld::ex3) -
+                hx3_ij * m_mblock.em_fields(i, j, fld::ex3)
+              ) / (hx1_iPj * hx3_iPj);
+  m_mblock.em_fields(i, j, fld::bx3) += (coeff_x2 * (
+                hx1_iPjP1 * m_mblock.em_fields(i, j + 1, fld::ex1) -
+                hx1_iPj * m_mblock.em_fields(i, j, fld::ex1)
+              ) + coeff_x1 * (
+                hx2_iP1jP * m_mblock.em_fields(i, j, fld::ex2) -
+                hx2_ijP * hx2 * m_mblock.em_fields(i + 1, j, fld::ex2))
+            ) / (hx1_iPjP * hx2_iPjP);
+}
+
+template <>
+Inline void Faraday<THREE_D>::operator()(const index_t i, const index_t j, const index_t k) const {
+  throw std::logic_error("# NOT IMPLEMENTED.");
+  m_mblock.em_fields(i, j, k, fld::bx1) += coeff_x3 * (
+                m_mblock.em_fields(i, j, k + 1, fld::ex2) -
+                m_mblock.em_fields(i, j, k, fld::ex2)
+              ) + coeff_x2 * (
+                m_mblock.em_fields(i, j, k, fld::ex3) -
+                m_mblock.em_fields(i, j + 1, k, fld::ex3)
+              );
+  m_mblock.em_fields(i, j, k, fld::bx2) += coeff_x1 * (
+                m_mblock.em_fields(i + 1, j, k, fld::ex3) -
+                m_mblock.em_fields(i, j, k, fld::ex3)
+              ) + coeff_x3 * (
+                m_mblock.em_fields(i, j, k, fld::ex1) -
+                m_mblock.em_fields(i, j, k + 1, fld::ex1)
+              );
+  m_mblock.em_fields(i, j, k, fld::bx3) += coeff_x2 * (
+                m_mblock.em_fields(i, j + 1, k, fld::ex1) -
+                m_mblock.em_fields(i, j, k, fld::ex1)
+              ) + coeff_x1 * (
+                m_mblock.em_fields(i, j, k, fld::ex2) -
+                m_mblock.em_fields(i + 1, j, k, fld::ex2)
+              );
+}
+
+
+#endif
+
+// clang-format on
 
 } // namespace ntt
 
