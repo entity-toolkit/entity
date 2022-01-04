6fa0dcd9323fbb0d5164664482c6e0ec8bfe838f
haykh <haykh.mfs+github@gmail.com>
2022-01-04 17:33:41 -0500

    update
    Former-commit-id: 2cfe0c4311b3017125e93bb743199a0c0f4c9263
    Former-commit-id: d84cf44bbfc65e6b4051ff7d8075eb1598b55a03

--- src/objects/grid.cpp
+++ src/objects/grid.cpp
@@ -2,6 +2,8 @@
 #include "grid.h"
 
 #include <vector>
+#include <algorithm>
+
 
 namespace ntt {
 
@@ -9,8 +11,24 @@ namespace ntt {
   Grid<D>::Grid(std::vector<real_t> ext,
                 std::vector<std::size_t> res)
     : m_extent {std::move(ext)},
-      m_resolution {std::move(res)}
-    {}
+      m_resolution {std::move(res)},
+      grid_x1 {"grid_x1", res.size() > 0 ? res[0] + 1 : 1},
+      grid_x2 {"grid_x2", res.size() > 1 ? res[1] + 1 : 1},
+      grid_x3 {"grid_x3", res.size() > 2 ? res[2] + 1 : 1} {
+    using index_t = NTTArray<real_t*>::size_type;
+    Kokkos::parallel_for(grid_x1.extent(0),
+        Lambda(index_t n) {
+          grid_x1(n) = static_cast<real_t>(n) / std::max(ONE, static_cast<real_t>(grid_x1.extent(0) - 1));
+    });
+    Kokkos::parallel_for(grid_x2.extent(0),
+        Lambda(index_t n) {
+          grid_x2(n) = static_cast<real_t>(n) / std::max(ONE, static_cast<real_t>(grid_x2.extent(0) - 1));
+    });
+    Kokkos::parallel_for(grid_x3.extent(0),
+        Lambda(index_t n) {
+          grid_x3(n) = static_cast<real_t>(n) / std::max(ONE, static_cast<real_t>(grid_x3.extent(0) - 1));
+    });
+  }
 
   // # # # # # # # # # # # # # # # #
   // custom range
