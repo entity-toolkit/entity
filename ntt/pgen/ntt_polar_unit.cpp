6bbd4d36ebd932aa2d83c12ac65922cc16c00cd4
Hayk Hakobyan <haykh.astro@gmail.com>
2022-01-05 18:14:28 -0500

    ready to test (no axial in polar)
    Former-commit-id: 0bf4cc268a2716f2d029cf42d552d80d39712b76
    Former-commit-id: bb50a3d7103a2f724731b3fe243114dbf71a6b14

--- ntt/pgen/ntt_polar_unit.cpp
+++ ntt/pgen/ntt_polar_unit.cpp
@@ -27,21 +27,18 @@ namespace ntt {
     UNUSED(sim_params);
     using index_t = NTTArray<real_t**>::size_type;
     Kokkos::deep_copy(mblock.em_fields, 0.0);
-    real_t dx1 {mblock.get_dx1()};
-    real_t dx2 {mblock.get_dx2()};
+    real_t r_min {mblock.m_coord_system->x1_min};
     Kokkos::parallel_for(
       "userInitFlds",
       mblock.loopActiveCells(),
       Lambda(index_t i, index_t j) {
-        auto x1 {mblock.convert_iTOx1(i)};
-        auto x2 {mblock.convert_jTOx2(j)};
+        auto i_ {static_cast<real_t>(i)};
+        auto j_half {static_cast<real_t>(j) + HALF};
 
-        auto r0 {mblock.m_coord_system->getSpherical_r(mblock.convert_iTOx1(N_GHOSTS), ZERO)};
-        auto rr {mblock.m_coord_system->getSpherical_r(x1, ZERO)};
+        auto [r_, th_] = mblock.m_coord_system->coord_CU_to_Sph(i_, j_half);
 
-        auto bx1 {ONE * r0 * r0 / (rr * rr)};
-        bx1 = mblock.m_coord_system->convert_LOC_to_CNT_x1(bx1, x1, x2 + 0.5 * dx2);
-        mblock.em_fields(i, j, fld::bx1) = bx1;
+        auto br_hat {ONE * r_min * r_min / (r_ * r_)};
+        mblock.em_fields(i, j, fld::bx1) = mblock.m_coord_system->vec_HAT_to_CNT_x1(br_hat, i_, j_half);
     });
   }
 
@@ -63,8 +60,6 @@ namespace ntt {
                                              Meshblock<TWO_D>& mblock) {
     UNUSED(sim_params);
     using index_t = NTTArray<real_t**>::size_type;
-    real_t dx1 {mblock.get_dx1()};
-    real_t dx2 {mblock.get_dx2()};
     real_t omega;
     if (time < 0.5) {
       omega = time / 10.0;
@@ -72,27 +67,25 @@ namespace ntt {
       omega = 0.05;
     }
     Kokkos::parallel_for(
-      "userBcFlds_rmax",
-      NTT2DRange({mblock.get_imin(), mblock.get_jmin()}, {mblock.get_imin() + 1, mblock.get_jmax()}),
+      "userBcFlds_rmin",
+      NTT2DRange({mblock.i_min, mblock.j_min}, {mblock.i_min + 1, mblock.j_max}),
       Lambda(index_t i, index_t j) {
-        auto x1 {mblock.convert_iTOx1(i)};
-        auto x2 {mblock.convert_jTOx2(j)};
+        auto i_ {static_cast<real_t>(i)};
+        auto j_half {static_cast<real_t>(j) + HALF};
+
+        auto [r_, th_] = mblock.m_coord_system->coord_CU_to_Sph(i_, j_half);
+        auto etheta_hat = omega * std::sin(th_);
 
         mblock.em_fields(i, j, fld::ex3) = 0.0;
+        mblock.em_fields(i, j, fld::ex2) = mblock.m_coord_system->vec_HAT_to_CNT_x2(etheta_hat, i_, j_half);
 
-        auto theta {mblock.m_coord_system->getSpherical_theta(ZERO, x2 + 0.5 * dx2)};
-        auto ex2 {omega * std::sin(theta)};
-        ex2 = mblock.m_coord_system->convert_LOC_to_CNT_x2(ex2, x1, x2 + 0.5 * dx2);
-        mblock.em_fields(i, j, fld::ex2) = ex2;
-
-        auto bx1 {1.0};
-        bx1 = mblock.m_coord_system->convert_LOC_to_CNT_x1(bx1, x1, x2 + 0.5 * dx1);
-        mblock.em_fields(i, j, fld::bx1) = bx1;
-    });
+        auto br_hat {ONE};
+        mblock.em_fields(i, j, fld::bx1) = mblock.m_coord_system->vec_HAT_to_CNT_x1(br_hat, i_, j_half);
+      });
 
     Kokkos::parallel_for(
-      "userBcFlds_rmin",
-      NTT2DRange({mblock.get_imax(), mblock.get_jmin()}, {mblock.get_imax() + 1, mblock.get_jmax()}),
+      "userBcFlds_rmax",
+      NTT2DRange({mblock.i_max, mblock.j_min}, {mblock.i_max + 1, mblock.j_max}),
       Lambda(index_t i, index_t j) {
         mblock.em_fields(i, j, fld::ex3) = 0.0;
         mblock.em_fields(i, j, fld::ex2) = 0.0;
