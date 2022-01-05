907286b09190563b133b168c00dafc27db78fadc
haykh <haykh.mfs+github@gmail.com>
2022-01-05 16:01:16 -0500

    fixed bugs
    Former-commit-id: 7f4119c21b1a97bc676cb7c7f5bce58972133e0a
    Former-commit-id: 0ca91cc42a28d5d481265378fc84a301127bcde5

--- src/objects/geometry/cartesian.h
+++ src/objects/geometry/cartesian.h
@@ -12,39 +12,35 @@ namespace ntt {
   struct CartesianSystem : public CoordinateGrid<D> {
   protected:
     const real_t dx, inv_dx;
-    const real_t x_min, y_min, z_min;
 
   public:
     CartesianSystem(std::vector<std::size_t> resolution, std::vector<real_t> extent)
         : CoordinateGrid<D> {"cartesian", resolution, extent},
-          dx((extent[1] - extent[0]) / (real_t)(resolution[0])),
-          inv_dx(ONE / dx),
-          x_min(resolution.size() > 0 ? extent[0] : ZERO),
-          y_min(resolution.size() > 1 ? extent[2] : ZERO),
-          z_min(resolution.size() > 2 ? extent[4] : ZERO) {}
+          dx((this->x1_max - this->x1_min) / (real_t)(this->Nx1)),
+          inv_dx(ONE / dx) {}
     ~CartesianSystem() = default;
 
     // coordinate transformations
     // conversion from code units (CU) to cartesian (Cart)
-    Inline auto coord_CU_to_Cart(const real_t &x1) const -> real_t override { 
-      return x1 * dx + x_min;
+    Inline auto coord_CU_to_Cart(const real_t &x1) const -> real_t override {
+      return x1 * dx + this->x1_min;
     }
     Inline auto coord_CU_to_Cart(const real_t &x1, const real_t &x2) const -> std::tuple<real_t, real_t> override {
-      return {x1 * dx + x_min, x2 * dx + y_min};
+      return {x1 * dx + this->x1_min, x2 * dx + this->x2_min};
     }
-    Inline auto coord_CU_to_Cart(const real_t &x1, const real_t &x2, const real_t &x3) const -> std::tuple<real_t, real_t, real_t> override { 
-      return {x1 * dx + x_min, x2 * dx + y_min, x3 * dx + z_min};
+    Inline auto coord_CU_to_Cart(const real_t &x1, const real_t &x2, const real_t &x3) const -> std::tuple<real_t, real_t, real_t> override {
+      return {x1 * dx + this->x1_min, x2 * dx + this->x2_min, x3 * dx + this->x3_min};
     }
 
     // conversion from cartesian (Cart) to code units (CU)
-    Inline auto coord_CART_to_CU(const real_t &x) const -> real_t override { 
-      return (x - x_min) * inv_dx;
+    Inline auto coord_CART_to_CU(const real_t &x) const -> real_t override {
+      return (x - this->x1_min) * inv_dx;
     }
     Inline auto coord_CART_to_CU(const real_t &x, const real_t &y) const -> std::tuple<real_t, real_t> override {
-      return {(x - x_min) * inv_dx, (y - y_min) * inv_dx};
+      return {(x - this->x1_min) * inv_dx, (y - this->x2_min) * inv_dx};
     }
     Inline auto coord_CART_to_CU(const real_t &x, const real_t &y, const real_t &z) const -> std::tuple<real_t, real_t, real_t> override {
-      return {(x - x_min) * inv_dx, (y - y_min) * inv_dx, (z - z_min) * inv_dx};
+      return {(x - this->x1_min) * inv_dx, (y - this->x2_min) * inv_dx, (z - this->x3_min) * inv_dx};
     }
 
     // // vector transformations
@@ -58,43 +54,43 @@ namespace ntt {
     // Inline auto transform_uxuyTOux1ux2(const real_t& ux, const real_t& uy) const -> std::tuple<real_t, real_t> override { return {ux, uy}; }
     // Inline auto transform_uxuyuzTOux1ux2ux3(const real_t& ux, const real_t& uy, const real_t& uz) const -> std::tuple<real_t, real_t, real_t> override { return {ux, uy, uz}; }
 
-    Inline auto h11(const real_t &) const -> real_t { 
+    Inline auto h11(const real_t &) const -> real_t {
       return dx;
     }
-    Inline auto h11(const real_t &, const real_t &) const -> real_t { 
+    Inline auto h11(const real_t &, const real_t &) const -> real_t {
       return dx;
     }
-    Inline auto h11(const real_t &, const real_t &, const real_t &) const -> real_t { 
-      return dx;
-    }
-
-    Inline auto h22(const real_t&) const -> real_t { 
-      return dx;
-    }
-    Inline auto h22(const real_t&, const real_t&) const -> real_t { 
-      return dx;
-    }
-    Inline auto h22(const real_t&, const real_t&, const real_t&) const -> real_t { 
+    Inline auto h11(const real_t &, const real_t &, const real_t &) const -> real_t {
       return dx;
     }
 
-    Inline auto h33(const real_t&) const -> real_t { 
+    Inline auto h22(const real_t&) const -> real_t {
       return dx;
     }
-    Inline auto h33(const real_t&, const real_t&) const -> real_t { 
+    Inline auto h22(const real_t&, const real_t&) const -> real_t {
       return dx;
     }
-    Inline auto h33(const real_t&, const real_t&, const real_t&) const -> real_t { 
+    Inline auto h22(const real_t&, const real_t&, const real_t&) const -> real_t {
       return dx;
     }
 
-    Inline auto sqrt_det_h(const real_t&) const -> real_t { 
+    Inline auto h33(const real_t&) const -> real_t {
+      return dx;
+    }
+    Inline auto h33(const real_t&, const real_t&) const -> real_t {
+      return dx;
+    }
+    Inline auto h33(const real_t&, const real_t&, const real_t&) const -> real_t {
+      return dx;
+    }
+
+    Inline auto sqrt_det_h(const real_t&) const -> real_t {
       return dx * dx * dx;
     }
-    Inline auto sqrt_det_h(const real_t&, const real_t&) const -> real_t { 
+    Inline auto sqrt_det_h(const real_t&, const real_t&) const -> real_t {
       return dx * dx * dx;
     }
-    Inline auto sqrt_det_h(const real_t&, const real_t&, const real_t&) const -> real_t { 
+    Inline auto sqrt_det_h(const real_t&, const real_t&, const real_t&) const -> real_t {
       return dx * dx * dx;
     }
 
