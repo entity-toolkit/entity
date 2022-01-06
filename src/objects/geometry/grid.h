241bd76a0761fbaca311b26f78520a7ca56049ed
Hayk Hakobyan <haykh.astro@gmail.com>
2022-01-06 00:19:31 -0500

    qsph ready to test
    Former-commit-id: e7717b903fc588e4a0d36ffe9c7bec67ee211d62
    Former-commit-id: 66e75d15e6d9b73cda9e14ed0d589edb1d4d6887

--- src/objects/geometry/grid.h
+++ src/objects/geometry/grid.h
@@ -9,27 +9,25 @@
 namespace ntt {
 
   template <Dimension D>
-  struct CoordinateGrid {
+  struct Grid {
     const std::string label;
     const long int Nx1, Nx2, Nx3;
     const real_t x1_min, x1_max;
     const real_t x2_min, x2_max;
     const real_t x3_min, x3_max;
 
-    CoordinateGrid(const std::string& label_,
-                   std::vector<std::size_t> resolution,
-                   std::vector<real_t> extent)
-        : label {label_},
-          Nx1 {resolution.size() > 0 ? resolution[0] : 1},
-          Nx2 {resolution.size() > 1 ? resolution[1] : 1},
-          Nx3 {resolution.size() > 2 ? resolution[2] : 1},
-          x1_min {resolution.size() > 0 ? extent[0] : ZERO},
-          x1_max {resolution.size() > 0 ? extent[1] : ZERO},
-          x2_min {resolution.size() > 1 ? extent[2] : ZERO},
-          x2_max {resolution.size() > 1 ? extent[3] : ZERO},
-          x3_min {resolution.size() > 2 ? extent[4] : ZERO},
-          x3_max {resolution.size() > 2 ? extent[5] : ZERO} {}
-    virtual ~CoordinateGrid() = default;
+    Grid(const std::string& label_, std::vector<std::size_t> resolution, std::vector<real_t> extent)
+      : label {label_},
+        Nx1 {resolution.size() > 0 ? resolution[0] : 1},
+        Nx2 {resolution.size() > 1 ? resolution[1] : 1},
+        Nx3 {resolution.size() > 2 ? resolution[2] : 1},
+        x1_min {resolution.size() > 0 ? extent[0] : ZERO},
+        x1_max {resolution.size() > 0 ? extent[1] : ZERO},
+        x2_min {resolution.size() > 1 ? extent[2] : ZERO},
+        x2_max {resolution.size() > 1 ? extent[3] : ZERO},
+        x3_min {resolution.size() > 2 ? extent[4] : ZERO},
+        x3_max {resolution.size() > 2 ? extent[5] : ZERO} {}
+    virtual ~Grid() = default;
 
     // coordinate transformations
     Inline auto CU_to_Idi(const real_t& xi) const -> std::pair<long int, float> {
@@ -42,26 +40,38 @@ namespace ntt {
     // conversion from code units (CU) to cartesian (Cart)
     virtual Inline auto coord_CU_to_Cart(const real_t&) const -> real_t { return -1.0; }
     virtual Inline auto coord_CU_to_Cart(const real_t&, const real_t&) const -> std::tuple<real_t, real_t> {
-      return {-1.0, -1.0}; }
-    virtual Inline auto coord_CU_to_Cart(const real_t&, const real_t&, const real_t&) const -> std::tuple<real_t, real_t, real_t> { return {-1.0, -1.0, -1.0}; }
+      return {-1.0, -1.0};
+    }
+    virtual Inline auto coord_CU_to_Cart(const real_t&, const real_t&, const real_t&) const
+      -> std::tuple<real_t, real_t, real_t> {
+      return {-1.0, -1.0, -1.0};
+    }
 
     // // conversion from cartesian (Cart) to code units (CU)
     // virtual Inline auto coord_CART_to_CU(const real_t&) const -> real_t { return -1.0; }
-    // virtual Inline auto coord_CART_to_CU(const real_t&, const real_t&) const -> std::tuple<real_t, real_t> { return {-1.0, -1.0}; }
-    // virtual Inline auto coord_CART_to_CU(const real_t&, const real_t&, const real_t&) const -> std::tuple<real_t, real_t, real_t> { return {-1.0, -1.0, -1.0}; }
+    // virtual Inline auto coord_CART_to_CU(const real_t&, const real_t&) const -> std::tuple<real_t, real_t> { return
+    // {-1.0, -1.0}; } virtual Inline auto coord_CART_to_CU(const real_t&, const real_t&, const real_t&) const ->
+    // std::tuple<real_t, real_t, real_t> { return {-1.0, -1.0, -1.0}; }
 
     // conversion from code units (CU) to spherical (Sph)
-    virtual Inline auto coord_CU_to_Sph(const real_t&, const real_t&) const -> std::tuple<real_t, real_t> { return {-1.0, -1.0}; }
-    virtual Inline auto coord_CU_to_Sph(const real_t&, const real_t&, const real_t&) const -> std::tuple<real_t, real_t, real_t> { return {-1.0, -1.0, -1.0}; }
+    virtual Inline auto coord_CU_to_Sph(const real_t&, const real_t&) const -> std::tuple<real_t, real_t> {
+      return {-1.0, -1.0};
+    }
+    virtual Inline auto coord_CU_to_Sph(const real_t&, const real_t&, const real_t&) const
+      -> std::tuple<real_t, real_t, real_t> {
+      return {-1.0, -1.0, -1.0};
+    }
 
     // // velocity conversion
     // virtual Inline auto transform_ux1TOux(const real_t&) const -> real_t { return -1.0; }
-    // virtual Inline auto transform_ux1ux2TOuxuy(const real_t&, const real_t&) const -> std::tuple<real_t, real_t> { return {-1.0, -1.0}; }
-    // virtual Inline auto transform_ux1ux2ux3TOuxuyuz(const real_t&, const real_t&, const real_t&) const -> std::tuple<real_t, real_t, real_t> { return {-1.0, -1.0, -1.0}; }
+    // virtual Inline auto transform_ux1ux2TOuxuy(const real_t&, const real_t&) const -> std::tuple<real_t, real_t> {
+    // return {-1.0, -1.0}; } virtual Inline auto transform_ux1ux2ux3TOuxuyuz(const real_t&, const real_t&, const
+    // real_t&) const -> std::tuple<real_t, real_t, real_t> { return {-1.0, -1.0, -1.0}; }
 
     // virtual Inline auto transform_uxTOux1(const real_t&) const -> real_t { return -1.0; }
-    // virtual Inline auto transform_uxuyTOux1ux2(const real_t&, const real_t&) const -> std::tuple<real_t, real_t> { return {-1.0, -1.0}; };
-    // virtual Inline auto transform_uxuyuzTOux1ux2ux3(const real_t&, const real_t&, const real_t&) const -> std::tuple<real_t, real_t, real_t> { return {-1.0, -1.0, -1.0}; }
+    // virtual Inline auto transform_uxuyTOux1ux2(const real_t&, const real_t&) const -> std::tuple<real_t, real_t> {
+    // return {-1.0, -1.0}; }; virtual Inline auto transform_uxuyuzTOux1ux2ux3(const real_t&, const real_t&, const
+    // real_t&) const -> std::tuple<real_t, real_t, real_t> { return {-1.0, -1.0, -1.0}; }
 
     virtual Inline auto h11(const real_t&) const -> real_t { return -1.0; }
     virtual Inline auto h11(const real_t&, const real_t&) const -> real_t { return -1.0; }
@@ -82,72 +92,121 @@ namespace ntt {
     virtual Inline auto polar_area(const real_t&, const real_t&) const -> real_t { return -1.0; }
     virtual Inline auto polar_area(const real_t&, const real_t&, const real_t&) const -> real_t { return -1.0; }
 
-    // CNT -> contravariant (upper index)
-    // CVR -> covariant (lower index)
-    // HAT -> local orthonormal (hatted index)
-    //
-    // CNT -> HAT
-    Inline auto vec_CNT_to_HAT_x1(const real_t& ax1, const real_t& x1) -> real_t {
-      return std::sqrt(h11(x1)) * ax1;
-    }
-    Inline auto vec_CNT_to_HAT_x1(const real_t& ax1, const real_t& x1, const real_t& x2) -> real_t {
-      return std::sqrt(h11(x1, x2)) * ax1;
-    }
-    Inline auto vec_CNT_to_HAT_x1(const real_t& ax1, const real_t& x1, const real_t& x2, const real_t& x3) -> real_t {
-      return std::sqrt(h11(x1, x2, x3)) * ax1;
-    }
+    Inline auto vec_CNT_to_HAT_x1(const real_t& ax1, const real_t& x1) -> real_t;
+    Inline auto vec_CNT_to_HAT_x1(const real_t& ax1, const real_t& x1, const real_t& x2) -> real_t;
+    Inline auto vec_CNT_to_HAT_x1(const real_t& ax1, const real_t& x1, const real_t& x2, const real_t& x3) -> real_t;
 
-    Inline auto vec_CNT_to_HAT_x2(const real_t& ax2, const real_t& x1) -> real_t {
-      return std::sqrt(h22(x1)) * ax2;
-    }
-    Inline auto vec_CNT_to_HAT_x2(const real_t& ax2, const real_t& x1, const real_t& x2) -> real_t {
-      return std::sqrt(h22(x1, x2)) * ax2;
-    }
-    Inline auto vec_CNT_to_HAT_x2(const real_t& ax2, const real_t& x1, const real_t& x2, const real_t& x3) -> real_t {
-      return std::sqrt(h22(x1, x2, x3)) * ax2;
-    }
+    Inline auto vec_CNT_to_HAT_x2(const real_t& ax2, const real_t& x1) -> real_t;
+    Inline auto vec_CNT_to_HAT_x2(const real_t& ax2, const real_t& x1, const real_t& x2) -> real_t;
+    Inline auto vec_CNT_to_HAT_x2(const real_t& ax2, const real_t& x1, const real_t& x2, const real_t& x3) -> real_t;
 
-    Inline auto vec_CNT_to_HAT_x3(const real_t& ax3, const real_t& x1) -> real_t {
-      return std::sqrt(h33(x1)) * ax3;
-    }
-    Inline auto vec_CNT_to_HAT_x3(const real_t& ax3, const real_t& x1, const real_t& x2) -> real_t {
-      return std::sqrt(h33(x1, x2)) * ax3;
-    }
-    Inline auto vec_CNT_to_HAT_x3(const real_t& ax3, const real_t& x1, const real_t& x2, const real_t& x3) -> real_t {
-      return std::sqrt(h33(x1, x2, x3)) * ax3;
-    }
+    Inline auto vec_CNT_to_HAT_x3(const real_t& ax3, const real_t& x1) -> real_t;
+    Inline auto vec_CNT_to_HAT_x3(const real_t& ax3, const real_t& x1, const real_t& x2) -> real_t;
+    Inline auto vec_CNT_to_HAT_x3(const real_t& ax3, const real_t& x1, const real_t& x2, const real_t& x3) -> real_t;
 
     // LOC -> CNT
-    Inline auto vec_HAT_to_CNT_x1(const real_t& ax1, const real_t& x1) -> real_t {
-      return ax1 / std::sqrt(h11(x1));
-    }
-    Inline auto vec_HAT_to_CNT_x1(const real_t& ax1, const real_t& x1, const real_t& x2) -> real_t {
-      return ax1 / std::sqrt(h11(x1, x2));
-    }
-    Inline auto vec_HAT_to_CNT_x1(const real_t& ax1, const real_t& x1, const real_t& x2, const real_t& x3) -> real_t {
-      return ax1 / std::sqrt(h11(x1, x2, x3));
-    }
+    Inline auto vec_HAT_to_CNT_x1(const real_t& ax1, const real_t& x1) -> real_t;
+    Inline auto vec_HAT_to_CNT_x1(const real_t& ax1, const real_t& x1, const real_t& x2) -> real_t;
+    Inline auto vec_HAT_to_CNT_x1(const real_t& ax1, const real_t& x1, const real_t& x2, const real_t& x3) -> real_t;
 
-    Inline auto vec_HAT_to_CNT_x2(const real_t& ax2, const real_t& x1) -> real_t {
-      return ax2 / std::sqrt(h22(x1));
-    }
-    Inline auto vec_HAT_to_CNT_x2(const real_t& ax2, const real_t& x1, const real_t& x2) -> real_t {
-      return ax2 / std::sqrt(h22(x1, x2));
-    }
-    Inline auto vec_HAT_to_CNT_x2(const real_t& ax2, const real_t& x1, const real_t& x2, const real_t& x3) -> real_t {
-      return ax2 / std::sqrt(h22(x1, x2, x3));
-    }
+    Inline auto vec_HAT_to_CNT_x2(const real_t& ax2, const real_t& x1) -> real_t;
+    Inline auto vec_HAT_to_CNT_x2(const real_t& ax2, const real_t& x1, const real_t& x2) -> real_t;
+    Inline auto vec_HAT_to_CNT_x2(const real_t& ax2, const real_t& x1, const real_t& x2, const real_t& x3) -> real_t;
 
-    Inline auto vec_HAT_to_CNT_x3(const real_t& ax3, const real_t& x1) -> real_t {
-      return ax3 / std::sqrt(h33(x1));
-    }
-    Inline auto vec_HAT_to_CNT_x3(const real_t& ax3, const real_t& x1, const real_t& x2) -> real_t {
-      return ax3 / std::sqrt(h33(x1, x2));
-    }
-    Inline auto vec_HAT_to_CNT_x3(const real_t& ax3, const real_t& x1, const real_t& x2, const real_t& x3) -> real_t {
-      return ax3 / std::sqrt(h33(x1, x2, x3));
-    }
+    Inline auto vec_HAT_to_CNT_x3(const real_t& ax3, const real_t& x1) -> real_t;
+    Inline auto vec_HAT_to_CNT_x3(const real_t& ax3, const real_t& x1, const real_t& x2) -> real_t;
+    Inline auto vec_HAT_to_CNT_x3(const real_t& ax3, const real_t& x1, const real_t& x2, const real_t& x3) -> real_t;
   };
+
+  // CNT -> contravariant (upper index)
+  // CVR -> covariant (lower index)
+  // HAT -> local orthonormal (hatted index)
+  //
+  // CNT -> HAT
+  template <>
+  Inline auto Grid<ONE_D>::vec_CNT_to_HAT_x1(const real_t& ax1, const real_t& x1) -> real_t {
+    return std::sqrt(h11(x1)) * ax1;
+  }
+  template <>
+  Inline auto Grid<TWO_D>::vec_CNT_to_HAT_x1(const real_t& ax1, const real_t& x1, const real_t& x2) -> real_t {
+    return std::sqrt(h11(x1, x2)) * ax1;
+  }
+  template <>
+  Inline auto Grid<THREE_D>::vec_CNT_to_HAT_x1(const real_t& ax1, const real_t& x1, const real_t& x2, const real_t& x3)
+    -> real_t {
+    return std::sqrt(h11(x1, x2, x3)) * ax1;
+  }
+
+  template <>
+  Inline auto Grid<ONE_D>::vec_CNT_to_HAT_x2(const real_t& ax2, const real_t& x1) -> real_t {
+    return std::sqrt(h22(x1)) * ax2;
+  }
+  template <>
+  Inline auto Grid<TWO_D>::vec_CNT_to_HAT_x2(const real_t& ax2, const real_t& x1, const real_t& x2) -> real_t {
+    return std::sqrt(h22(x1, x2)) * ax2;
+  }
+  template <>
+  Inline auto Grid<THREE_D>::vec_CNT_to_HAT_x2(const real_t& ax2, const real_t& x1, const real_t& x2, const real_t& x3)
+    -> real_t {
+    return std::sqrt(h22(x1, x2, x3)) * ax2;
+  }
+
+  template <>
+  Inline auto Grid<ONE_D>::vec_CNT_to_HAT_x3(const real_t& ax3, const real_t& x1) -> real_t {
+    return std::sqrt(h33(x1)) * ax3;
+  }
+  template <>
+  Inline auto Grid<TWO_D>::vec_CNT_to_HAT_x3(const real_t& ax3, const real_t& x1, const real_t& x2) -> real_t {
+    return std::sqrt(h33(x1, x2)) * ax3;
+  }
+  template <>
+  Inline auto Grid<THREE_D>::vec_CNT_to_HAT_x3(const real_t& ax3, const real_t& x1, const real_t& x2, const real_t& x3)
+    -> real_t {
+    return std::sqrt(h33(x1, x2, x3)) * ax3;
+  }
+
+  // LOC -> CNT
+  template <>
+  Inline auto Grid<ONE_D>::vec_HAT_to_CNT_x1(const real_t& ax1, const real_t& x1) -> real_t {
+    return ax1 / std::sqrt(h11(x1));
+  }
+  template <>
+  Inline auto Grid<TWO_D>::vec_HAT_to_CNT_x1(const real_t& ax1, const real_t& x1, const real_t& x2) -> real_t {
+    return ax1 / std::sqrt(h11(x1, x2));
+  }
+  template <>
+  Inline auto Grid<THREE_D>::vec_HAT_to_CNT_x1(const real_t& ax1, const real_t& x1, const real_t& x2, const real_t& x3)
+    -> real_t {
+    return ax1 / std::sqrt(h11(x1, x2, x3));
+  }
+
+  template <>
+  Inline auto Grid<ONE_D>::vec_HAT_to_CNT_x2(const real_t& ax2, const real_t& x1) -> real_t {
+    return ax2 / std::sqrt(h22(x1));
+  }
+  template <>
+  Inline auto Grid<TWO_D>::vec_HAT_to_CNT_x2(const real_t& ax2, const real_t& x1, const real_t& x2) -> real_t {
+    return ax2 / std::sqrt(h22(x1, x2));
+  }
+  template <>
+  Inline auto Grid<THREE_D>::vec_HAT_to_CNT_x2(const real_t& ax2, const real_t& x1, const real_t& x2, const real_t& x3)
+    -> real_t {
+    return ax2 / std::sqrt(h22(x1, x2, x3));
+  }
+
+  template <>
+  Inline auto Grid<ONE_D>::vec_HAT_to_CNT_x3(const real_t& ax3, const real_t& x1) -> real_t {
+    return ax3 / std::sqrt(h33(x1));
+  }
+  template <>
+  Inline auto Grid<TWO_D>::vec_HAT_to_CNT_x3(const real_t& ax3, const real_t& x1, const real_t& x2) -> real_t {
+    return ax3 / std::sqrt(h33(x1, x2));
+  }
+  template <>
+  Inline auto Grid<THREE_D>::vec_HAT_to_CNT_x3(const real_t& ax3, const real_t& x1, const real_t& x2, const real_t& x3)
+    -> real_t {
+    return ax3 / std::sqrt(h33(x1, x2, x3));
+  }
 
 } // namespace ntt
 
--- src/objects/geometry/spherical.h
+++ src/objects/geometry/spherical.h
@@ -9,154 +9,124 @@
 
 namespace ntt {
 
+  // r, theta, phi
   template <Dimension D>
-  struct SphericalSystem : public CoordinateGrid<D> {
+  struct SphericalSystem : public Grid<D> {
   protected:
     const real_t dr, dtheta, dphi;
     const real_t dr_sqr, dtheta_sqr, dphi_sqr;
 
   public:
     SphericalSystem(std::vector<std::size_t> resolution, std::vector<real_t> extent)
-      : CoordinateGrid<D> {"spherical", resolution, extent},
+      : Grid<D> {"spherical", resolution, extent},
         dr((this->x1_max - this->x1_min) / (real_t)(this->Nx1)),
-        dtheta((this->x2_max - this->x2_min) / (real_t)(this->Nx2)),
-        dphi((this->x3_max - this->x3_min) / (real_t)(this->Nx3)),
+        dtheta(PI / (real_t)(this->Nx2)),
+        dphi(TWO_PI / (real_t)(this->Nx3)),
         dr_sqr(dr * dr),
         dtheta_sqr(dtheta * dtheta),
         dphi_sqr(dphi * dphi) {}
     ~SphericalSystem() = default;
 
+    // * * * * * * * * * * * * * * *
+    // 2D:
+    // * * * * * * * * * * * * * * *
     // coordinate transformations
     // conversion from code units (CU) to cartesian (Cart)
-    // Inline auto coord_CU_to_Cart(const real_t& x1) const -> real_t override { return x1 * dx + this->x1_min; }
     Inline auto coord_CU_to_Cart(const real_t& x1, const real_t& x2) const -> std::tuple<real_t, real_t> override {
       auto r {x1 * dr + this->x1_min};
-      auto theta {x2 * dtheta + this->x2_min};
+      auto theta {x2 * dtheta};
       return {r * std::sin(theta), r * std::cos(theta)};
     }
+
+    // // conversion from cartesian (Cart) to code units (CU)
+    // Inline auto coord_CART_to_CU(const real_t& x, const real_t& y) const -> std::tuple<real_t, real_t> override {
+    //   return {x, y};
+    // }
+
+    // conversion to spherical
+    Inline auto coord_CU_to_Sph(const real_t& x1, const real_t& x2) const -> std::tuple<real_t, real_t> {
+      return {x1 * dr + this->x1_min, x2 * dtheta};
+    }
+
+    // metric coefficients
+    Inline auto h11(const real_t&, const real_t&) const -> real_t { return dr_sqr; }
+
+    Inline auto h22(const real_t& x1, const real_t&) const -> real_t {
+      auto r {x1 * dr + this->x1_min};
+      return dtheta_sqr * r * r;
+    }
+
+    Inline auto h33(const real_t& x1, const real_t& x2) const -> real_t {
+      auto r {x1 * dr + this->x1_min};
+      auto theta {x2 * dtheta};
+      real_t sin_theta {std::sin(theta)};
+      return r * r * sin_theta * sin_theta;
+    }
+
+    // det of metric
+    Inline auto sqrt_det_h(const real_t& x1, const real_t& x2) const -> real_t {
+      auto r {x1 * dr + this->x1_min};
+      auto theta {x2 * dtheta};
+      return dr * dtheta * r * r * std::sin(theta);
+    }
+
+    // area at poles
+    Inline auto polar_area(const real_t& x1, const real_t& x2) const -> real_t {
+      auto r {x1 * dr + this->x1_min};
+      auto del_theta {x2 * dtheta};
+      return r * r * (ONE - std::cos(del_theta));
+    }
+
+    // * * * * * * * * * * * * * * *
+    // 3D:
+    // * * * * * * * * * * * * * * *
+    // coordinate transformations
+    // conversion from code units (CU) to cartesian (Cart)
     Inline auto coord_CU_to_Cart(const real_t& x1, const real_t& x2, const real_t& x3) const
       -> std::tuple<real_t, real_t, real_t> override {
       auto r {x1 * dr + this->x1_min};
-      auto theta {x2 * dtheta + this->x2_min};
-      auto phi {x3 * dphi + this->x3_min};
+      auto theta {x2 * dtheta};
+      auto phi {x3 * dphi};
       return {r * std::sin(theta) * std::cos(phi), r * std::sin(theta) * std::sin(phi), r * std::cos(theta)};
     }
 
     // // conversion from cartesian (Cart) to code units (CU)
-    // Inline auto coord_CART_to_CU(const real_t&) const -> real_t override {
-    //   assert(false);
-    //   return -1.0;
-    // }
-    // Inline auto coord_CART_to_CU(const real_t& x, const real_t& y) const -> std::tuple<real_t, real_t> override {
-    //   return {x, y};
-    // }
     // Inline auto coord_CART_to_CU(const real_t& x, const real_t& y, const real_t& z) const
     //     -> std::tuple<real_t, real_t, real_t> override {
     //   return {x, y, z};
     // }
 
     // conversion to spherical
-    Inline auto coord_CU_to_Sph(const real_t& x1, const real_t& x2) const -> std::tuple<real_t, real_t> {
-      return {x1 * dr + this->x1_min, x2 * dtheta + this->x2_min};
-    }
     Inline auto coord_CU_to_Sph(const real_t& x1, const real_t& x2, const real_t& x3) const
       -> std::tuple<real_t, real_t, real_t> {
-      return {x1 * dr + this->x1_min, x2 * dtheta + this->x2_min, x3 * dphi + this->x3_min};
+      return {x1 * dr + this->x1_min, x2 * dtheta, x3 * dphi};
     }
-
-    // // vector transformations
-    // // ... curvilinear -> cartesian
-    // Inline auto transform_ux1TOux(const real_t&) const -> real_t override {
-    //   assert(false);
-    //   return -1.0;
-    // }
-    // Inline auto transform_ux1ux2TOuxuy(const real_t& ux1, const real_t& ux2) const -> std::tuple<real_t, real_t>
-    // override {
-    //   return {ux1, ux2};
-    // }
-    // Inline auto transform_ux1ux2ux3TOuxuyuz(const real_t& ux1, const real_t& ux2, const real_t& ux3) const ->
-    // std::tuple<real_t, real_t, real_t> override {
-    //   return {ux1, ux2, ux3};
-    // }
-
-    // // ... cartesian -> curvilinear
-    // Inline auto transform_uxTOux1(const real_t&) const -> real_t override {
-    //   assert(false);
-    //   return -1.0;
-    // }
-    // Inline auto transform_uxuyTOux1ux2(const real_t& ux, const real_t& uy) const -> std::tuple<real_t, real_t>
-    // override {
-    //   return {ux, uy};
-    // }
-    // Inline auto transform_uxuyuzTOux1ux2ux3(const real_t& ux, const real_t& uy, const real_t& uz) const ->
-    // std::tuple<real_t, real_t, real_t> override {
-    //   return {ux, uy, uz};
-    // }
 
     // metric coefficients
-    // Inline auto h11(const real_t&) const -> real_t {
-    //   assert(false);
-    //   return -1.0;
-    // }
-    Inline auto h11(const real_t&, const real_t&) const -> real_t { return dr_sqr; }
     Inline auto h11(const real_t&, const real_t&, const real_t&) const -> real_t { return dr_sqr; }
-
-    // Inline auto h22(const real_t&) const -> real_t {
-    //   assert(false);
-    //   return -1.0;
-    // }
-    Inline auto h22(const real_t& x1, const real_t&) const -> real_t {
-      auto r {x1 * dr + this->x1_min};
-      return dtheta_sqr * r * r;
-    }
     Inline auto h22(const real_t& x1, const real_t&, const real_t&) const -> real_t {
       auto r {x1 * dr + this->x1_min};
       return dtheta_sqr * r * r;
     }
-
-    // Inline auto h33(const real_t&) const -> real_t {
-    //   assert(false);
-    //   return -1.0;
-    // }
-    Inline auto h33(const real_t& x1, const real_t& x2) const -> real_t {
-      auto r {x1 * dr + this->x1_min};
-      auto theta {x2 * dtheta + this->x2_min};
-      real_t sin_theta {std::sin(theta)};
-      return r * r * sin_theta * sin_theta;
-    }
     Inline auto h33(const real_t& x1, const real_t& x2, const real_t&) const -> real_t {
       auto r {x1 * dr + this->x1_min};
-      auto theta {x2 * dtheta + this->x2_min};
+      auto theta {x2 * dtheta};
       auto sin_theta {std::sin(theta)};
       return dphi_sqr * r * r * sin_theta * sin_theta;
     }
 
     // det of metric
-    // Inline auto sqrt_det_h(const real_t&) const -> real_t {
-    //   assert(false);
-    //   return -1.0;
-    // }
-    Inline auto sqrt_det_h(const real_t& x1, const real_t& x2) const -> real_t {
-      auto r {x1 * dr + this->x1_min};
-      auto theta {x2 * dtheta + this->x2_min};
-      return dr * dtheta * r * r * std::sin(theta);
-    }
     Inline auto sqrt_det_h(const real_t& x1, const real_t& x2, const real_t&) const -> real_t {
       auto r {x1 * dr + this->x1_min};
-      auto theta {x2 * dtheta + this->x2_min};
+      auto theta {x2 * dtheta};
       return dr * dtheta * dphi * r * r * std::sin(theta);
     }
 
     // area at poles
-    Inline auto polar_area(const real_t& x1, const real_t& x2) const -> real_t {
-      auto r {x1 * dr + this->x1_min};
-      auto del_theta {x2 * dtheta + this->x2_min};
-      return r * r * (ONE - std::cos(del_theta));
-    }
     Inline auto polar_area(const real_t& x1, const real_t& x2, const real_t&) const -> real_t {
       auto r {x1 * dr + this->x1_min};
-      auto del_theta {x2 * dtheta + this->x2_min};
-      return dphi * r * r * (ONE - std::cos(del_theta));
+      auto del_theta {x2 * dtheta};
+      return r * r * (ONE - std::cos(del_theta));
     }
   };
 } // namespace ntt
