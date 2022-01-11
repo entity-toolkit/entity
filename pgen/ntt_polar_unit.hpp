01d8981935759d8902bd8a50cfb3b86d4c4bc1f0
haykh <haykh.mfs+github@gmail.com>
2022-01-11 17:50:56 -0500

    absorb working
    Former-commit-id: 373250fe825cc2c601f52920eedb04ae88075c60
    Former-commit-id: d8fa72c1c41b19cad0f058303d63f8b0309d5312

--- pgen/ntt_polar_unit.hpp
+++ pgen/ntt_polar_unit.hpp
@@ -1,39 +1,3 @@
-// #ifndef PROBLEM_GENERATOR_H
-// #define PROBLEM_GENERATOR_H
-
-// #include "global.h"
-// #include "pgen.h"
-// #include "sim_params.h"
-// #include "meshblock.h"
-
-// namespace ntt {
-
-//   template <Dimension D>
-//   struct ProblemGenerator : PGen<D> {
-//     ProblemGenerator(SimulationParams&);
-//     ~ProblemGenerator() = default;
-
-//     void userInitFields(SimulationParams&, Meshblock<D>&);
-//     void userBCFields(const real_t&, SimulationParams&, Meshblock<D>&);
-
-//     Inline auto userTargetField_br_HAT(Meshblock<D>&, const real_t&) const -> real_t;
-//     Inline auto userTargetField_br_HAT(Meshblock<D>&, const real_t&, const real_t&) const -> real_t;
-//     Inline auto userTargetField_br_HAT(Meshblock<D>&, const real_t&, const real_t&, const real_t&) const -> real_t;
-//   };
-
-//   template <>
-//   Inline auto ProblemGenerator<TWO_D>::userTargetField_br_HAT(Meshblock<TWO_D>& mblock,
-//                                                               const real_t& x1,
-//                                                               const real_t& x2) const -> real_t {
-//     auto [r_, th_] = mblock.grid->coord_CU_to_Sph(x1, x2);
-//     auto r_min {mblock.grid->x1_min};
-//     return ONE * r_min * r_min / (r_ * r_);
-//   }
-
-// } // namespace ntt
-
-// #endif
-
 #ifndef PROBLEM_GENERATOR_H
 #define PROBLEM_GENERATOR_H
 
@@ -50,6 +14,13 @@ namespace ntt {
 
     void userInitFields(const SimulationParams&, Meshblock<D, S>&) override;
     void userBCFields(const real_t&, const SimulationParams&, Meshblock<D, S>&) override;
+
+    Inline auto userTargetField_br_hat(const Meshblock<D, S>& mblock, const coord_t<D>& x) const -> real_t override {
+      coord_t<D> rth_;
+      real_t r_min {mblock.metric->x1_min};
+      mblock.metric->x_Code2Sph(x, rth_);
+      return ONE * r_min * r_min / (rth_[0] * rth_[0]);
+    }
   };
 
 } // namespace ntt
