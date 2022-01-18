aa516add6425d7f59cb7c174ad23025b3e721100
haykh <haykh.mfs+github@gmail.com>
2022-01-18 00:31:09 -0500

    started grpic
    Former-commit-id: 4d0c50c2e564d81cad1e2b3f1f3708456a62d9b8
    Former-commit-id: a9e6a34df8edf700e86397e798eac5203f67ba76

---
+++ src/grpic/grpic.cpp
@@ -0,0 +1,31 @@
+#if SIMTYPE == GRPIC_SIMTYPE
+
+#  include "global.h"
+#  include "timer.h"
+#  include "grpic.h"
+
+namespace ntt {
+
+  template <Dimension D>
+  void GRPIC<D>::mainloop() {
+  }
+
+  template <Dimension D>
+  void GRPIC<D>::process() {
+  }
+
+  template <Dimension D>
+  void GRPIC<D>::step_forward(const real_t& time) {
+  }
+
+  template <Dimension D>
+  void GRPIC<D>::step_backward(const real_t& time) {
+
+  }
+} // namespace ntt
+
+template class ntt::GRPIC<ntt::Dimension::ONE_D>;
+template class ntt::GRPIC<ntt::Dimension::TWO_D>;
+template class ntt::GRPIC<ntt::Dimension::THREE_D>;
+
+#endif
