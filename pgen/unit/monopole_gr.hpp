78fb460559be5123ed316f274702ea903708155d
bcrinquand <benjamin.crinquand@univ-grenoble-alpes.fr>
2022-03-11 10:47:15 -0500

    Cleaned absorbing BC.

--- pgen/unit/monopole_gr.hpp
+++ pgen/unit/monopole_gr.hpp
@@ -41,13 +41,6 @@ namespace ntt {
       return ONE - std::cos(rth_[1]);
     }
 
-    Inline auto userTargetField_br_hat(const Meshblock<D, S>& mblock, const coord_t<D>& x) const -> real_t {
-      coord_t<D> rth_;
-      real_t r_min {mblock.metric.x1_min};
-      mblock.metric.x_Code2Sph(x, rth_);
-      return ZERO;
-      }
-
     Inline auto userTargetField_br_cntrv(const Meshblock<D, S>& mblock, const coord_t<D>& x) const -> real_t {
       coord_t<D> x0m, x0p;
       real_t inv_sqrt_detH_ijP  {ONE / mblock.metric.sqrt_det_h(x)};
--- pgen/unit/vertical_gr.hpp
+++ pgen/unit/vertical_gr.hpp
@@ -41,12 +41,6 @@ namespace ntt {
       return HALF * std::sin(rth_[1]) * std::sin(rth_[1]) * rth_[0] * rth_[0];
     }
 
-    Inline auto userTargetField_br_hat(const Meshblock<D, S>& mblock, const coord_t<D>& x) const -> real_t {
-      coord_t<D> rth_;
-      mblock.metric.x_Code2Sph(x, rth_);
-      return ONE * std::cos(rth_[1]); // Vertical field at infinity
-    }
-
     Inline auto userTargetField_br_cntrv(const Meshblock<D, S>& mblock, const coord_t<D>& x) const -> real_t {
       coord_t<D> x0m, x0p;
       real_t inv_sqrt_detH_ijP  {ONE / mblock.metric.sqrt_det_h(x)};
@@ -60,7 +54,11 @@ namespace ntt {
       real_t inv_sqrt_detH_iPj  {ONE / mblock.metric.sqrt_det_h(x)};
       x0m[0] = x[0] + HALF - HALF * epsilon, x0m[1] = x[1];
       x0p[0] = x[0] + HALF + HALF * epsilon, x0p[1] = x[1];
+      if (x[1] == ZERO) {
+      return ZERO;
+      } else {
       return - (A3(mblock, x0p) - A3(mblock, x0m)) * inv_sqrt_detH_iPj / epsilon;
+      }
     }
 
   };
