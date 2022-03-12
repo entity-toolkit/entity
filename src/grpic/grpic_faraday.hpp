aea5695cdcc8baf19d005571346deaca553c51af
bcrinquand <benjamin.crinquand@univ-grenoble-alpes.fr>
2022-03-12 11:59:57 -0500

    Corrected typos.

--- src/grpic/grpic_faraday.hpp
+++ src/grpic/grpic_faraday.hpp
@@ -51,13 +51,6 @@ namespace ntt {
     m_mblock.em0(i, j, em::bx3) += m_coeff * inv_sqrt_detH_iPjP
                                 * (m_mblock.aux(i, j + 1, em::ex1) - m_mblock.aux(i, j, em::ex1)
                                 + m_mblock.aux(i, j, em::ex2) - m_mblock.aux(i + 1, j, em::ex2));  
-  
-  if ((j<=6) && (i<=6)) {
-  printf("%f %f %f %lu %lu\n",m_mblock.em0(i, j, em::bx3), 
-  m_mblock.metric.sqrt_det_h({i_ + HALF, j_ + HALF}), m_mblock.metric.polar_area({i_ + HALF, j_ + HALF}), i, j);
-  //m_mblock.aux(i, j + 1, em::ex1), m_mblock.aux(i, j, em::ex1), i, j);
-  }
-
   }
 
   template <>
