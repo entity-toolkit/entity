342644c85d8b0ceff1eeeb9fcd1f5da3eadcbdc0
haykh <haykh.mfs+github@gmail.com>
2022-03-23 12:30:22 -0400

    minor restruct
    Former-commit-id: 92efeae1fcf6efb0bbf37c83611878757bd6edc5
    Former-commit-id: da8985401efa87db34dfc7cb10569f07b4d4a27d

--- src/framework/metrics/kerr_schild.h
+++ src/framework/metrics/kerr_schild.h
@@ -143,8 +143,7 @@ namespace ntt {
     Inline auto polar_area(const coord_t<D>& x) const -> real_t {
       real_t r {x[0] * dr + this->x1_min};
       real_t del_theta {x[1] * dtheta};
-      return dtheta * std::sqrt((r * r + a * a) * (ONE + TWO * r / ( r * r + a * a))) * (ONE - std::cos(del_theta));
-      // return dtheta * sqrt_det_h_tilde({x[0], ZERO}) * (ONE - std::cos(del_theta));
+      return dtheta * (r * r + a * a) * std::sqrt(ONE + TWO * r / (r * r + a * a)) * (ONE - std::cos(del_theta));
     }
 
 /**
--- src/grpic/grpic_faraday.hpp
+++ src/grpic/grpic_faraday.hpp
@@ -11,12 +11,12 @@
 namespace ntt {
 
   /**
-   * Algorithms for Faraday's law: `dB/dt = -curl E`.
+   * Algorithms for Faraday's law.
    *
    * @tparam D Dimension.
    */
   template <Dimension D>
-  class Faraday_push0{
+  class Faraday_push0 {
     using index_t = typename RealFieldND<D, 6>::size_type;
     Meshblock<D, SimulationType::GRPIC> m_mblock;
     real_t m_coeff;
@@ -35,27 +35,26 @@ namespace ntt {
     real_t j_ {static_cast<real_t>(j - N_GHOSTS)};
     index_t j_min {static_cast<index_t>(m_mblock.j_min())};
 
-    real_t inv_sqrt_detH_iPj  {ONE / m_mblock.metric.sqrt_det_h({i_ + HALF, j_})};
-    real_t inv_sqrt_detH_ijP  {ONE / m_mblock.metric.sqrt_det_h({i_, j_ + HALF})};
+    real_t inv_sqrt_detH_iPj {ONE / m_mblock.metric.sqrt_det_h({i_ + HALF, j_})};
+    real_t inv_sqrt_detH_ijP {ONE / m_mblock.metric.sqrt_det_h({i_, j_ + HALF})};
     real_t inv_sqrt_detH_iPjP {ONE / m_mblock.metric.sqrt_det_h({i_ + HALF, j_ + HALF})};
-   
+
     m_mblock.em0(i, j, em::bx1)
       += m_coeff * inv_sqrt_detH_ijP * (m_mblock.aux(i, j, em::ex3) - m_mblock.aux(i, j + 1, em::ex3));
 
     if (j == j_min) {
-    m_mblock.em0(i, j, em::bx2) = ZERO;
+      m_mblock.em0(i, j, em::bx2) = ZERO;
     } else {
-    m_mblock.em0(i, j, em::bx2)
-      += m_coeff * inv_sqrt_detH_iPj * (m_mblock.aux(i + 1, j, em::ex3) - m_mblock.aux(i, j, em::ex3));
+      m_mblock.em0(i, j, em::bx2)
+        += m_coeff * inv_sqrt_detH_iPj * (m_mblock.aux(i + 1, j, em::ex3) - m_mblock.aux(i, j, em::ex3));
     }
     m_mblock.em0(i, j, em::bx3) += m_coeff * inv_sqrt_detH_iPjP
-                                * (m_mblock.aux(i, j + 1, em::ex1) - m_mblock.aux(i, j, em::ex1)
-                                + m_mblock.aux(i, j, em::ex2) - m_mblock.aux(i + 1, j, em::ex2));  
+                                   * (m_mblock.aux(i, j + 1, em::ex1) - m_mblock.aux(i, j, em::ex1)
+                                      + m_mblock.aux(i, j, em::ex2) - m_mblock.aux(i + 1, j, em::ex2));
   }
 
   template <>
-  Inline void
-  Faraday_push0<Dimension::THREE_D>::operator()(const index_t, const index_t, const index_t) const {
+  Inline void Faraday_push0<Dimension::THREE_D>::operator()(const index_t, const index_t, const index_t) const {
     // 3d curvilinear faraday not implemented
   }
 
@@ -79,29 +78,30 @@ namespace ntt {
     real_t j_ {static_cast<real_t>(j - N_GHOSTS)};
     index_t j_min {static_cast<index_t>(m_mblock.j_min())};
 
-    real_t inv_sqrt_detH_iPj  {ONE / m_mblock.metric.sqrt_det_h({i_ + HALF, j_})};
-    real_t inv_sqrt_detH_ijP  {ONE / m_mblock.metric.sqrt_det_h({i_, j_ + HALF})};
+    real_t inv_sqrt_detH_iPj {ONE / m_mblock.metric.sqrt_det_h({i_ + HALF, j_})};
+    real_t inv_sqrt_detH_ijP {ONE / m_mblock.metric.sqrt_det_h({i_, j_ + HALF})};
     real_t inv_sqrt_detH_iPjP {ONE / m_mblock.metric.sqrt_det_h({i_ + HALF, j_ + HALF})};
-   
-    m_mblock.em0(i, j, em::bx1) = m_mblock.em(i, j, em::bx1) + 
-                                  m_coeff * inv_sqrt_detH_ijP * (m_mblock.aux(i, j, em::ex3) - m_mblock.aux(i, j + 1, em::ex3));
+
+    m_mblock.em0(i, j, em::bx1)
+      = m_mblock.em(i, j, em::bx1)
+        + m_coeff * inv_sqrt_detH_ijP * (m_mblock.aux(i, j, em::ex3) - m_mblock.aux(i, j + 1, em::ex3));
 
     if (j == j_min) {
-    m_mblock.em0(i, j, em::bx2) = ZERO;
+      m_mblock.em0(i, j, em::bx2) = ZERO;
     } else {
-    m_mblock.em0(i, j, em::bx2) = m_mblock.em(i, j, em::bx2) + 
-                                  m_coeff * inv_sqrt_detH_iPj * (m_mblock.aux(i + 1, j, em::ex3) - m_mblock.aux(i, j, em::ex3));
+      m_mblock.em0(i, j, em::bx2)
+        = m_mblock.em(i, j, em::bx2)
+          + m_coeff * inv_sqrt_detH_iPj * (m_mblock.aux(i + 1, j, em::ex3) - m_mblock.aux(i, j, em::ex3));
     }
 
-    m_mblock.em0(i, j, em::bx3) = m_mblock.em(i, j, em::bx3) 
-                                  +  m_coeff * inv_sqrt_detH_iPjP
-                                  * (m_mblock.aux(i, j + 1, em::ex1) - m_mblock.aux(i, j, em::ex1)
-                                  +  m_mblock.aux(i, j, em::ex2) - m_mblock.aux(i + 1, j, em::ex2));
-    }
+    m_mblock.em0(i, j, em::bx3) = m_mblock.em(i, j, em::bx3)
+                                  + m_coeff * inv_sqrt_detH_iPjP
+                                      * (m_mblock.aux(i, j + 1, em::ex1) - m_mblock.aux(i, j, em::ex1)
+                                         + m_mblock.aux(i, j, em::ex2) - m_mblock.aux(i + 1, j, em::ex2));
+  }
 
   template <>
-  Inline void
-  Faraday_push<Dimension::THREE_D>::operator()(const index_t, const index_t, const index_t) const {
+  Inline void Faraday_push<Dimension::THREE_D>::operator()(const index_t, const index_t, const index_t) const {
     // 3d curvilinear faraday not implemented
   }
 
