5a2f6a5a8c4ff48dd9f5db0a14f31105ac69afdc
Hayk Hakobyan <haykh.astro@gmail.com>
2022-03-23 20:28:10 -0400

    bugs fixed, no compile error, NOT TESTED
    Former-commit-id: bf5a0b11ea5a63351175cf17c4d9acf888d58f32
    Former-commit-id: 4ddab30d3c5b30891320f5a7adf096d597814a86

--- src/framework/aux/init_fields.hpp
+++ src/framework/aux/init_fields.hpp
@@ -10,13 +10,12 @@
 namespace ntt {
 
   /**
-   * Computes D and B from A3, A1, A0 
+   * Computes D and B from A3, A1, A0
    *
    * @tparam D Dimension.
    */
-
   template <Dimension D>
-    class init_fields_potential {
+  class init_fields_potential {
     using index_t = typename RealFieldND<D, 6>::size_type;
     Meshblock<D, SimulationType::GRPIC> m_mblock;
     real_t m_eps;
@@ -25,13 +24,12 @@ namespace ntt {
     real_t (*m_a3)(const Meshblock<D, SimulationType::GRPIC>&, const coord_t<D>& x);
 
   public:
-    init_fields_potential(
-    const Meshblock<D, SimulationType::GRPIC>& mblock,
-    real_t eps,
-    real_t (*a0)(const Meshblock<D, SimulationType::GRPIC>&, const coord_t<D>& x),
-    real_t (*a1)(const Meshblock<D, SimulationType::GRPIC>&, const coord_t<D>& x),
-    real_t (*a3)(const Meshblock<D, SimulationType::GRPIC>&, const coord_t<D>& x)
-    ): m_mblock(mblock), m_eps(eps), m_a0(a0), m_a1(a1), m_a3(a3) {}
+    init_fields_potential(const Meshblock<D, SimulationType::GRPIC>& mblock,
+                          real_t eps,
+                          real_t (*a0)(const Meshblock<D, SimulationType::GRPIC>&, const coord_t<D>& x),
+                          real_t (*a1)(const Meshblock<D, SimulationType::GRPIC>&, const coord_t<D>& x),
+                          real_t (*a3)(const Meshblock<D, SimulationType::GRPIC>&, const coord_t<D>& x))
+      : m_mblock(mblock), m_eps(eps), m_a0(a0), m_a1(a1), m_a3(a3) {}
 
     Inline void operator()(const index_t, const index_t) const;
   };
@@ -43,60 +41,60 @@ namespace ntt {
     index_t j_min {static_cast<index_t>(m_mblock.j_min())};
     coord_t<Dimension::TWO_D> x0m, x0p;
 
-    real_t inv_sqrt_detH_ij   {ONE / m_mblock.metric.sqrt_det_h({i_, j_})};
-    real_t inv_sqrt_detH_ijP  {ONE / m_mblock.metric.sqrt_det_h({i_, j_ + HALF})};
-    real_t inv_sqrt_detH_iPj  {ONE / m_mblock.metric.sqrt_det_h({i_ + HALF, j_})};
+    real_t inv_sqrt_detH_ij {ONE / m_mblock.metric.sqrt_det_h({i_, j_})};
+    real_t inv_sqrt_detH_ijP {ONE / m_mblock.metric.sqrt_det_h({i_, j_ + HALF})};
+    real_t inv_sqrt_detH_iPj {ONE / m_mblock.metric.sqrt_det_h({i_ + HALF, j_})};
     real_t inv_sqrt_detH_iPjP {ONE / m_mblock.metric.sqrt_det_h({i_ + HALF, j_ + HALF})};
-    real_t sqrt_detH_ij  {m_mblock.metric.sqrt_det_h({i_, j_})};
+    real_t sqrt_detH_ij {m_mblock.metric.sqrt_det_h({i_, j_})};
     real_t sqrt_detH_ijP {m_mblock.metric.sqrt_det_h({i_, j_ + HALF})};
-    real_t alpha_ij   {m_mblock.metric.alpha({i_, j_})};
-    real_t alpha_ijP  {m_mblock.metric.alpha({i_, j_ + HALF})};
-    real_t alpha_iPj  {m_mblock.metric.alpha({i_ + HALF, j_})};
-    real_t beta_ij   {m_mblock.metric.beta1u({i_, j_})};
-    real_t beta_ijP  {m_mblock.metric.beta1u({i_, j_ + HALF})};
+    real_t alpha_ij {m_mblock.metric.alpha({i_, j_})};
+    real_t alpha_ijP {m_mblock.metric.alpha({i_, j_ + HALF})};
+    real_t alpha_iPj {m_mblock.metric.alpha({i_ + HALF, j_})};
+    real_t beta_ij {m_mblock.metric.beta1u({i_, j_})};
+    real_t beta_ijP {m_mblock.metric.beta1u({i_, j_ + HALF})};
     real_t h_11_inv_iPj {m_mblock.metric.h_11_inv({i_ + HALF, j_})};
     real_t h_13_inv_iPj {m_mblock.metric.h_13_inv({i_ + HALF, j_})};
     real_t h_22_inv_ijP {m_mblock.metric.h_22_inv({i_, j_ + HALF})};
-    real_t h_33_inv_ij  {m_mblock.metric.h_33_inv({i_, j_})};
-    real_t h_13_inv_ij  {m_mblock.metric.h_13_inv({i_, j_})};
+    real_t h_33_inv_ij {m_mblock.metric.h_33_inv({i_, j_})};
+    real_t h_13_inv_ij {m_mblock.metric.h_13_inv({i_, j_})};
 
     x0m[0] = i_, x0m[1] = j_ + HALF - HALF * m_eps;
     x0p[0] = i_, x0p[1] = j_ + HALF + HALF * m_eps;
 
-    real_t E2d    {  (m_a0(m_mblock, x0p) - m_a0(m_mblock, x0m)) / m_eps};
-    real_t B1u    {  (m_a3(m_mblock, x0p) - m_a3(m_mblock, x0m)) * inv_sqrt_detH_ijP / m_eps};
-    real_t B3_aux {- (m_a1(m_mblock, x0p) - m_a1(m_mblock, x0m)) * inv_sqrt_detH_ijP / m_eps};
+    real_t E2d {(m_a0(m_mblock, x0p) - m_a0(m_mblock, x0m)) / m_eps};
+    real_t B1u {(m_a3(m_mblock, x0p) - m_a3(m_mblock, x0m)) * inv_sqrt_detH_ijP / m_eps};
+    real_t B3_aux {-(m_a1(m_mblock, x0p) - m_a1(m_mblock, x0m)) * inv_sqrt_detH_ijP / m_eps};
 
     x0m[0] = i_ + HALF - HALF * m_eps, x0m[1] = j_;
     x0p[0] = i_ + HALF + HALF * m_eps, x0p[1] = j_;
 
     real_t B2u;
     if (j == j_min) {
-    B2u = ZERO;
+      B2u = ZERO;
     } else {
-    B2u = - (m_a3(m_mblock, x0p) - m_a3(m_mblock, x0m)) * inv_sqrt_detH_iPj / m_eps;
+      B2u = -(m_a3(m_mblock, x0p) - m_a3(m_mblock, x0m)) * inv_sqrt_detH_iPj / m_eps;
     }
     real_t E1d {(m_a0(m_mblock, x0p) - m_a0(m_mblock, x0m)) / m_eps};
 
     x0m[0] = i_ + HALF, x0m[1] = j_ + HALF - HALF * m_eps;
     x0p[0] = i_ + HALF, x0p[1] = j_ + HALF + HALF * m_eps;
-    
-    real_t B3u {- (m_a1(m_mblock, x0p) - m_a1(m_mblock, x0m)) * inv_sqrt_detH_iPjP / m_eps};
+
+    real_t B3u {-(m_a1(m_mblock, x0p) - m_a1(m_mblock, x0m)) * inv_sqrt_detH_iPjP / m_eps};
 
     x0m[0] = i_ - HALF * m_eps, x0m[1] = j_;
     x0p[0] = i_ + HALF * m_eps, x0p[1] = j_;
 
     real_t B2_aux;
     if (j == j_min) {
-    B2_aux = ZERO;
+      B2_aux = ZERO;
     } else {
-    B2_aux = - (m_a3(m_mblock, x0p) - m_a3(m_mblock, x0m)) * inv_sqrt_detH_ij / m_eps;
+      B2_aux = -(m_a3(m_mblock, x0p) - m_a3(m_mblock, x0m)) * inv_sqrt_detH_ij / m_eps;
     }
 
     // Compute covariant D
     real_t D1d {E1d / alpha_iPj};
     real_t D2d {E2d / alpha_ijP + sqrt_detH_ijP * beta_ijP * B3_aux / alpha_ijP};
-    real_t D3d {- sqrt_detH_ij * beta_ij * B2_aux / alpha_ij};
+    real_t D3d {-sqrt_detH_ij * beta_ij * B2_aux / alpha_ij};
 
     // Covariant D to contravariant D
     real_t D1u {h_11_inv_iPj * D1d + h_13_inv_iPj * D3d};
@@ -105,18 +103,10 @@ namespace ntt {
 
     // h33_inv is singular at theta = 0.
     if (j == j_min) {
-    D3u = ZERO;
+      D3u = ZERO;
     } else {
-    D3u = h_33_inv_ij * D3d + h_13_inv_ij * D1d;
+      D3u = h_33_inv_ij * D3d + h_13_inv_ij * D1d;
     }
-
-    m_mblock.em0(i, j, em::bx1) = B1u;
-    m_mblock.em0(i, j, em::bx2) = B2u;
-    m_mblock.em0(i, j, em::bx3) = B3u;
-    m_mblock.em0(i, j, em::ex1) = D1u;
-    m_mblock.em0(i, j, em::ex2) = D2u;
-    m_mblock.em0(i, j, em::ex3) = D3u;
-
     m_mblock.em(i, j, em::bx1) = B1u;
     m_mblock.em(i, j, em::bx2) = B2u;
     m_mblock.em(i, j, em::bx3) = B3u;
@@ -132,16 +122,13 @@ namespace ntt {
    */
 
   template <Dimension D>
-    class Compute_Aphi {
+  class Compute_Aphi {
     using index_t = typename RealFieldND<D, 6>::size_type;
     Meshblock<D, SimulationType::GRPIC> m_mblock;
     real_t m_eps;
 
   public:
-    Compute_Aphi(
-    const Meshblock<D, SimulationType::GRPIC>& mblock,
-    real_t eps
-    ): m_mblock(mblock), m_eps(eps) {}
+    Compute_Aphi(const Meshblock<D, SimulationType::GRPIC>& mblock, real_t eps) : m_mblock(mblock), m_eps(eps) {}
 
     Inline void operator()(const index_t, const index_t) const;
   };
@@ -151,17 +138,15 @@ namespace ntt {
     real_t i_ {static_cast<real_t>(i - N_GHOSTS)};
     real_t j_ {static_cast<real_t>(j - N_GHOSTS)};
 
-    Kokkos::parallel_for("compute_aphi",
-    NTTRange<Dimension::ONE_D>({m_mblock.j_min() + 1}, {(int)j_}), 
-    Lambda(index_t k_) {
+    Kokkos::parallel_for(
+      "compute_aphi", NTTRange<Dimension::ONE_D>({m_mblock.j_min() + 1}, {(int)j_}), Lambda(index_t k_) {
         real_t sqrt_detH_ij1 {m_mblock.metric.sqrt_det_h({i_, (real_t)k_ - HALF})};
         real_t sqrt_detH_ij2 {m_mblock.metric.sqrt_det_h({i_, (real_t)k_ + HALF})};
         index_t k {k_ + N_GHOSTS};
-        m_mblock.aphi(i, j, 1) += HALF * (sqrt_detH_ij1 * m_mblock.em(i, k - 1, em::bx1) + sqrt_detH_ij2 * m_mblock.em(i, k, em::bx1));   
-                      }
-    );
+        m_mblock.aphi(i, j, 1)
+          += HALF * (sqrt_detH_ij1 * m_mblock.em(i, k - 1, em::bx1) + sqrt_detH_ij2 * m_mblock.em(i, k, em::bx1));
+      });
   }
-
 
 } // namespace ntt
 
