dc887edea0c380281007b63f166f6bbf7fbbf251
haykh <haykh.mfs+github@gmail.com>
2022-04-05 09:18:00 -0400

    bc working ok
    Former-commit-id: 395e9214b96d09b2d579328e46526b00466df792
    Former-commit-id: 0862e5dc9da66a34f7d21c1e424cab522e84036d

--- src/grpic/grpic_fields_bc.cpp
+++ src/grpic/grpic_fields_bc.cpp
@@ -18,24 +18,24 @@ namespace ntt {
       Kokkos::parallel_for(
         "2d_bc_rmin", NTTRange<Dimension::ONE_D>({mblock.j_min()}, {mblock.j_max() + 1}), Lambda(index_t j) {
           mblock.em0(i_min - 1, j, em::ex1) = mblock.em0(i_min, j, em::ex1);
-          // mblock.em0(i_min, j, em::ex2) = mblock.em0(i_min + 1, j, em::ex2);
+          mblock.em0(i_min, j, em::ex2) = mblock.em0(i_min + 1, j, em::ex2);
           mblock.em0(i_min - 1, j, em::ex2) = mblock.em0(i_min, j, em::ex2);
           mblock.em0(i_min, j, em::ex3) = mblock.em0(i_min + 1, j, em::ex3);
           mblock.em0(i_min - 1, j, em::ex3) = mblock.em0(i_min, j, em::ex3);
 
           mblock.em(i_min - 1, j, em::ex1) = mblock.em(i_min, j, em::ex1);
-          // mblock.em(i_min, j, em::ex2) = mblock.em(i_min + 1, j, em::ex2);
+          mblock.em(i_min, j, em::ex2) = mblock.em(i_min + 1, j, em::ex2);
           mblock.em(i_min - 1, j, em::ex2) = mblock.em(i_min, j, em::ex2);
           mblock.em(i_min, j, em::ex3) = mblock.em(i_min + 1, j, em::ex3);
           mblock.em(i_min - 1, j, em::ex3) = mblock.em(i_min, j, em::ex3);
         });
       // Absorbing boundary
       auto r_absorb {m_sim_params.metric_parameters()[2]};
+      auto absorb_coeff {m_sim_params.metric_parameters()[3]};
+      auto absorb_norm {ONE / (ONE - std::exp(absorb_coeff))};
       auto r_max {m_mblock.metric.x1_max};
       auto pGen {this->m_pGen};
       Kokkos::parallel_for(
-        // !TEMP:
-        // "2d_absorbing bc", m_mblock.loopActiveCells(), Lambda(index_t i, index_t j) {
         "2d_absorbing bc",
         NTTRange<Dimension::TWO_D>({mblock.i_min(), mblock.j_min()}, {mblock.i_max() + 1, mblock.j_max() + 1}),
         Lambda(index_t i, index_t j) {
@@ -45,40 +45,35 @@ namespace ntt {
           // i
           vec_t<Dimension::TWO_D> rth_;
           mblock.metric.x_Code2Sph({i_, j_}, rth_);
-          real_t delta_r1 {(rth_[0] - r_absorb) / (r_max - r_absorb)};
-          // !TEMP:
-          // real_t sigma_r1 {ONE - std::exp(-5.0 * HEAVISIDE(delta_r1)*delta_r1 * delta_r1 * delta_r1)};
-          real_t sigma_r1 {HEAVISIDE(delta_r1) * delta_r1 * delta_r1 * delta_r1};
+          if (rth_[0] > r_absorb) {
+            real_t delta_r1 {(rth_[0] - r_absorb) / (r_max - r_absorb)};
+            real_t sigma_r1 {absorb_norm * (ONE - std::exp(absorb_coeff * HEAVISIDE(delta_r1) * CUBE(delta_r1)))};
 
+            mblock.em0(i, j, em::ex2) = (ONE - sigma_r1) * mblock.em0(i, j, em::ex2);
+            mblock.em0(i, j, em::ex3) = (ONE - sigma_r1) * mblock.em0(i, j, em::ex3);
+
+            mblock.em(i, j, em::ex2) = (ONE - sigma_r1) * mblock.em(i, j, em::ex2);
+            mblock.em(i, j, em::ex3) = (ONE - sigma_r1) * mblock.em(i, j, em::ex3);
+          }
           // i + 1/2
           mblock.metric.x_Code2Sph({i_ + HALF, j_}, rth_);
-          real_t delta_r2 {(rth_[0] - r_absorb) / (r_max - r_absorb)};
-          // real_t sigma_r2 {ONE - std::exp(-5.0 * HEAVISIDE(delta_r2) * delta_r2 * delta_r2 * delta_r2)};
-          real_t sigma_r2 {HEAVISIDE(delta_r2) * delta_r2 * delta_r2 * delta_r2};
+          if (rth_[0] > r_absorb) {
+            real_t delta_r2 {(rth_[0] - r_absorb) / (r_max - r_absorb)};
+            real_t sigma_r2 {absorb_norm * (ONE - std::exp(absorb_coeff * HEAVISIDE(delta_r2) * CUBE(delta_r2)))};
 
-          mblock.em0(i, j, em::ex1) = (ONE - sigma_r2) * mblock.em0(i, j, em::ex1);
-          mblock.em0(i, j, em::ex2) = (ONE - sigma_r1) * mblock.em0(i, j, em::ex2);
-          mblock.em0(i, j, em::ex3) = (ONE - sigma_r1) * mblock.em0(i, j, em::ex3);
-
-          mblock.em(i, j, em::ex1) = (ONE - sigma_r2) * mblock.em(i, j, em::ex1);
-          mblock.em(i, j, em::ex2) = (ONE - sigma_r1) * mblock.em(i, j, em::ex2);
-          mblock.em(i, j, em::ex3) = (ONE - sigma_r1) * mblock.em(i, j, em::ex3);
+            mblock.em0(i, j, em::ex1) = (ONE - sigma_r2) * mblock.em0(i, j, em::ex1);
+            mblock.em(i, j, em::ex1) = (ONE - sigma_r2) * mblock.em(i, j, em::ex1);
+          }
         });
-
       // r = rmax
       auto i_max {mblock.i_max()};
       Kokkos::parallel_for(
         "2d_bc_rmax", NTTRange<Dimension::ONE_D>({mblock.j_min()}, {mblock.j_max()}), Lambda(index_t j) {
-          // mblock.em0(i, j, em::ex3) = mblock.em0(i - 1, j, em::ex3) * 0;
-          // mblock.em0(i, j, em::ex2) = mblock.em0(i - 1, j, em::ex2) * 0;
-
-          // mblock.em(i, j, em::ex3) = mblock.em(i - 1, j, em::ex3) * 0;
-          // mblock.em(i, j, em::ex2) = mblock.em(i - 1, j, em::ex2) * 0;
-          mblock.em0(i_max, j, em::ex3) = mblock.em0(i_max - 1, j, em::ex3);
           mblock.em0(i_max, j, em::ex2) = mblock.em0(i_max - 1, j, em::ex2);
+          mblock.em0(i_max, j, em::ex3) = mblock.em0(i_max - 1, j, em::ex3);
 
-          mblock.em(i_max, j, em::ex3) = mblock.em(i_max - 1, j, em::ex3);
           mblock.em(i_max, j, em::ex2) = mblock.em(i_max - 1, j, em::ex2);
+          mblock.em(i_max, j, em::ex3) = mblock.em(i_max - 1, j, em::ex3);
         });
     } else if (f == gr_bc::Bfield) {
       // theta = 0 boundary
@@ -87,10 +82,6 @@ namespace ntt {
         "2d_bc_theta0", NTTRange<Dimension::ONE_D>({mblock.i_min() - 1}, {mblock.i_max()}), Lambda(index_t i) {
           mblock.em0(i, j_min, em::bx2) = ZERO;
           mblock.em(i, j_min, em::bx2) = ZERO;
-          // mblock.em0(i, j_min - 1, em::bx1) = mblock.em0(i, j_min, em::bx1);
-          // mblock.em0(i, j_min - 1, em::bx3) = mblock.em0(i, j_min, em::bx3);
-          // mblock.em(i, j_min - 1, em::bx1) = mblock.em(i, j_min, em::bx1);
-          // mblock.em(i, j_min - 1, em::bx3) = mblock.em(i, j_min, em::bx3);
         });
 
       // theta = pi boundary
@@ -99,11 +90,6 @@ namespace ntt {
         "2d_bc_thetaPi", NTTRange<Dimension::ONE_D>({mblock.i_min() - 1}, {m_mblock.i_max()}), Lambda(index_t i) {
           mblock.em0(i, j_max, em::bx2) = ZERO;
           mblock.em(i, j_max, em::bx2) = ZERO;
-
-          // mblock.em0(i, j_max, em::bx1) = mblock.em0(i, j_max - 1, em::bx1);
-          // mblock.em0(i, j_max, em::bx3) = mblock.em0(i, j_max - 1, em::bx3);
-          // mblock.em(i, j_max, em::bx1) = mblock.em(i, j_max - 1, em::bx1);
-          // mblock.em(i, j_max, em::bx3) = mblock.em(i, j_max - 1, em::bx3);
         });
 
       // r = rmin boundary
@@ -122,11 +108,11 @@ namespace ntt {
         });
 
       auto r_absorb {m_sim_params.metric_parameters()[2]};
+      auto absorb_coeff {m_sim_params.metric_parameters()[3]};
+      auto absorb_norm {ONE / (ONE - std::exp(absorb_coeff))};
       auto r_max {m_mblock.metric.x1_max};
       auto pGen {this->m_pGen};
       Kokkos::parallel_for(
-        // !TEMP:
-        // "2d_absorbing bc", m_mblock.loopActiveCells(), Lambda(index_t i, index_t j) {
         "2d_absorbing bc",
         NTTRange<Dimension::TWO_D>({mblock.i_min(), mblock.j_min()}, {mblock.i_max() + 1, mblock.j_max() + 1}),
         Lambda(index_t i, index_t j) {
@@ -136,35 +122,24 @@ namespace ntt {
           // i
           vec_t<Dimension::TWO_D> rth_;
           mblock.metric.x_Code2Sph({i_, j_}, rth_);
-          real_t delta_r1 {(rth_[0] - r_absorb) / (r_max - r_absorb)};
-          // real_t sigma_r1 {ONE - std::exp(-HEAVISIDE(5.0 * delta_r1) * delta_r1 * delta_r1 * delta_r1)};
-          real_t sigma_r1 {HEAVISIDE(delta_r1) * delta_r1 * delta_r1 * delta_r1};
-
+          if (rth_[0] > r_absorb) {
+            real_t delta_r1 {(rth_[0] - r_absorb) / (r_max - r_absorb)};
+            real_t sigma_r1 {absorb_norm * (ONE - std::exp(absorb_coeff * HEAVISIDE(delta_r1) * CUBE(delta_r1)))};
+            real_t br_target {pGen.userTargetField_br_cntrv(mblock, {i_, j_ + HALF})};
+            mblock.em0(i, j, em::bx1) = (ONE - sigma_r1) * mblock.em0(i, j, em::bx1) + sigma_r1 * br_target;
+            mblock.em(i, j, em::bx1) = (ONE - sigma_r1) * mblock.em(i, j, em::bx1) + sigma_r1 * br_target;
+          }
           // i + 1/2
           mblock.metric.x_Code2Sph({i_ + HALF, j_}, rth_);
-          real_t delta_r2 {(rth_[0] - r_absorb) / (r_max - r_absorb)};
-          // real_t sigma_r2 {ONE - std::exp(-HEAVISIDE(5.0 * delta_r2) * delta_r2 * delta_r2 * delta_r2)};
-          real_t sigma_r2 {HEAVISIDE(delta_r2) * delta_r2 * delta_r2 * delta_r2};
-
-          real_t br_target {pGen.userTargetField_br_cntrv(mblock, {i_, j_ + HALF})};
-          real_t bth_target {pGen.userTargetField_bth_cntrv(mblock, {i_ + HALF, j_})};
-          real_t bx1_source_cntr {mblock.em0(i, j, em::bx1)};
-          real_t bx2_source_cntr {mblock.em0(i, j, em::bx2)};
-          real_t br_interm {(ONE - sigma_r1) * bx1_source_cntr + sigma_r1 * br_target};
-          real_t bth_interm {(ONE - sigma_r2) * bx2_source_cntr + sigma_r2 * bth_target};
-
-          mblock.em0(i, j, em::bx1) = br_interm;
-          mblock.em0(i, j, em::bx2) = bth_interm;
-          mblock.em0(i, j, em::bx3) = (ONE - sigma_r2) * mblock.em0(i, j, em::bx3);
-
-          bx1_source_cntr = mblock.em(i, j, em::bx1);
-          bx2_source_cntr = mblock.em(i, j, em::bx2);
-          br_interm = (ONE - sigma_r1) * bx1_source_cntr + sigma_r1 * br_target;
-          bth_interm = (ONE - sigma_r2) * bx2_source_cntr + sigma_r2 * bth_target;
-
-          mblock.em(i, j, em::bx1) = br_interm;
-          mblock.em(i, j, em::bx2) = bth_interm;
-          mblock.em(i, j, em::bx3) = (ONE - sigma_r2) * mblock.em(i, j, em::bx3);
+          if (rth_[0] > r_absorb) {
+            real_t delta_r2 {(rth_[0] - r_absorb) / (r_max - r_absorb)};
+            real_t sigma_r2 {absorb_norm * (ONE - std::exp(absorb_coeff * HEAVISIDE(delta_r2) * CUBE(delta_r2)))};
+            real_t bth_target {pGen.userTargetField_bth_cntrv(mblock, {i_ + HALF, j_})};
+            mblock.em0(i, j, em::bx2) = (ONE - sigma_r2) * mblock.em0(i, j, em::bx2) + sigma_r2 * bth_target;
+            mblock.em(i, j, em::bx2) = (ONE - sigma_r2) * mblock.em(i, j, em::bx2) + sigma_r2 * bth_target;
+            mblock.em0(i, j, em::bx3) = (ONE - sigma_r2) * mblock.em0(i, j, em::bx3);
+            mblock.em(i, j, em::bx3) = (ONE - sigma_r2) * mblock.em(i, j, em::bx3);
+          }
         });
 
       // r = rmax
@@ -194,29 +169,14 @@ namespace ntt {
       // r = rmin boundary
       Kokkos::parallel_for(
         "2d_bc_rmin", range, Lambda(index_t j) {
-          // mblock.aux(i_min, j, em::ex1) = ZERO;
-          // mblock.aux(i_min, j, em::ex2) = ZERO;
-          // mblock.aux(i_min, j, em::ex3) = ZERO;
-          // mblock.aux(i_min + 1, j, em::ex1) = ZERO;
-          // mblock.aux(i_min + 1, j, em::ex2) = ZERO;
-          // mblock.aux(i_min + 1, j, em::ex3) = ZERO;
           mblock.aux(i_min - 1, j, em::ex1) = mblock.aux(i_min, j, em::ex1);
-          // mblock.aux(i_min, j, em::ex2) = mblock.aux(i_min + 1, j, em::ex2);
           mblock.aux(i_min - 1, j, em::ex2) = mblock.aux(i_min, j, em::ex2);
-          mblock.aux(i_min, j, em::ex3) = mblock.aux(i_min + 1, j, em::ex3);
           mblock.aux(i_min - 1, j, em::ex3) = mblock.aux(i_min, j, em::ex3);
         });
     } else if (f == gr_bc::Hfield) {
       // r = rmin boundary
       Kokkos::parallel_for(
         "2d_bc_rmin", range, Lambda(index_t j) {
-          // mblock.aux(i_min, j, em::bx1) = ZERO;
-          // mblock.aux(i_min, j, em::bx2) = ZERO;
-          // mblock.aux(i_min, j, em::bx3) = ZERO;
-          // mblock.aux(i_min + 1, j, em::bx1) = ZERO;
-          // mblock.aux(i_min + 1, j, em::bx2) = ZERO;
-          // mblock.aux(i_min + 1, j, em::bx3) = ZERO;
-          mblock.aux(i_min, j, em::bx1) = mblock.aux(i_min + 1, j, em::bx1);
           mblock.aux(i_min - 1, j, em::bx1) = mblock.aux(i_min, j, em::bx1);
           mblock.aux(i_min - 1, j, em::bx2) = mblock.aux(i_min, j, em::bx2);
           mblock.aux(i_min - 1, j, em::bx3) = mblock.aux(i_min, j, em::bx3);
