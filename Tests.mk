9df381a5579ace95d4ddb7deacd259dfd4df0d9a
haykh <haykh.mfs+github@gmail.com>
2022-07-13 01:40:36 -0400

    ✅ test facility
    Former-commit-id: 27d7b811fcfe2348bac7500aeb8d4e8ef665d30b
    Former-commit-id: 0664cdc664ad0d0fc80b630b457bba44983f4fcc

---
+++ Tests.mk
@@ -0,0 +1,56 @@
+.PHONY: test test_pic test_grpic
+
+TEST_CFLAGS := $(filter-out -DSIMTYPE=%, $(filter-out -DMETRIC=%, $(CFLAGS)))
+test_compile_command := ${CXX} ${CXXSTANDARD} $(INCFLAGS) $(DEFINITIONS) $(TEST_CFLAGS) -MMD
+
+MINKOWSKI_FLAG := -DMETRIC=MINKOWSKI_METRIC
+SPHERICAL_FLAG := -DMETRIC=SPHERICAL_METRIC
+QSPHERICAL_FLAG := -DMETRIC=QSPHERICAL_METRIC
+PIC_FLAG := -DSIMTYPE=PIC_SIMTYPE
+
+TEST_MINK_PIC_OBJS := $(subst ${SRC_DIR},${BUILD_TEST_DIR}PIC_mnk,$(SRCS:%=%.o))
+TEST_SPH_PIC_OBJS := $(subst ${SRC_DIR},${BUILD_TEST_DIR}PIC_sph,$(SRCS:%=%.o))
+TEST_QSPH_PIC_OBJS := $(subst ${SRC_DIR},${BUILD_TEST_DIR}PIC_qsph,$(SRCS:%=%.o))
+
+TEST_DEPS := ${BUILD_TEST_DIR}PIC_mnk/test.d
+
+test: pgenCopy $(KOKKOS_LINK_DEPENDS) test_pic
+
+test_pic: ${BIN_DIR}/testPICMink.exec
+
+# test_sph_pic test_qsph_pic
+
+# test_grpic: test_ks_grpic test_qks_grpic
+
+# PIC Minkowski
+${BIN_DIR}/testPICMink.exec: $(TEST_MINK_PIC_OBJS) ${BUILD_TEST_DIR}PIC_mnk/test.o
+	@echo [L]inking $(notdir $@) from $<
+	$(HIDE)mkdir -p ${BIN_DIR}
+	$(HIDE)${link_command} $^ -o $@ $(LIBS)
+
+${BUILD_TEST_DIR}PIC_mnk/%.o: ${SRC_DIR}/%
+	@echo [C]ompiling \`src\` for \`PIC Mink\` test: $(subst ${ROOT_DIR}/,,$<)
+	$(HIDE)mkdir -p $(dir $@)
+	$(HIDE)${test_compile_command} ${MINKOWSKI_FLAG} ${PIC_FLAG} -c $< -o $@
+
+${BUILD_TEST_DIR}PIC_mnk/test.o: ${TEST_DIR}/test.cpp
+	@echo [C]ompiling \`test\` for \`PIC Mink\` test: $(subst ${ROOT_DIR}/,,$<)
+	$(HIDE)mkdir -p $(dir $@)
+	$(HIDE)${test_compile_command} ${MINKOWSKI_FLAG} ${PIC_FLAG} -c $< -o $@
+
+# PIC Spherical
+${BUILD_TEST_DIR}PIC_sph/%.o: ${SRC_DIR}/%
+	@echo [C]ompiling \`src\` for \`PIC Sph\` test: $(subst ${ROOT_DIR}/,,$<)
+	$(HIDE)mkdir -p $(dir $@)
+	$(HIDE)${test_compile_command} ${SPHERICAL_FLAG} ${PIC_FLAG} -c $< -o $@
+
+# PIC QSpherical
+${BUILD_TEST_DIR}PIC_qsph/%.o: ${SRC_DIR}/%
+	@echo [C]ompiling \`src\` for \`PIC Qsph\` test: $(subst ${ROOT_DIR}/,,$<)
+	$(HIDE)mkdir -p $(dir $@)
+	$(HIDE)${test_compile_command} ${QSPHERICAL_FLAG} ${PIC_FLAG} -c $< -o $@
+
+-include $(TEST_DEPS)
+
+print: 
+	@echo $(TEST_DEPS)
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,78 +1,83 @@
-// // test external libraries
-// #include "test_extern_toml.h"
-// #include "test_extern_kokkos.h"
+// test external libraries
+#include "test_extern_toml.h"
+#include "test_extern_kokkos.h"
+#include "test_qmath.h"
 
-// #include <acutest.h>
+#include "pic/test_pic_minkowski.h"
 
-// void testSuccess() {}
+#include <acutest.h>
 
-// TEST_LIST
-//     = {{"extern/toml", testExternToml},
-//        {"extern/kokkos", testExternKokkos},
-//        {"success", testSuccess},
-//        {nullptr, nullptr}};
+void testSuccess() {}
 
-#include "global.h"
-#include "cargs.h"
-#include "input.h"
+TEST_LIST
+    = {{"extern/toml", testExternToml},
+       {"extern/kokkos", testExternKokkos},
+       {"qmath", testQmath},
+       {"pic/minkowski", testPICMinkowski},
+       {"success", testSuccess},
+       {nullptr, nullptr}};
 
-#include <toml/toml.hpp>
+// #include "global.h"
+// #include "cargs.h"
+// #include "input.h"
 
-#include <plog/Log.h>
-#include <plog/Init.h>
-#include <plog/Appenders/ColorConsoleAppender.h>
+// #include <toml/toml.hpp>
 
-#include <iostream>
-#include <vector>
-#include <stdexcept>
+// #include <plog/Log.h>
+// #include <plog/Init.h>
+// #include <plog/Appenders/ColorConsoleAppender.h>
 
-using plog_t = plog::ColorConsoleAppender<plog::NTTFormatter>;
+// #include <iostream>
+// #include <vector>
+// #include <stdexcept>
 
-void initLogger(plog_t* console_appender);
+// using plog_t = plog::ColorConsoleAppender<plog::NTTFormatter>;
 
-auto main(int argc, char* argv[]) -> int {
-  plog_t console_appender;
-  initLogger(&console_appender);
+// void initLogger(plog_t* console_appender);
 
-  Kokkos::initialize();
-  try {
-    ntt::CommandLineArguments cl_args;
-    cl_args.readCommandLineArguments(argc, argv);
-    // auto inputfilename = cl_args.getArgument("-input", ntt::defaults::input_filename);
-    // // auto outputpath = cl_args.getArgument("-output", ntt::DEF_output_path);
-    // auto  inputdata = toml::parse(static_cast<std::string>(inputfilename));
-    // short res = static_cast<short>(ntt::readFromInput<std::vector<int>>(inputdata, "domain", "resolution").size());
+// auto main(int argc, char* argv[]) -> int {
+//   plog_t console_appender;
+//   initLogger(&console_appender);
 
-    // if (res == 1) {
-    //   // ntt::SIMULATION_CONTAINER<ntt::Dimension::ONE_D> sim(inputdata);
-    //   // sim.process();
-    // } else if (res == 2) {
-    //   ntt::SIMULATION_CONTAINER<ntt::Dimension::TWO_D> sim(inputdata);
-    //   sim.process();
-    // } else if (res == 3) {
-    //   ntt::SIMULATION_CONTAINER<ntt::Dimension::THREE_D> sim(inputdata);
-    //   sim.process();
-    // } else {
-    //   NTTError("wrong dimension specified");
-    // }
-  }
-  catch (std::exception& err) {
-    std::cerr << err.what() << std::endl;
-    Kokkos::finalize();
+//   Kokkos::initialize();
+//   try {
+//     ntt::CommandLineArguments cl_args;
+//     cl_args.readCommandLineArguments(argc, argv);
+//     // auto inputfilename = cl_args.getArgument("-input", ntt::defaults::input_filename);
+//     // // auto outputpath = cl_args.getArgument("-output", ntt::DEF_output_path);
+//     // auto  inputdata = toml::parse(static_cast<std::string>(inputfilename));
+//     // short res = static_cast<short>(ntt::readFromInput<std::vector<int>>(inputdata, "domain", "resolution").size());
 
-    return -1;
-  }
-  Kokkos::finalize();
+//     // if (res == 1) {
+//     //   // ntt::SIMULATION_CONTAINER<ntt::Dimension::ONE_D> sim(inputdata);
+//     //   // sim.process();
+//     // } else if (res == 2) {
+//     //   ntt::SIMULATION_CONTAINER<ntt::Dimension::TWO_D> sim(inputdata);
+//     //   sim.process();
+//     // } else if (res == 3) {
+//     //   ntt::SIMULATION_CONTAINER<ntt::Dimension::THREE_D> sim(inputdata);
+//     //   sim.process();
+//     // } else {
+//     //   NTTError("wrong dimension specified");
+//     // }
+//   }
+//   catch (std::exception& err) {
+//     std::cerr << err.what() << std::endl;
+//     Kokkos::finalize();
 
-  return 0;
-}
+//     return -1;
+//   }
+//   Kokkos::finalize();
 
-void initLogger(plog_t* console_appender) {
-  plog::Severity max_severity;
-#ifdef DEBUG
-  max_severity = plog::verbose;
-#else
-  max_severity = plog::info;
-#endif
-  plog::init(max_severity, console_appender);
-}
+//   return 0;
+// }
+
+// void initLogger(plog_t* console_appender) {
+//   plog::Severity max_severity;
+// #ifdef DEBUG
+//   max_severity = plog::verbose;
+// #else
+//   max_severity = plog::info;
+// #endif
+//   plog::init(max_severity, console_appender);
+// }
