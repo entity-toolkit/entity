5f3a7afcb16b68342e5bfe22887ac92dc1fbe653
haykh <haykh.mfs+github@gmail.com>
2022-07-26 18:47:59 -0400

    ✅ ExB test
    Former-commit-id: 8c3f90142cbd205d7e91472badfe314ddb5eac2d
    Former-commit-id: 7d9c4925cb3914f88b6aa87dcc54909d581163bd

--- tests/pic/test_pic_minkowski.h
+++ tests/pic/test_pic_minkowski.h
@@ -18,7 +18,7 @@
 TEST_CASE("testing PIC") {
   Kokkos::initialize();
   SUBCASE("Minkowski") {
-    SUBCASE("2D") {
+    SUBCASE("2D pusher [E x B]") {
       std::string        input_toml = R"TOML(
         [domain]
         resolution      = [64, 64]
@@ -53,20 +53,27 @@ TEST_CASE("testing PIC") {
       CHECK(ntt::AlmostEqual(sim.sim_params()->charge0(), 100.0f));
       CHECK(ntt::AlmostEqual(sim.sim_params()->sigma0(), 0.01f));
 
+      real_t beta_drift = 0.1f;
+
       // initialize Bz
       auto mblock = sim.mblock();
       Kokkos::parallel_for(
         "set fields", sim.loopActiveCells(), Lambda(const std::size_t i, const std::size_t j) {
           real_t i_ {(real_t)(static_cast<int>(i) - ntt::N_GHOSTS)};
           real_t j_ {(real_t)(static_cast<int>(j) - ntt::N_GHOSTS)};
-          ntt::vec_t<ntt::Dimension::THREE_D> b_cntrv;
+          ntt::vec_t<ntt::Dimension::THREE_D> e_cntrv, b_cntrv;
           mblock->metric.v_Hat2Cntrv({i_ + HALF, j_ + HALF}, {ZERO, ZERO, ONE}, b_cntrv);
           mblock->em(i, j, ntt::em::bx3) = b_cntrv[2];
+
+          mblock->metric.v_Hat2Cntrv({i_, j_ + HALF}, {ZERO, beta_drift, ZERO}, e_cntrv);
+          mblock->em(i, j, ntt::em::ex2) = e_cntrv[1];
         });
+      sim.fieldBoundaryConditions(0.0f);
 
       // initialize particles
       real_t                              gammabeta = 1.0f;
       ntt::coord_t<ntt::Dimension::TWO_D> x_init {0.1, 0.12};
+
       Kokkos::parallel_for(
         "set particles",
         ntt::NTTRange<ntt::Dimension::ONE_D>({0}, {1}),
@@ -97,23 +104,56 @@ TEST_CASE("testing PIC") {
         CHECK(ntt::AlmostEqual(mblock->particles[i].dx2(0), 0.92f));
       }
 
-      sim.fieldBoundaryConditions(0.0f);
-
-      real_t runtime = 1000.0f;
+      // run for some # of timesteps
+      real_t runtime = 500.0f;
       for (int i {0}; i < (int)runtime; ++i) {
+
         sim.pushParticlesSubstep(0.0f, ONE);
         sim.particleBoundaryConditions(0.0f);
       }
 
       // compare with analytics
+      real_t larmor0 = sim.sim_params()->larmor0();
+      real_t dt      = sim.mblock()->timestep();
 
-      real_t beta          = gammabeta / math::sqrt(1.0f + SQR(gammabeta));
-      real_t phase         = (runtime * sim.mblock()->timestep() / (gammabeta / beta));
-      real_t Larmor_radius = gammabeta;
-      real_t x_p1_fin      = (x_init[0]) + (Larmor_radius * std::sin(phase));
-      real_t y_p1_fin      = (x_init[1] + Larmor_radius) - (Larmor_radius * std::cos(phase));
-      real_t x_p2_fin      = (x_init[0]) + (Larmor_radius * std::sin(phase));
-      real_t y_p2_fin      = (x_init[1] - Larmor_radius) + (Larmor_radius * std::cos(phase));
+      real_t beta        = gammabeta / math::sqrt(1.0f + SQR(gammabeta));
+      real_t gamma_drift = 1.0f / math::sqrt(1.0f - SQR(beta_drift));
+      real_t time        = (runtime - 1.0f) * dt / gamma_drift;
+
+      real_t gammabeta1 = gammabeta * (gamma_drift - gamma_drift * beta_drift / beta);
+      real_t beta1      = gammabeta1 / math::sqrt(1.0f + SQR(gammabeta1));
+      real_t larmor1    = larmor0 * gammabeta1;
+      real_t omega1     = beta1 / larmor1;
+
+      real_t phase    = (time * omega1);
+      real_t x_p1_fin = (x_init[0] + beta_drift * time) + (larmor1 * std::sin(phase));
+      real_t y_p1_fin = (x_init[1] + larmor1) - (larmor1 * std::cos(phase));
+      real_t x_p2_fin = (x_init[0] + beta_drift * time) + (larmor1 * std::sin(phase));
+      real_t y_p2_fin = (x_init[1] - larmor1) + (larmor1 * std::cos(phase));
+      while (x_p1_fin >= 2.0f) {
+        x_p1_fin -= 4.0f;
+      }
+      while (x_p1_fin < -2.0f) {
+        x_p1_fin += 4.0f;
+      }
+      while (x_p2_fin >= 2.0f) {
+        x_p2_fin -= 4.0f;
+      }
+      while (x_p2_fin < -2.0f) {
+        x_p2_fin += 4.0f;
+      }
+      while (y_p1_fin >= 2.0f) {
+        y_p1_fin -= 4.0f;
+      }
+      while (y_p1_fin < -2.0f) {
+        y_p1_fin += 4.0f;
+      }
+      while (y_p2_fin >= 2.0f) {
+        y_p2_fin -= 4.0f;
+      }
+      while (y_p2_fin < -2.0f) {
+        y_p2_fin += 4.0f;
+      }
 
       // check: 1 % accurate after ~ 5 rotations
       ntt::coord_t<ntt::Dimension::TWO_D> x_p {ZERO, ZERO};
@@ -133,7 +173,9 @@ TEST_CASE("testing PIC") {
 
       sim.finalize();
     }
-    // test field solver
+    SUBCASE("2D fieldsolver") {
+      // test field solver
+    }
     // test deposition
   }
   Kokkos::finalize();
