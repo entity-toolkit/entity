07f3011773cc4777364d03dbb28144e61a9034fd
haykh <haykh.mfs+github@gmail.com>
2022-08-29 19:30:34 -0400

    ✨ axis bc particle (no deposit yet)
    Former-commit-id: 433d56ff9ecdde8a51782094af6702382e355025
    Former-commit-id: 226bcee5ac362b5c00d295b398b7d900c49c7b99

--- src/framework/particle_macros.h
+++ src/framework/particle_macros.h
@@ -1,20 +1,21 @@
 #ifndef FRAMEWORK_PARTICLE_MACROS_H
 #define FRAMEWORK_PARTICLE_MACROS_H
 
-#define PRTL_X1(P)                                                                            \
-  (static_cast<real_t>(m_particles.i1((p))) + static_cast<real_t>(m_particles.dx1((p))))
+#define PRTL_X1(PARTICLES, P)                                                                 \
+  (static_cast<real_t>((PARTICLES).i1((p))) + static_cast<real_t>((PARTICLES).dx1((p))))
 
-#define PRTL_X2(P)                                                                            \
-  (static_cast<real_t>(m_particles.i2((p))) + static_cast<real_t>(m_particles.dx2((p))))
+#define PRTL_X2(PARTICLES, P)                                                                 \
+  (static_cast<real_t>((PARTICLES).i2((p))) + static_cast<real_t>((PARTICLES).dx2((p))))
 
-#define PRTL_X3(P)                                                                            \
-  (static_cast<real_t>(m_particles.i3((p))) + static_cast<real_t>(m_particles.dx3((p))))
+#define PRTL_X3(PARTICLES, P)                                                                 \
+  (static_cast<real_t>((PARTICLES).i3((p))) + static_cast<real_t>((PARTICLES).dx3((p))))
 
-#define PRTL_USQR_SR(P)                                                                       \
-  m_particles.ux1((P)) * m_particles.ux1((P)) + m_particles.ux2((P)) * m_particles.ux2((P))   \
-    + m_particles.ux3((P)) * m_particles.ux3((P))
+#define PRTL_USQR_SR(PARTICLES, P)                                                            \
+  (PARTICLES).ux1((P)) * (PARTICLES).ux1((P)) + (PARTICLES).ux2((P)) * (PARTICLES).ux2((P))   \
+    + (PARTICLES).ux3((P)) * (PARTICLES).ux3((P))
 
-#define PRTL_GAMMA_SR(P) math::sqrt(static_cast<real_t>(1.0) + PRTL_USQR_SR((P)))
+#define PRTL_GAMMA_SR(PARTICLES, P)                                                           \
+  math::sqrt(static_cast<real_t>(1.0) + PRTL_USQR_SR((PARTICLES), (P)))
 
 #define PICPRTL_XYZ_1D(MBLOCK, SPECIES, INDEX, X1, U1, U2, U3)                                \
   {                                                                                           \
