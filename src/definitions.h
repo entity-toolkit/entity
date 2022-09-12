14b00fd46efe4b785caa3810e547459936837de5
haykh <haykh.mfs+github@gmail.com>
2022-09-12 14:06:59 -0400

    🚧 filters ready (not properly tested)
    Former-commit-id: 5456a155877221fa805cff7273c9d00f3a5e6424
    Former-commit-id: ee4c68b420552fdb0f15fe7fd2e5adb17c123b90

--- src/definitions.h
+++ src/definitions.h
@@ -43,12 +43,24 @@ inline constexpr float TWO {2.0f};
 inline constexpr float ZERO {0.0f};
 inline constexpr float HALF {0.5f};
 inline constexpr float QUARTER {0.25f};
+inline constexpr float INV_2 {0.5f};
+inline constexpr float INV_4 {0.25f};
+inline constexpr float INV_8 {0.125f};
+inline constexpr float INV_16 {0.0625f};
+inline constexpr float INV_32 {0.03125f};
+inline constexpr float INV_64 {0.015625f};
 #else
 inline constexpr double ONE {1.0};
 inline constexpr double TWO {2.0};
 inline constexpr double ZERO {0.0};
 inline constexpr double HALF {0.5};
 inline constexpr double QUARTER {0.25};
+inline constexpr double INV_2 {0.5};
+inline constexpr double INV_4 {0.25};
+inline constexpr double INV_8 {0.125};
+inline constexpr double INV_16 {0.0625};
+inline constexpr double INV_32 {0.03125};
+inline constexpr double INV_64 {0.015625};
 #endif
 
 #define IMIN(a, b)   ((a) < (b) ? (a) : (b))
