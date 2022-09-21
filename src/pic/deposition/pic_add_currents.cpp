2a8e48922117c371cc47ee36c765dd550ba0a7e1
haykh <haykh.mfs+github@gmail.com>
2022-09-21 16:28:38 -0400

    🎉 wip axis deposit (initial)
    Former-commit-id: fe789570fa133dfadbcdd2ffc9bad00fe6706faa
    Former-commit-id: 0df8baad3a64834914df1709b2bc41ac09f1cc52

--- src/pic/deposition/pic_add_currents.cpp
+++ src/pic/deposition/pic_add_currents.cpp
@@ -14,14 +14,14 @@ namespace ntt {
    */
   template <Dimension D>
   void PIC<D>::addCurrentsSubstep(const real_t&) {
-    const auto dt {this->m_mblock.timestep()};
-    const auto rho0 {this->m_sim_params.larmor0()};
-    const auto de0 {this->m_sim_params.skindepth0()};
-    const auto n0 {this->m_sim_params.ppc0()};
-    const auto coeff {-dt * rho0 / (n0 * SQR(de0))};
+    const auto dt    = this->m_mblock.timestep();
+    const auto rho0  = this->m_sim_params.larmor0();
+    const auto de0   = this->m_sim_params.skindepth0();
+    const auto n0    = this->m_sim_params.ppc0();
+    const auto coeff = -dt * rho0 / (n0 * SQR(de0));
 #ifdef MINKOWSKI_METRIC
-    const auto dx {(this->m_mblock.metric.x1_max - this->m_mblock.metric.x1_min)
-                   / this->m_mblock.metric.nx1};
+    const auto dx = (this->m_mblock.metric.x1_max - this->m_mblock.metric.x1_min)
+                    / this->m_mblock.metric.nx1;
     Kokkos::parallel_for("add_currents",
                          this->m_mblock.rangeActiveCells(),
                          AddCurrentsMinkowski<D>(this->m_mblock, coeff / CUBE(dx)));
