d08b4391c5639609d35c0a1f9e1fce5e1452a8de
haykh <haykh.mfs+github@gmail.com>
2022-10-03 17:56:37 -0400

    ⬆️ compatible with cmake nttiny
    Former-commit-id: 6053c89f0416af55fe72baaedfe86874ed267f17
    Former-commit-id: 627b24e4cb3eafc1a611e48b15f51840c5e71267

--- Nttiny.mk
+++ Nttiny.mk
@@ -1,27 +1,38 @@
 # to compile visualization app
 #
-COMPILER := ${CXX}
+COMPILER := ${HOST_CXX}
 VIS_TARGET := ${BIN_DIR}/vis.exec
 BUILD_VIS_DIR := $(subst ${ROOT_DIR}/,,${VIS_DIR})
 
-# VIS_SRC := $(subst ntt,nttiny,${VIS_DIR}/${PGEN}.cpp)
 VIS_SRC := ${VIS_DIR}/nttiny.cpp
 VIS_OBJ := $(subst ${VIS_DIR},${BUILD_VIS_DIR},$(VIS_SRC:%=%.o))
-export COMPILE_GLFW := y
-export COMPILE_FREETYPE := n
-export DEBUG := ${DEBUGMODE}
-export VERBOSE := ${VERBOSE}
-export COMPILER := ${CXX}
--include ${NTTINY_DIR}/Makefile
 
-vis : pgenCopy nttiny_static ${VIS_TARGET}
+ENV_VAR_FILES := ${BUILD_DIR}/lib/NTTINY_INC_DIRS ${BUILD_DIR}/lib/NTTINY_LIB_DIRS ${BUILD_DIR}/lib/NTTINY_LIBS
 
-${VIS_TARGET}: ${NTTINY_DIR}/build/libnttiny.a $(OBJS) $(VIS_OBJ) $(NTTINY_LIBS)
+nttiny : ${BUILD_DIR}/lib/libnttiny.a $(ENV_VAR_FILES)
+
+vis : pgenCopy nttiny ${VIS_TARGET}
+
+${BUILD_DIR}/lib/libnttiny.a $(ENV_VAR_FILES) : ${NTTINY_DIR}
+	@echo ${BUILD_DIR}
+	$(HIDE)cd ${NTTINY_DIR} && cmake -B build -D CMAKE_CXX_COMPILER=${HOST_CXX} -D CMAKE_LD_FLAGS="" -D CMAKE_INSTALL_PREFIX=${BUILD_DIR}/lib && cd build && make nttiny -j && make install
+
+nttiny_clean :
+	$(HIDE)rm -rf ${BUILD_DIR}/lib/*nttiny* $(ENV_VAR_FILES)
+	$(HIDE)cd ${NTTINY_DIR}/build && make clean
+
+${VIS_TARGET} : ${BUILD_DIR}/lib/libnttiny.a $(OBJS) $(VIS_OBJ)
+	NTTINY_LIB_DIRS := $(subst ",,$(subst ;, ,"$(shell cat ${BUILD_DIR}/lib/NTTINY_LIB_DIRS)")) ${BUILD_DIR}/lib
+	NTTINY_LIBS := $(subst ",,$(subst ;, ,"$(shell cat ${BUILD_DIR}/lib/NTTINY_LIBS)")) nttiny
+	NTTINY_LIB_FLAGS := $(addprefix -l, $(NTTINY_LIBS))
+	NTTINY_LD_FLAGS := $(addprefix -L, $(NTTINY_LIB_DIRS)) $(LIBS)
 	@echo [L]inking $@ from $^
 	$(HIDE)mkdir -p ${BIN_DIR}
-	$(HIDE)${link_command} $^ $(NTTINY_LIBS) -o $@ $(NTTINY_LINKFLAGS) $(LIBS)
+	$(HIDE)${link_command} $^ $(NTTINY_LIB_FLAGS) -o $@ $(NTTINY_LD_FLAGS)
 
 ${BUILD_VIS_DIR}/%.o : ${VIS_DIR}/%
+	NTTINY_LIBS := $(subst ",,$(subst ;, ,"$(shell cat ${BUILD_DIR}/lib/NTTINY_LIBS)")) nttiny
+	NTTINY_INC_FLAGS := $(addprefix -I, $(NTTINY_INC_DIRS) ${NTTINY_DIR}/src ${NTTINY_DIR}/extern $(dir $(wildcard ${NTTINY_DIR}/src/**/)))
 	@echo [C]ompiling \`vis\`: $(subst ${ROOT_DIR}/,,$<)
 	$(HIDE)mkdir -p $(dir $@)
-	$(HIDE)${compile_command} $(NTTINY_INCFLAGS) -I${NTTINY_DIR} -c $^ -o $@
+	$(HIDE)${compile_command} $(NTTINY_INC_FLAGS) -c $^ -o $@
