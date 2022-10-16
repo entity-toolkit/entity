e03be47800cfbf166fc33c8509449a416c260b1b
haykh <haykh.mfs+github@gmail.com>
2022-10-15 23:28:22 -0400

    🚧 wip placeholders in pgens
    Former-commit-id: 8c1f9594735f77ecba4d5856158c4ef80393e57f
    Former-commit-id: 79a9487ba9b29bde569950e24792c9d06379bb58

---
+++ src/pic/pgen/placeholders/2d.h
@@ -0,0 +1,22 @@
+template <>
+ProblemGenerator<Dim2, TypePIC>::ProblemGenerator(const SimulationParams&) {}
+
+template <>
+void ProblemGenerator<Dim2, TypePIC>::UserInitFields(const SimulationParams&,
+                                                     Meshblock<Dim2, TypePIC>&) {}
+
+template <>
+void ProblemGenerator<Dim2, TypePIC>::UserInitParticles(const SimulationParams&,
+                                                        Meshblock<Dim2, TypePIC>&) {}
+
+template <>
+void ProblemGenerator<Dim2, TypePIC>::UserBCFields(const real_t&,
+                                                   const SimulationParams&,
+                                                   Meshblock<Dim2, TypePIC>&) {}
+
+template <>
+Inline auto
+ProblemGenerator<Dim2, TypePIC>::UserTargetField_br_hat(const Meshblock<Dim2, TypePIC>&,
+                                                        const coord_t<Dim2>&) const -> real_t {
+  return ZERO;
+}
