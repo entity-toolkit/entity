e74ef9d9637e221157c46af322f26ecbd218414e
haykh <haykh.mfs+github@gmail.com>
2022-10-17 01:59:19 -0400

    🚸 mover not an issue in scaling
    Former-commit-id: 78a41a05ce2faab3aa1aafea5b639035b06759ce
    Former-commit-id: 59afc5e878b47e65cecf0c593a85d104823282f8

--- src/pic/pgen/unit/oneprtl.hpp
+++ src/pic/pgen/unit/oneprtl.hpp
@@ -9,16 +9,14 @@ namespace ntt {
 
   template <Dimension D, SimulationType S>
   struct ProblemGenerator {
-    ProblemGenerator(const SimulationParams&) {}
+    ProblemGenerator(const SimulationParams&);
 
     void        UserInitFields(const SimulationParams&, Meshblock<D, S>&);
     void        UserInitParticles(const SimulationParams&, Meshblock<D, S>&);
     void        UserBCFields(const real_t&, const SimulationParams&, Meshblock<D, S>&);
     Inline auto UserTargetField_br_hat(const Meshblock<D, S>&, const coord_t<D>&) const
-      -> real_t {
-      return ZERO;
-    }
-    void UserDriveParticles(const real_t&, const SimulationParams&, Meshblock<D, S>&) {}
+      -> real_t;
+    void UserDriveParticles(const real_t&, const SimulationParams&, Meshblock<D, S>&);
   };
 
 } // namespace ntt
