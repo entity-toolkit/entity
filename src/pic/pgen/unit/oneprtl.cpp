008ea1e3fea95c429eeca68f9f8cb13d92adeecd
haykh <haykh.mfs+github@gmail.com>
2022-10-17 01:59:19 -0400

    🚸 mover not an issue in scaling
    Former-commit-id: 11e9047679434b82258c737681038a5f4944a50f
    Former-commit-id: faf71cddfb1945a2e9700c64ee2d237ae9dd17fb

--- src/pic/pgen/unit/oneprtl.cpp
+++ src/pic/pgen/unit/oneprtl.cpp
@@ -11,62 +11,62 @@
 namespace ntt {
 
   template <>
+  ProblemGenerator<Dim2, TypePIC>::ProblemGenerator(const SimulationParams&) {}
+
+  template <>
   void ProblemGenerator<Dim2, TypePIC>::UserInitFields(const SimulationParams&,
                                                        Meshblock<Dim2, TypePIC>& mblock) {
-
-    Kokkos::parallel_for(
-      "UserInitFlds", mblock.rangeActiveCells(), Lambda(index_t i, index_t j) {
-        real_t i_ {(real_t)(static_cast<int>(i) - N_GHOSTS)},
-          j_ {(real_t)(static_cast<int>(j) - N_GHOSTS)};
-        real_t      ex2_hat {0.1}, bx3_hat {1.0};
-        vec_t<Dim3> e_cntrv, b_cntrv;
-        mblock.metric.v_Hat2Cntrv({i_ + HALF, j_}, {ZERO, ex2_hat, ZERO}, e_cntrv);
-        mblock.metric.v_Hat2Cntrv({i_ + HALF, j_ + HALF}, {ZERO, ZERO, bx3_hat}, b_cntrv);
-        mblock.em(i, j, em::ex1) = ZERO;
-        mblock.em(i, j, em::ex2) = e_cntrv[1];
-        mblock.em(i, j, em::ex3) = ZERO;
-        mblock.em(i, j, em::bx1) = ZERO;
-        mblock.em(i, j, em::bx2) = ZERO;
-        mblock.em(i, j, em::bx3) = b_cntrv[2];
-      });
+    //   Kokkos::parallel_for(
+    //     "UserInitFlds", mblock.rangeActiveCells(), Lambda(index_t i, index_t j) {
+    //       real_t i_ {(real_t)(static_cast<int>(i) - N_GHOSTS)},
+    //         j_ {(real_t)(static_cast<int>(j) - N_GHOSTS)};
+    //       real_t      ex2_hat {0.1}, bx3_hat {1.0};
+    //       vec_t<Dim3> e_cntrv, b_cntrv;
+    //       mblock.metric.v_Hat2Cntrv({i_ + HALF, j_}, {ZERO, ex2_hat, ZERO}, e_cntrv);
+    //       mblock.metric.v_Hat2Cntrv({i_ + HALF, j_ + HALF}, {ZERO, ZERO, bx3_hat}, b_cntrv);
+    //       mblock.em(i, j, em::ex1) = ZERO;
+    //       mblock.em(i, j, em::ex2) = e_cntrv[1];
+    //       mblock.em(i, j, em::ex3) = ZERO;
+    //       mblock.em(i, j, em::bx1) = ZERO;
+    //       mblock.em(i, j, em::bx2) = ZERO;
+    //       mblock.em(i, j, em::bx3) = b_cntrv[2];
+    //     });
   }
 
   template <>
   void ProblemGenerator<Dim2, TypePIC>::UserInitParticles(const SimulationParams&,
                                                           Meshblock<Dim2, TypePIC>& mblock) {
-    auto& electrons   = mblock.particles[0];
-    auto& positrons   = mblock.particles[1];
-    auto  random_pool = *(mblock.random_pool_ptr);
+    auto& electrons = mblock.particles[0];
+    auto& positrons = mblock.particles[1];
+    electrons.setNpart(1);
+    positrons.setNpart(1);
     Kokkos::parallel_for(
       "UserInitPrtls", CreateRangePolicy<Dim1>({0}, {1}), Lambda(index_t p) {
-        typename RandomNumberPool_t::generator_type rand_gen = random_pool.get_state();
-        real_t rx = rand_gen.frand((real_t)(-2.0), (real_t)(2.0));
-        real_t ry = rand_gen.frand((real_t)(-2.0), (real_t)(2.0));
+        real_t rx = 0.0, ry = 0.0;
         init_prtl_2d_XYZ(mblock, electrons, p, rx, ry, 1.0, 0.0, 0.0);
         init_prtl_2d_XYZ(mblock, positrons, p, rx, ry, 1.0, 0.0, 0.0);
-        // init_prtl_2d_XYZ(&mblock, 2, p, 0.1, 0.12, 1.0, 0.0, 0.0);
-        // init_prtl_2d_XYZ(&mblock, 3, p, 0.1, 0.12, 1.0, 0.0, 0.0);
       });
-    mblock.particles[0].setNpart(1);
-    mblock.particles[1].setNpart(1);
-    // mblock.particles[2].setNpart(1);
-    // mblock.particles[3].setNpart(1);
   }
-  // 1D
-  template <>
-  void ProblemGenerator<Dim1, TypePIC>::UserInitFields(const SimulationParams&,
-                                                       Meshblock<Dim1, TypePIC>&) {}
-  template <>
-  void ProblemGenerator<Dim1, TypePIC>::UserInitParticles(const SimulationParams&,
-                                                          Meshblock<Dim1, TypePIC>&) {}
 
-  // 3D
   template <>
-  void ProblemGenerator<Dim3, TypePIC>::UserInitFields(const SimulationParams&,
-                                                       Meshblock<Dim3, TypePIC>&) {}
+  void ProblemGenerator<Dim2, TypePIC>::UserDriveParticles(const real_t&,
+                                                           const SimulationParams&,
+                                                           Meshblock<Dim2, TypePIC>&) {}
+
   template <>
-  void ProblemGenerator<Dim3, TypePIC>::UserInitParticles(const SimulationParams&,
-                                                          Meshblock<Dim3, TypePIC>&) {}
+  void ProblemGenerator<Dim2, TypePIC>::UserBCFields(const real_t&,
+                                                     const SimulationParams&,
+                                                     Meshblock<Dim2, TypePIC>&) {}
+  template <>
+  Inline auto ProblemGenerator<Dim2, TypePIC>::UserTargetField_br_hat(
+    const Meshblock<Dim2, TypePIC>&, const coord_t<Dim2>&) const -> real_t {
+    return ZERO;
+  }
+
+  // clang-format off
+  @PgenPlaceholder1D@
+  @PgenPlaceholder3D@
+  // clang-format on
 
 } // namespace ntt
 
