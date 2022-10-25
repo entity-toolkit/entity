f192cd7fa509fed6e0e33dd87c2c89e07921c31a
haykh <haykh.mfs+github@gmail.com>
2022-10-25 15:29:16 -0400

    🚧 first attempt on inject (langmuir.hpp)
    Former-commit-id: 6174f5e6a8960d15a7d234bcf130d181914f98b5
    Former-commit-id: ee470ee79907698e06be50ef3d940fee8e957de2

--- src/pic/pgen/langmuir.hpp
+++ src/pic/pgen/langmuir.hpp
@@ -14,33 +14,59 @@
 #include <map>
 
 namespace ntt {
+  // template <Dimension D, SimulationType S>
+  // class Injector {
+  //   for
+  // };
 
   template <Dimension D, SimulationType S>
   struct ProblemGenerator : public PGen<D, S> {
     inline ProblemGenerator(const SimulationParams&) {}
     inline void UserInitParticles(const SimulationParams& params, Meshblock<D, S>& mblock) {
       if constexpr (D == Dim2) {
-        auto        ncells      = mblock.Ni1() * mblock.Ni2() * mblock.Ni3();
-        std::size_t npart       = (std::size_t)((double)(ncells * params.ppc0() * 0.5));
-        auto&       electrons   = mblock.particles[0];
-        auto        random_pool = *(mblock.random_pool_ptr);
-        real_t      Xmin        = mblock.metric.x1_min;
-        real_t      Xmax        = mblock.metric.x1_max;
-        real_t      Ymin        = mblock.metric.x2_min;
-        real_t      Ymax        = mblock.metric.x2_max;
-        electrons.setNpart(npart);
+        auto&                electrons   = mblock.particles[0];
+        auto                 random_pool = *(mblock.random_pool_ptr);
+        array_t<std::size_t> nprtl("Nprtl");
         Kokkos::parallel_for(
-          "userInitPrtls", CreateRangePolicy<Dim1>({0}, {(int)npart}), Lambda(index_t p) {
+          "test_prtl_inject", mblock.rangeActiveCells(), Lambda(index_t i1, index_t i2) {
+            const real_t i1_ {static_cast<real_t>(static_cast<int>(i1) - N_GHOSTS)};
+            const real_t i2_ {static_cast<real_t>(static_cast<int>(i2) - N_GHOSTS)};
+
+            // if (i1 % 2 == 0) {
             typename RandomNumberPool_t::generator_type rand_gen = random_pool.get_state();
+            std::size_t p = Kokkos::atomic_fetch_add(&nprtl(), 1);
 
-            real_t rx = rand_gen.frand(Xmin, Xmax);
-            real_t ry = rand_gen.frand(Ymin, Ymax);
-            real_t u1 = (real_t)(0.01)
-                        * math::sin((real_t)(2.0) * constant::TWO_PI * rx / (Xmax - Xmin));
-
-            init_prtl_2d(mblock, electrons, p, rx, ry, u1, 0.0, 0.0);
+            vec_t<Dim2> xmin_Cart {ZERO}, xmax_Cart {ZERO};
+            mblock.metric.x_Code2Cart({i1_, i2_}, xmin_Cart);
+            mblock.metric.x_Code2Cart({i1_ + ONE, i2_ + ONE}, xmax_Cart);
+            real_t rx = rand_gen.frand(xmin_Cart[0], xmax_Cart[0]);
+            real_t ry = rand_gen.frand(xmin_Cart[1], xmax_Cart[1]);
+            init_prtl_2d(mblock, electrons, p, rx, ry, 0.0, 0.0, 0.0);
             random_pool.free_state(rand_gen);
+            // }
           });
+        auto nprtl_h = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), nprtl);
+        electrons.setNpart(nprtl_h());
+        // auto        ncells      = mblock.Ni1() * mblock.Ni2() * mblock.Ni3();
+        // std::size_t npart       = (std::size_t)((double)(ncells * params.ppc0() * 0.5));
+        // auto&       electrons   = mblock.particles[0];
+        // auto        random_pool = *(mblock.random_pool_ptr);
+        // real_t      Xmin        = mblock.metric.x1_min;
+        // real_t      Xmax        = mblock.metric.x1_max;
+        // real_t      Ymin        = mblock.metric.x2_min;
+        // real_t      Ymax        = mblock.metric.x2_max;
+        // electrons.setNpart(npart);
+        // Kokkos::parallel_for(
+        //   "userInitPrtls", CreateRangePolicy<Dim1>({0}, {(int)npart}), Lambda(index_t p) {
+        //     typename RandomNumberPool_t::generator_type rand_gen = random_pool.get_state();
+
+        //     real_t rx {rand_gen.frand(Xmin, Xmax)};
+        //     real_t ry {rand_gen.frand(Ymin, Ymax)};
+        //     real_t u1 {0.01 * math::sin(2.0 * constant::TWO_PI * rx / (Xmax - Xmin))};
+
+        //     init_prtl_2d(mblock, electrons, p, rx, ry, u1, 0.0, 0.0);
+        //     random_pool.free_state(rand_gen);
+        //   });
       }
     }
 
