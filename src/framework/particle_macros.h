f5f41e2dc58681345f0aa950da12552c6e488b57
haykh <haykh.mfs+github@gmail.com>
2022-10-25 11:15:53 -0400

    🚸 minor renaming in particle macros + moved tests to legacy
    Former-commit-id: b3b0891a1b8e8eba6bd5538e3a8161f27279f533
    Former-commit-id: 86e292d2ba1bcc7bcb7c9c77e55683bce3099697

--- src/framework/particle_macros.h
+++ src/framework/particle_macros.h
@@ -26,96 +26,102 @@
 #define get_prtl_Gamma_SR(PARTICLES, P)                                                       \
   math::sqrt(static_cast<real_t>(1.0) + get_prtl_Usqr_SR((PARTICLES), (P)))
 
-#define init_prtl_1d_XYZ(MBLOCK, SPECIES, INDEX, X1, U1, U2, U3)                              \
-  {                                                                                           \
-    coord_t<Dim1> X_CU;                                                                       \
-    int           I;                                                                          \
-    float         DX;                                                                         \
-    ((MBLOCK).metric).x_Cart2Code({(X1)}, X_CU);                                              \
-    from_Xi_to_i_di(X_CU[0], I, DX);                                                          \
-    (SPECIES).i1((INDEX))  = I;                                                               \
-    (SPECIES).dx1((INDEX)) = DX;                                                              \
-    (SPECIES).ux1((INDEX)) = U1;                                                              \
-    (SPECIES).ux2((INDEX)) = U2;                                                              \
-    (SPECIES).ux3((INDEX)) = U3;                                                              \
-  }
+#ifdef MINKOWSKI_METRIC
 
-#define init_prtl_2d_XYZ(MBLOCK, SPECIES, INDEX, X1, X2, U1, U2, U3)                          \
-  {                                                                                           \
-    coord_t<Dim2> X_CU;                                                                       \
-    int           I;                                                                          \
-    float         DX;                                                                         \
-    ((MBLOCK).metric).x_Cart2Code({(X1), (X2)}, X_CU);                                        \
-    from_Xi_to_i_di(X_CU[0], I, DX);                                                          \
-    (SPECIES).i1((INDEX))  = I;                                                               \
-    (SPECIES).dx1((INDEX)) = DX;                                                              \
-    from_Xi_to_i_di(X_CU[1], I, DX);                                                          \
-    (SPECIES).i2((INDEX))  = I;                                                               \
-    (SPECIES).dx2((INDEX)) = DX;                                                              \
-    (SPECIES).ux1((INDEX)) = U1;                                                              \
-    (SPECIES).ux2((INDEX)) = U2;                                                              \
-    (SPECIES).ux3((INDEX)) = U3;                                                              \
-  }
+#  define init_prtl_1d(MBLOCK, SPECIES, INDEX, X1, U1, U2, U3)                                \
+    {                                                                                         \
+      coord_t<Dim1> X_CU;                                                                     \
+      int           I;                                                                        \
+      float         DX;                                                                       \
+      ((MBLOCK).metric).x_Cart2Code({(X1)}, X_CU);                                            \
+      from_Xi_to_i_di(X_CU[0], I, DX);                                                        \
+      (SPECIES).i1((INDEX))  = I;                                                             \
+      (SPECIES).dx1((INDEX)) = DX;                                                            \
+      (SPECIES).ux1((INDEX)) = U1;                                                            \
+      (SPECIES).ux2((INDEX)) = U2;                                                            \
+      (SPECIES).ux3((INDEX)) = U3;                                                            \
+    }
 
-#define init_prtl_3d_XYZ(MBLOCK, SPECIES, INDEX, X1, X2, X3, U1, U2, U3)                      \
-  {                                                                                           \
-    coord_t<Dim3> X_CU;                                                                       \
-    int           I;                                                                          \
-    float         DX;                                                                         \
-    ((MBLOCK).metric).x_Cart2Code({(X1), (X2), (X3)}, X_CU);                                  \
-    from_Xi_to_i_di(X_CU[0], I, DX);                                                          \
-    (SPECIES).i1((INDEX))  = I;                                                               \
-    (SPECIES).dx1((INDEX)) = DX;                                                              \
-    from_Xi_to_i_di(X_CU[1], I, DX);                                                          \
-    (SPECIES).i2((INDEX))  = I;                                                               \
-    (SPECIES).dx2((INDEX)) = DX;                                                              \
-    from_Xi_to_i_di(X_CU[2], I, DX);                                                          \
-    (SPECIES).i3((INDEX))  = I;                                                               \
-    (SPECIES).dx3((INDEX)) = DX;                                                              \
-    (SPECIES).ux1((INDEX)) = U1;                                                              \
-    (SPECIES).ux2((INDEX)) = U2;                                                              \
-    (SPECIES).ux3((INDEX)) = U3;                                                              \
-  }
+#  define init_prtl_2d(MBLOCK, SPECIES, INDEX, X1, X2, U1, U2, U3)                            \
+    {                                                                                         \
+      coord_t<Dim2> X_CU;                                                                     \
+      int           I;                                                                        \
+      float         DX;                                                                       \
+      ((MBLOCK).metric).x_Cart2Code({(X1), (X2)}, X_CU);                                      \
+      from_Xi_to_i_di(X_CU[0], I, DX);                                                        \
+      (SPECIES).i1((INDEX))  = I;                                                             \
+      (SPECIES).dx1((INDEX)) = DX;                                                            \
+      from_Xi_to_i_di(X_CU[1], I, DX);                                                        \
+      (SPECIES).i2((INDEX))  = I;                                                             \
+      (SPECIES).dx2((INDEX)) = DX;                                                            \
+      (SPECIES).ux1((INDEX)) = U1;                                                            \
+      (SPECIES).ux2((INDEX)) = U2;                                                            \
+      (SPECIES).ux3((INDEX)) = U3;                                                            \
+    }
 
-#define init_prtl_2d_Sph(MBLOCK, SPECIES, INDEX, X1, X2, U1, U2, U3)                          \
-  {                                                                                           \
-    coord_t<Dim2> X_CU;                                                                       \
-    vec_t<Dim3>   U_C {ZERO, ZERO, ZERO};                                                     \
-    int           I;                                                                          \
-    float         DX;                                                                         \
-    ((MBLOCK).metric).x_Sph2Code({(X1), (X2)}, X_CU);                                         \
-    ((MBLOCK).metric).v_Hat2Cart({X_CU[0], X_CU[1], ZERO}, {U1, U2, U3}, U_C);                \
-    from_Xi_to_i_di(X_CU[0], I, DX);                                                          \
-    (SPECIES).i1((INDEX))  = I;                                                               \
-    (SPECIES).dx1((INDEX)) = DX;                                                              \
-    from_Xi_to_i_di(X_CU[1], I, DX);                                                          \
-    (SPECIES).i2((INDEX))  = I;                                                               \
-    (SPECIES).dx2((INDEX)) = DX;                                                              \
-    (SPECIES).ux1((INDEX)) = U_C[0];                                                          \
-    (SPECIES).ux2((INDEX)) = U_C[1];                                                          \
-    (SPECIES).ux3((INDEX)) = U_C[2];                                                          \
-  }
+#  define init_prtl_3d(MBLOCK, SPECIES, INDEX, X1, X2, X3, U1, U2, U3)                        \
+    {                                                                                         \
+      coord_t<Dim3> X_CU;                                                                     \
+      int           I;                                                                        \
+      float         DX;                                                                       \
+      ((MBLOCK).metric).x_Cart2Code({(X1), (X2), (X3)}, X_CU);                                \
+      from_Xi_to_i_di(X_CU[0], I, DX);                                                        \
+      (SPECIES).i1((INDEX))  = I;                                                             \
+      (SPECIES).dx1((INDEX)) = DX;                                                            \
+      from_Xi_to_i_di(X_CU[1], I, DX);                                                        \
+      (SPECIES).i2((INDEX))  = I;                                                             \
+      (SPECIES).dx2((INDEX)) = DX;                                                            \
+      from_Xi_to_i_di(X_CU[2], I, DX);                                                        \
+      (SPECIES).i3((INDEX))  = I;                                                             \
+      (SPECIES).dx3((INDEX)) = DX;                                                            \
+      (SPECIES).ux1((INDEX)) = U1;                                                            \
+      (SPECIES).ux2((INDEX)) = U2;                                                            \
+      (SPECIES).ux3((INDEX)) = U3;                                                            \
+    }
 
-#define init_prtl_3d_Sph(MBLOCK, SPECIES, INDEX, X1, X2, X3, U1, U2, U3)                      \
-  {                                                                                           \
-    coord_t<Dim3> X_CU;                                                                       \
-    vec_t<Dim3>   U_C {ZERO, ZERO, ZERO};                                                     \
-    int           I;                                                                          \
-    float         DX;                                                                         \
-    ((MBLOCK).metric).x_Sph2Code({(X1), (X2), (X3)}, X_CU);                                   \
-    ((MBLOCK).metric).v_Hat2Cart(X_CU, {U1, U2, U3}, U_C);                                    \
-    from_Xi_to_i_di(X_CU[0], I, DX);                                                          \
-    (SPECIES).i1((INDEX))  = I;                                                               \
-    (SPECIES).dx1((INDEX)) = DX;                                                              \
-    from_Xi_to_i_di(X_CU[1], I, DX);                                                          \
-    (SPECIES).i2((INDEX))  = I;                                                               \
-    (SPECIES).dx2((INDEX)) = DX;                                                              \
-    from_Xi_to_i_di(X_CU[2], I, DX);                                                          \
-    (SPECIES).i3((INDEX))  = I;                                                               \
-    (SPECIES).dx3((INDEX)) = DX;                                                              \
-    (SPECIES).ux1((INDEX)) = U_C[0];                                                          \
-    (SPECIES).ux2((INDEX)) = U_C[1];                                                          \
-    (SPECIES).ux3((INDEX)) = U_C[2];                                                          \
-  }
+#else
+
+#  define init_prtl_2d(MBLOCK, SPECIES, INDEX, X1, X2, U1, U2, U3)                            \
+    {                                                                                         \
+      coord_t<Dim2> X_CU;                                                                     \
+      vec_t<Dim3>   U_C {ZERO, ZERO, ZERO};                                                   \
+      int           I;                                                                        \
+      float         DX;                                                                       \
+      ((MBLOCK).metric).x_Sph2Code({(X1), (X2)}, X_CU);                                       \
+      ((MBLOCK).metric).v_Hat2Cart({X_CU[0], X_CU[1], ZERO}, {U1, U2, U3}, U_C);              \
+      from_Xi_to_i_di(X_CU[0], I, DX);                                                        \
+      (SPECIES).i1((INDEX))  = I;                                                             \
+      (SPECIES).dx1((INDEX)) = DX;                                                            \
+      from_Xi_to_i_di(X_CU[1], I, DX);                                                        \
+      (SPECIES).i2((INDEX))  = I;                                                             \
+      (SPECIES).dx2((INDEX)) = DX;                                                            \
+      (SPECIES).ux1((INDEX)) = U_C[0];                                                        \
+      (SPECIES).ux2((INDEX)) = U_C[1];                                                        \
+      (SPECIES).ux3((INDEX)) = U_C[2];                                                        \
+    }
+
+#  define init_prtl_3d(MBLOCK, SPECIES, INDEX, X1, X2, X3, U1, U2, U3)                        \
+    {                                                                                         \
+      coord_t<Dim3> X_CU;                                                                     \
+      vec_t<Dim3>   U_C {ZERO, ZERO, ZERO};                                                   \
+      int           I;                                                                        \
+      float         DX;                                                                       \
+      ((MBLOCK).metric).x_Sph2Code({(X1), (X2), (X3)}, X_CU);                                 \
+      ((MBLOCK).metric).v_Hat2Cart(X_CU, {U1, U2, U3}, U_C);                                  \
+      from_Xi_to_i_di(X_CU[0], I, DX);                                                        \
+      (SPECIES).i1((INDEX))  = I;                                                             \
+      (SPECIES).dx1((INDEX)) = DX;                                                            \
+      from_Xi_to_i_di(X_CU[1], I, DX);                                                        \
+      (SPECIES).i2((INDEX))  = I;                                                             \
+      (SPECIES).dx2((INDEX)) = DX;                                                            \
+      from_Xi_to_i_di(X_CU[2], I, DX);                                                        \
+      (SPECIES).i3((INDEX))  = I;                                                             \
+      (SPECIES).dx3((INDEX)) = DX;                                                            \
+      (SPECIES).ux1((INDEX)) = U_C[0];                                                        \
+      (SPECIES).ux2((INDEX)) = U_C[1];                                                        \
+      (SPECIES).ux3((INDEX)) = U_C[2];                                                        \
+    }
+
+#endif
 
 #endif
