57f1922e1c28d399f25893ca24d916ff143dcdde
haykh <haykh.mfs+github@gmail.com>
2023-01-19 01:14:27 -0500

    🚸 sync pgens

--- src/pic/pgen/weibel.hpp
+++ src/pic/pgen/weibel.hpp
@@ -15,19 +15,26 @@ namespace ntt {
   struct Drift : public EnergyDistribution<D, S> {
     Drift(const SimulationParams& params, const Meshblock<D, S>& mblock)
       : EnergyDistribution<D, S>(params, mblock),
-        udrift { params.get<real_t>("problem", "udrift", 1.0) } {}
+        udrift1 { params.get<real_t>("problem", "udrift1", -10.0) },
+        udrift2 { params.get<real_t>("problem", "udrift2", -10.0) },
+        udrift3 { params.get<real_t>("problem", "udrift3", 10.0) },
+        udrift4 { params.get<real_t>("problem", "udrift4", 10.0) } {}
     Inline void operator()(const coord_t<D>&,
                            vec_t<Dim3>& v,
                            const int&   species) const override {
-      if (species == 0) {
-        v[0] = udrift;
-      } else {
-        v[0] = -udrift;
+      if (species == 1) {
+        v[2] = udrift1;
+      } else if (species == 2) {
+        v[2] = udrift2;
+      } else if (species == 3) {
+        v[2] = udrift3;
+      } else if (species == 4) {
+        v[2] = udrift4;
       }
     }
 
   private:
-    const real_t udrift;
+    const real_t udrift1, udrift2, udrift3, udrift4;
   };
 
   template <Dimension D, SimulationEngine S>
@@ -40,6 +47,7 @@ namespace ntt {
   inline void ProblemGenerator<Dim2, PICEngine>::UserInitParticles(
     const SimulationParams& params, Meshblock<Dim2, PICEngine>& mblock) {
     InjectUniform<Dim2, PICEngine, Drift>(params, mblock, { 1, 2 }, params.ppc0() * 0.5);
+    InjectUniform<Dim2, PICEngine, Drift>(params, mblock, { 3, 4 }, params.ppc0() * 0.5);
   }
 }    // namespace ntt
 
