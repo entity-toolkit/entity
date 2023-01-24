1e4aeffcb07d32245ea438ea524305400b3153a1
hayk <haykh.astro@gmail.com>
2023-01-24 02:43:49 -0500

    🎨 minor reformatting + version bump + tasklist upd

--- TASKLIST.md
+++ TASKLIST.md
@@ -5,13 +5,13 @@
 - [ ] add diagnostics for nans in fields and particles
 - [ ] add gravitationally bound atmosphere
 - [x] rewrite UniformInjector with global random pool
-- [ ] add particle deletion routine
+- [?] add particle deletion routine
 - [ ] make more user-friendly and understandable boundary conditions
 - [ ] refine output
 - [ ] fix bugs in `nttiny`
   - [x] delete plot removes all labels
   - [ ] add particles to `nttiny`
-  - [ ] state should also save skip interval
+  - [x] state should also save skip interval
   - [ ] (?) add autosave state
   - [x] why last cell does not work?
 - [ ] add different moments (momX, momY, momZ, meanGamma)
--- src/framework/particles.h
+++ src/framework/particles.h
@@ -28,12 +28,13 @@ namespace ntt {
 
     /**
      * @brief Constructor for the particle species container.
-     * @param index species index (meshblock::particles vector index + 1).
-     * @param label species label.
-     * @param m species mass.
-     * @param ch species charge.
-     * @param maxnpart max number of allocated particles.
-     * @param pusher pusher assigned for the species.
+     *
+     * @param index The index of the species in the meshblock::particles vector (index + 1).
+     * @param label The label for the species.
+     * @param m The mass of the species.
+     * @param ch The charge of the species.
+     * @param maxnpart The maximum number of allocated particles for the species.
+     * @param pusher The pusher assigned for the species.
      */
     ParticleSpecies(const int&            index,
                     const std::string&    label,
@@ -41,67 +42,91 @@ namespace ntt {
                     const float&          ch,
                     const std::size_t&    maxnpart,
                     const ParticlePusher& pusher);
+
     /**
      * @brief Constructor for the particle species container which deduces the pusher itself.
+     *
      * @overload
-     * @param index species index (meshblock::particles vector index + 1).
-     * @param label species label.
-     * @param m species mass.
-     * @param ch species charge.
-     * @param maxnpart max number of allocated particles.
+     * @param index The index of the species in the meshblock::particles vector (index + 1).
+     * @param label The label for the species.
+     * @param m The mass of the species.
+     * @param ch The charge of the species.
+     * @param maxnpart The maximum number of allocated particles for the species.
      */
     ParticleSpecies(
       const int&, const std::string&, const float&, const float&, const std::size_t&);
+
     /**
      * @brief Copy constructor for the particle species container.
+     *
      * @overload
-     * @param spec
+     * @param spec The particle species to copy from.
      */
     ParticleSpecies(const ParticleSpecies&) = default;
+
+    /**
+     * @brief Destructor for the particle species container.
+     */
     ~ParticleSpecies()                      = default;
 
     /**
      * @brief Get the species label.
+     *
+     * @return The species label as a std::string.
      */
     [[nodiscard]] auto label() const -> std::string {
       return m_label;
     }
+
     /**
      * @brief Get the species mass.
+     *
+     * @return The species mass as a float.
      */
     [[nodiscard]] auto mass() const -> float {
       return m_mass;
     }
+
     /**
      * @brief Get the species charge.
+     *
+     * @return The species charge as a float.
      */
     [[nodiscard]] auto charge() const -> float {
       return m_charge;
     }
+
     /**
      * @brief Get the max number of allocated particles.
+     *
+     * @return The maximum number of allocated particles as a std::size_t.
      */
     [[nodiscard]] auto maxnpart() const -> std::size_t {
       return m_maxnpart;
     }
+
     /**
      * @brief Get the pusher assigned for the species.
+     *
+     * @return The pusher assigned for the species as a ParticlePusher.
      */
     [[nodiscard]] auto pusher() const -> ParticlePusher {
       return m_pusher;
     }
+
     /**
      * @brief Get the species index.
+     *
+     * @return The index of the species in the meshblock::particles vector as an int.
      */
     [[nodiscard]] auto index() const -> int {
       return m_index;
     }
   };
-
   /**
    * @brief Container class to carry particle information for a specific species.
-   * @tparam D Dimension.
-   * @tparam S Simulation engine.
+   * @tparam D The dimension of the simulation.
+   * @tparam S The simulation engine being used.
    */
   template <Dimension D, SimulationEngine S>
   struct Particles : public ParticleSpecies {
@@ -111,62 +136,80 @@ namespace ntt {
 
   public:
     /**
-     * 1D arrays with particle data
-     * TODO: Perhaps try VPIC-style arrays.
+     * Arrays containing particle data.
      */
-    // Cell number of the current particle.
+    // Cell indices of the current particle.
     array_t<int*>           i1, i2, i3;
-    // host mirrors for cell index.
-    array_mirror_t<int*>    i1_h, i2_h, i3_h;
-    // Displacement of a particle within the shell.
+    // Displacement of a particle within the cell.
     array_t<float*>         dx1, dx2, dx3;
-    // host mirrors for the displacements.
-    array_mirror_t<float*>  dx1_h, dx2_h, dx3_h;
     // Three spatial components of the covariant 4-velocity (physical units).
     array_t<real_t*>        ux1, ux2, ux3;
-    // host mirrors for the 4-velocities.
-    array_mirror_t<real_t*> ux1_h, ux2_h, ux3_h;
     // Particle weights.
     array_t<float*>         weight;
-
     // Additional variables (specific to different cases).
     // previous coordinates (GR specific)
     array_t<real_t*>        i1_prev, i2_prev, i3_prev;
     array_t<real_t*>        dx1_prev, dx2_prev, dx3_prev;
     // phi coordinate (for axisymmetry)
     array_t<real_t*>        phi;
-
+    // Array to track whether a particle is dead or not.
     array_t<bool*>          is_dead;
+
+    // Host mirrors for cell indices.
+    array_mirror_t<int*>    i1_h, i2_h, i3_h;
+    // Host mirrors for displacement.
+    array_mirror_t<float*>  dx1_h, dx2_h, dx3_h;
+    // Host mirrors for the 4-velocities.
+    array_mirror_t<real_t*> ux1_h, ux2_h, ux3_h;
+    // Host mirrors for the particle weights.
+    array_mirror_t<float*>  weight_h;
+    // host mirrors for previous coordinates
+    array_mirror_t<real_t*> i1_prev_h, i2_prev_h, i3_prev_h;
+    array_mirror_t<real_t*> dx1_prev_h, dx2_prev_h, dx3_prev_h;
+    // host mirrors for phi
+    array_mirror_t<real_t*> phi_h;
+    // host mirrors for is_dead
+    array_mirror_t<bool*>   is_dead_h;
+    // host mirrors for is_dead
+    array_mirror_t<bool*>   is_dead_h;
 
     /**
      * @brief Constructor for the particle container.
-     * @param index species index (meshblock::particles vector index + 1).
-     * @param label species label.
-     * @param m species mass.
-     * @param ch species charge.
-     * @param maxnpart max number of allocated particles.
+     * @param index The index of the species in the meshblock::particles vector (index + 1).
+     * @param label The label for the species.
+     * @param m The mass of the species.
+     * @param ch The charge of the species.
+     * @param maxnpart The maximum number of allocated particles for the species.
      */
-    Particles(const int&, const std::string&, const float&, const float&, const std::size_t&);
+    Particles(const int&         index,
+              const std::string& label,
+              const float&       m,
+              const float&       ch,
+              const std::size_t& maxnpart);
+
     /**
      * @brief Constructor for the particle container.
      * @overload
-     * @param spec species container.
+     * @param spec The particle species container.
      */
-    Particles(const ParticleSpecies&);
+    Particles(const ParticleSpecies& spec);
     ~Particles() = default;
 
     /**
      * @brief Loop over all active particles.
-     * @returns 1D Kokkos range policy of size of `npart`.
+     * @returns A 1D Kokkos range policy of size of `npart`.
      */
     auto               rangeActiveParticles() -> range_t<Dim1>;
+
     /**
      * @brief Loop over all particles.
-     * @returns 1D Kokkos range policy of size of `npart`.
+     * @returns A 1D Kokkos range policy of size of `npart`.
      */
     auto               rangeAllParticles() -> range_t<Dim1>;
+
     /**
      * @brief Get the number of active particles.
+     * @return The number of active particles as a std::size_t.
      */
     [[nodiscard]] auto npart() const -> std::size_t {
       return m_npart;
@@ -174,26 +217,32 @@ namespace ntt {
 
     /**
      * @brief Set the number of particles.
-     * @param npart number of particles.
+     * @param npart The number of particles as a std::size_t.
      */
-    void setNpart(const std::size_t& N) {
-      NTTHostErrorIf(N > maxnpart(),
+    void setNpart(const std::size_t& npart) {
+      NTTHostErrorIf(npart > maxnpart(),
                      fmt::format("Trying to set npart to {} which is larger than maxnpart {}.",
-                                 N,
+                                 npart,
                                  maxnpart()));
-      m_npart = N;
+      m_npart = npart;
     }
 
     /**
      * @brief Synchronize data from device to host.
+     * Synchronize all the arrays that have host mirrors.
      */
     void SynchronizeHostDevice();
 
     /**
-     * @brief Remove dead particles by sorting and reducing the `npart`.
-     *
+     * @brief Count the number of living particles
+     * @return The number of living particles as a std::size_t.
      */
-    void RemoveDead();
+    auto CountLiving() const -> std::size_t;
+
+    /**
+     * @brief Reshuffle dead particles to the end of all arrays.
+     */
+    void ReshuffleDead();
   };
 
 }    // namespace ntt
