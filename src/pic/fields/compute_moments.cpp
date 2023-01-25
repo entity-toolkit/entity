37685c4ef99103faa7fe3c35758c7802baef15aa
haykh <haykh.mfs+github@gmail.com>
2023-01-24 23:59:08 -0500

    🚧 n_dens norm (wip)

--- src/pic/fields/compute_moments.cpp
+++ src/pic/fields/compute_moments.cpp
@@ -1,6 +1,7 @@
 #include "wrapper.h"
 
 #include "fields.h"
+#include "particle_macros.h"
 #include "pic.h"
 
 namespace ntt {
@@ -17,10 +18,11 @@ namespace ntt {
       if (species.mass() != 0.0) {
         Kokkos::parallel_for(
           "ComputeDensity_1D", species.rangeActiveParticles(), Lambda(index_t p) {
-            auto i1          = species.i1(p);
-            auto dens_access = scatter_buff.access();
+            auto   i1          = species.i1(p);
+            auto   dens_access = scatter_buff.access();
+            real_t x1          = get_prtl_x1(species, p);
             for (int i1_ = i1 - smooth + N_GHOSTS; i1_ <= i1 + smooth + N_GHOSTS; ++i1_) {
-              dens_access(i1, fld::dens) += contrib;
+              dens_access(i1, fld::dens) += contrib * mblock.metric.sqrt_det_h({ x1 });
             }
           });
       }
@@ -44,16 +46,19 @@ namespace ntt {
       if (species.mass() != 0.0) {
         Kokkos::parallel_for(
           "ComputeDensity_2D", species.rangeActiveParticles(), Lambda(index_t p) {
-            auto i1          = species.i1(p);
-            auto i2          = species.i2(p);
-            auto dens_access = scatter_buff.access();
+            auto   i1          = species.i1(p);
+            auto   i2          = species.i2(p);
+            auto   dens_access = scatter_buff.access();
+            real_t x1          = get_prtl_x1(species, p);
+            real_t x2          = get_prtl_x2(species, p);
             for (int i2_ { IMIN(IMAX(i2 - smooth + N_GHOSTS, 0), ni2 + 2 * N_GHOSTS) };
                  i2_ <= IMIN(IMAX(i2 + smooth + N_GHOSTS, 0), ni2 + 2 * N_GHOSTS);
                  ++i2_) {
               for (int i1_ { IMIN(IMAX(i1 - smooth + N_GHOSTS, 0), ni1 + 2 * N_GHOSTS) };
                    i1_ <= IMIN(IMAX(i1 + smooth + N_GHOSTS, 0), ni1 + 2 * N_GHOSTS);
                    ++i1_) {
-                dens_access(i1_, i2_, fld::dens) += contrib;
+                dens_access(i1_, i2_, fld::dens)
+                  += contrib * mblock.metric.sqrt_det_h({ x1, x2 });
               }
             }
           });
@@ -76,14 +81,18 @@ namespace ntt {
       if (species.mass() != 0.0) {
         Kokkos::parallel_for(
           "ComputeDensity_3D", species.rangeActiveParticles(), Lambda(index_t p) {
-            auto i1          = species.i1(p);
-            auto i2          = species.i2(p);
-            auto i3          = species.i3(p);
-            auto dens_access = scatter_buff.access();
+            auto   i1          = species.i1(p);
+            auto   i2          = species.i2(p);
+            auto   i3          = species.i3(p);
+            auto   dens_access = scatter_buff.access();
+            real_t x1          = get_prtl_x1(species, p);
+            real_t x2          = get_prtl_x2(species, p);
+            real_t x3          = get_prtl_x3(species, p);
             for (int i3_ = i3 - smooth + N_GHOSTS; i3_ <= i3 + smooth + N_GHOSTS; ++i3_) {
               for (int i2_ = i2 - smooth + N_GHOSTS; i2_ <= i2 + smooth + N_GHOSTS; ++i2_) {
                 for (int i1_ = i1 - smooth + N_GHOSTS; i1_ <= i1 + smooth + N_GHOSTS; ++i1_) {
-                  dens_access(i1_, i2_, i3_, fld::dens) += contrib;
+                  dens_access(i1_, i2_, i3_, fld::dens)
+                    += contrib * mblock.metric.sqrt_det_h({ x1, x2, x3 });
                 }
               }
             }
