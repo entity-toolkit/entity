b72611471ce750bd2f8a0f2b937cf9be034e009c
hayk <haykh.astro@gmail.com>
2023-05-31 21:03:20 -0400

    ✨ vector/coordinate comparison

--- src/framework/utils/qmath.h
+++ src/framework/utils/qmath.h
@@ -11,7 +11,7 @@ namespace ntt {
    * @brief Function to compare two float values.
    * @param a First value.
    * @param b Second value.
-   * @param epsilon Accuracy.
+   * @param epsilon Accuracy [optional].
    * @returns true/false.
    */
   Inline bool AlmostEqual(float a, float b, float epsilon = 0.00001f) {
@@ -32,7 +32,7 @@ namespace ntt {
    * @brief Function to compare two double values.
    * @param a First value.
    * @param b Second value.
-   * @param epsilon Accuracy.
+   * @param epsilon Accuracy [optional].
    * @returns true/false.
    */
   Inline bool AlmostEqual(double a, double b, double epsilon = 1e-8) {
@@ -52,6 +52,35 @@ namespace ntt {
       return (diff <= (math::max(a, b) * epsilon));
     }
   }
+
+  /**
+   * @brief Function to compare two vectors or coordinate vectors.
+   * @param a First vector.
+   * @param b Second vector.
+   * @param epsilon Accuracy [optional].
+   * @returns true/false.
+   */
+  template <Dimension D>
+  Inline bool AlmostEqual(const vec_t<D>& a,
+                          const vec_t<D>& b,
+                          real_t          epsilon
+                          = std::is_same<real_t, float>::value ? 0.00001f : 1e-8) {
+    for (auto i { 0 }; i < static_cast<short>(D); ++i) {
+      if (!AlmostEqual(a[i], b[i], epsilon)) {
+        return false;
+      }
+    }
+    return true;
+  }
 }    // namespace ntt
 
+template bool ntt::AlmostEqual<ntt::Dim1>(const ntt::vec_t<ntt::Dim1>&,
+                                          const ntt::vec_t<ntt::Dim1>&,
+                                          real_t);
+template bool ntt::AlmostEqual<ntt::Dim2>(const ntt::vec_t<ntt::Dim2>&,
+                                          const ntt::vec_t<ntt::Dim2>&,
+                                          real_t);
+template bool ntt::AlmostEqual<ntt::Dim3>(const ntt::vec_t<ntt::Dim3>&,
+                                          const ntt::vec_t<ntt::Dim3>&,
+                                          real_t);
 #endif
