00733d5b7114a13e487219afe228ae14327625a6
hayk <haykh.astro@gmail.com>
2023-05-31 23:34:45 -0400

    ✅ full metric tests

---
+++ tests/metric-comp-sr-mink.cpp
@@ -0,0 +1,73 @@
+#include "wrapper.h"
+
+#include METRIC_HEADER
+#include "utils/qmath.h"
+
+#include <cstdio>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+auto main(int argc, char* argv[]) -> int {
+  Kokkos::initialize(argc, argv);
+  try {
+    const auto resolution = std::vector<unsigned int>({ 200, 500, 100 });
+    const auto extent     = std::vector<real_t>({ -15.0, 15.0, -75.0, 0.0, -5.0, 10.0 });
+
+    auto       params     = new real_t[2];
+    ntt::Metric<ntt::Dim3> metric(resolution, extent, params);
+    delete[] params;
+
+    {
+      /* ------------------------- Test components of h_ij ------------------------ */
+      auto range = ntt::CreateRangePolicy<ntt::Dim3>(
+        { 0, 0, 0 }, { (int)resolution[0], (int)resolution[1], (int)resolution[3] });
+      int correct;
+      Kokkos::parallel_reduce(
+        "Metric components",
+        range,
+        Lambda(ntt::index_t i1, ntt::index_t i2, ntt::index_t i3, int& correct_l) {
+          const auto              i1_ = static_cast<real_t>(i1);
+          const auto              i2_ = static_cast<real_t>(i2);
+          const auto              i3_ = static_cast<real_t>(i3);
+          ntt::coord_t<ntt::Dim3> xi { i1_ + HALF, i2_ + HALF, i3_ + HALF };
+          ntt::coord_t<ntt::Dim3> xph { ZERO };
+
+          const auto              h_11        = metric.h_11(xi);
+          const auto              h_22        = metric.h_22(xi);
+          const auto              h_33        = metric.h_33(xi);
+
+          const auto              h_11_expect = ONE;
+          const auto              h_22_expect = ONE;
+          const auto              h_33_expect = ONE;
+
+          ntt::vec_t<ntt::Dim3>   h_ij_temp { ZERO }, h_ij_predict { ZERO };
+          metric.v3_Cov2PhysCov(xi, { h_11, h_22, h_33 }, h_ij_temp);
+          metric.v3_Cov2PhysCov(xi, h_ij_temp, h_ij_predict);
+
+          const auto h_11isCorrect = ntt::AlmostEqual(h_ij_predict[0], h_11_expect);
+          const auto h_22isCorrect = ntt::AlmostEqual(h_ij_predict[1], h_22_expect);
+          const auto h_33isCorrect = ntt::AlmostEqual(h_ij_predict[2], h_33_expect);
+
+          const auto all_correct   = h_11isCorrect && h_22isCorrect && h_33isCorrect;
+
+          correct_l                = correct_l && all_correct;
+          if (!all_correct) {
+            printf("i1, i2, i3 = %lu, %lu, %lu\n", i1, i2, i3);
+            printf("h_11 = %f [%f]\n", h_ij_predict[0], h_11_expect);
+            printf("h_22 = %f [%f]\n", h_ij_predict[1], h_22_expect);
+            printf("h_33 = %f [%f]\n", h_ij_predict[2], h_33_expect);
+          }
+        },
+        Kokkos::LAnd<int>(correct));
+      (!correct) ? throw std::logic_error("Metric is incorrect") : (void)0;
+    }
+  } catch (std::exception& err) {
+    std::cerr << err.what() << std::endl;
+    Kokkos::finalize();
+    return -1;
+  }
+  Kokkos::finalize();
+
+  return 0;
+}
