e15a2fb867dd901e0123f7ac2b8481e3161e3552
hayk <haykh.astro@gmail.com>
2024-04-18 01:22:54 -0400

    tests

--- src/kernels/tests/gca_pusher.cpp
+++ src/kernels/tests/gca_pusher.cpp
@@ -1,4 +1,3 @@
-#include "kernels/particle_pusher_sr.hpp"
 #include "enums.h"
 #include "global.h"
 
@@ -10,6 +9,8 @@
 #include "metrics/minkowski.h"
 #include "metrics/qspherical.h"
 #include "metrics/spherical.h"
+
+#include "kernels/particle_pusher_sr.hpp"
 
 #include <Kokkos_Core.hpp>
 #include <Kokkos_ScatterView.hpp>
@@ -41,15 +42,15 @@ void put_value(array_t<T*>& arr, T v, index_t p) {
 
 inline static constexpr auto epsilon = std::numeric_limits<real_t>::epsilon();
 
-struct Pgen{
-  static constexpr auto is_pgen {true};
+struct Pgen {
+  static constexpr auto is_pgen { true };
 };
 
 template <SimEngine::type S, typename M>
 void testGCAPusher(const std::vector<std::size_t>&      res,
-                         const boundaries_t<real_t>&          ext,
-                         const std::map<std::string, real_t>& params = {},
-                         const real_t                         acc    = ONE) {
+                   const boundaries_t<real_t>&          ext,
+                   const std::map<std::string, real_t>& params = {},
+                   const real_t                         acc    = ONE) {
   static_assert(M::Dim == 3);
   errorIf(res.size() != M::Dim, "res.size() != M::Dim");
 
@@ -72,19 +73,19 @@ void testGCAPusher(const std::vector<std::size_t>&      res,
   auto coeff = real_t { 1.0 };
   auto dt    = real_t { 1.0 };
 
-  const auto        range_ext = CreateRangePolicy<Dim::_3D>(
+  const auto range_ext = CreateRangePolicy<Dim::_3D>(
     { 0, 0, 0 },
     { res[0] + 2 * N_GHOSTS, res[1] + 2 * N_GHOSTS, res[2] + 2 * N_GHOSTS });
 
-  auto              emfield = ndfield_t<Dim::_3D, 6> { "emfield",
-                                                       res[0] + 2 * N_GHOSTS,
-                                                       res[1] + 2 * N_GHOSTS,
-                                                       res[2] + 2 * N_GHOSTS };
+  auto emfield = ndfield_t<Dim::_3D, 6> { "emfield",
+                                          res[0] + 2 * N_GHOSTS,
+                                          res[1] + 2 * N_GHOSTS,
+                                          res[2] + 2 * N_GHOSTS };
 
   Kokkos::parallel_for(
     "init 3D",
     range_ext,
-    Lambda(index_t i1, index_t i2, index_t i3 ) {
+    Lambda(index_t i1, index_t i2, index_t i3) {
       emfield(i1, i2, i3, em::ex1) = 0.0;
       emfield(i1, i2, i3, em::ex2) = 0.0;
       emfield(i1, i2, i3, em::ex3) = 0.0;
@@ -132,20 +133,23 @@ void testGCAPusher(const std::vector<std::size_t>&      res,
   put_value<real_t>(ux3, (real_t)(3.0), 1);
   put_value<short>(tag, ParticleTag::alive, 1);
 
-// Particle boundaries
+  // Particle boundaries
   auto boundaries = boundaries_t<PrtlBC> {};
-    boundaries = {
-      { PrtlBC::PERIODIC, PrtlBC::PERIODIC},
-      { PrtlBC::PERIODIC, PrtlBC::PERIODIC},
-      { PrtlBC::PERIODIC, PrtlBC::PERIODIC}
-    };
-  
-  auto pgen = Pgen{};
+  boundaries      = {
+    {PrtlBC::PERIODIC, PrtlBC::PERIODIC},
+    {PrtlBC::PERIODIC, PrtlBC::PERIODIC},
+    {PrtlBC::PERIODIC, PrtlBC::PERIODIC}
+  };
 
-      auto pusher = Pusher_kernel<Minkowski<Dim::_3D>, Pgen, Boris_GCA_t, false>(emfield, i1, i2, i3, i1, i2, i3, dx1, dx2, dx3,
-                  dx1, dx2, dx3, ux1, ux2, ux3, phi, tag, metric, pgen,
-                  (real_t)0.0, coeff, dt, nx1, nx2, nx3, boundaries, (real_t)10.0, (real_t)1.0, (real_t)1.0);
-
+  auto pgen = Pgen {};
+  // clang-format off
+  auto pusher = Pusher_kernel<Minkowski<Dim::_3D>, Pgen, Boris_GCA_t, false>(
+                              emfield, i1, i2, i3, i1, i2, i3, dx1, dx2, dx3,
+                                       dx1, dx2, dx3, ux1, ux2, ux3, phi, tag, 
+                                       metric, pgen,
+                                       (real_t)0.0, coeff, dt, nx1, nx2, nx3, boundaries, 
+                                       (real_t)10.0, (real_t)1.0, (real_t)1.0);
+  // clang-format on
 }
 
 auto main(int argc, char* argv[]) -> int {
@@ -160,7 +164,7 @@ auto main(int argc, char* argv[]) -> int {
         10,
         10
     },
-      { { 0.0, 10.0 }, { 0.0, 10.0 } , { 0.0, 10.0 } },
+      { { 0.0, 10.0 }, { 0.0, 10.0 }, { 0.0, 10.0 } },
       {});
 
   } catch (std::exception& e) {
