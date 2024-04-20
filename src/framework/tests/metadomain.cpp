090a2350867575c0cabcc6df25c5c82b3da87209
hayk <haykh.astro@gmail.com>
2024-04-20 05:20:28 -0400

    metadomain test built

--- src/framework/tests/metadomain.cpp
+++ src/framework/tests/metadomain.cpp
@@ -43,149 +43,151 @@ auto main(int argc, char* argv[]) -> int {
         4, { -1, -1 },
          res, extent, fldsbc, prtlbc, params, {}
       };
-      // std::size_t nx1 { 0 }, nx2 { 0 };
-      // raise::ErrorIf(metadomain.mesh().n_active() != res,
-      //                "Mesh::n_active() failed",
-      //                HERE);
-      // raise::ErrorIf(metadomain.mesh().extent() != extent,
-      //                "Mesh::extent() failed",
-      //                HERE);
-      // raise::ErrorIf(metadomain.mesh().flds_bc() != fldsbc,
-      //                "Mesh::flds_bc() failed",
-      //                HERE);
-      // raise::ErrorIf(metadomain.mesh().prtl_bc() != prtlbc,
-      //                "Mesh::prtl_bc() failed",
-      //                HERE);
-      // for (unsigned int idx = 0; idx < 4; ++idx) {
-      //   auto self  = metadomain.idx2subdomain(idx);
-      //   nx1       += self.mesh.n_active(in::x1);
-      //   nx2       += self.mesh.n_active(in::x2);
-      //   raise::ErrorIf(self.index() != idx, "Domain::index() failed", HERE);
+      std::size_t nx1 { 0 }, nx2 { 0 };
+      raise::ErrorIf(metadomain.mesh().n_active() != res,
+                     "Mesh::n_active() failed",
+                     HERE);
+      raise::ErrorIf(metadomain.mesh().extent() != extent,
+                     "Mesh::extent() failed",
+                     HERE);
+      raise::ErrorIf(metadomain.mesh().flds_bc() != fldsbc,
+                     "Mesh::flds_bc() failed",
+                     HERE);
+      raise::ErrorIf(metadomain.mesh().prtl_bc() != prtlbc,
+                     "Mesh::prtl_bc() failed",
+                     HERE);
+      for (unsigned int idx = 0; idx < 4; ++idx) {
+        auto& self  = metadomain.idx2subdomain(idx);
+        nx1        += self.mesh.n_active(in::x1);
+        nx2        += self.mesh.n_active(in::x2);
+        raise::ErrorIf(self.index() != idx, "Domain::index() failed", HERE);
 
-      //   // check field allocations
-      //   raise::ErrorIf(self.fields.em.extent(0) != self.mesh.n_all(in::x1),
-      //                  "Domain::fields.em(0) failed",
-      //                  HERE);
-      //   raise::ErrorIf(self.fields.em.extent(1) != self.mesh.n_all(in::x2),
-      //                  "Domain::fields.em(1) failed",
-      //                  HERE);
-      //   raise::ErrorIf(self.fields.em.extent(2) != 6,
-      //                  "Domain::fields.em(2) failed",
-      //                  HERE);
+        // check field allocations
+        raise::ErrorIf(self.fields.em.extent(0) != self.mesh.n_all(in::x1),
+                       "Domain::fields.em(0) failed",
+                       HERE);
+        raise::ErrorIf(self.fields.em.extent(1) != self.mesh.n_all(in::x2),
+                       "Domain::fields.em(1) failed",
+                       HERE);
+        raise::ErrorIf(self.fields.em.extent(2) != 6,
+                       "Domain::fields.em(2) failed",
+                       HERE);
 
-      //   // check current allocations
-      //   raise::ErrorIf(self.fields.cur.extent(0) != self.mesh.n_all(in::x1),
-      //                  "Domain::fields.cur(0) failed",
-      //                  HERE);
-      //   raise::ErrorIf(self.fields.cur.extent(1) != self.mesh.n_all(in::x2),
-      //                  "Domain::fields.cur(1) failed",
-      //                  HERE);
-      //   raise::ErrorIf(self.fields.cur.extent(2) != 3,
-      //                  "Domain::fields.cur(2) failed",
-      //                  HERE);
+        // check current allocations
+        raise::ErrorIf(self.fields.cur.extent(0) != self.mesh.n_all(in::x1),
+                       "Domain::fields.cur(0) failed",
+                       HERE);
+        raise::ErrorIf(self.fields.cur.extent(1) != self.mesh.n_all(in::x2),
+                       "Domain::fields.cur(1) failed",
+                       HERE);
+        raise::ErrorIf(self.fields.cur.extent(2) != 3,
+                       "Domain::fields.cur(2) failed",
+                       HERE);
 
-      //   // check em0 allocations (should be unallocated)
-      //   raise::ErrorIf(self.fields.em0.extent(0) != 0,
-      //                  "Domain::fields.em0(0) failed",
-      //                  HERE);
-      //   raise::ErrorIf(self.fields.em0.extent(1) != 0,
-      //                  "Domain::fields.em0(1) failed",
-      //                  HERE);
-      //   raise::ErrorIf(self.fields.aux.extent(0) != 0,
-      //                  "Domain::fields.aux(0) failed",
-      //                  HERE);
-      //   raise::ErrorIf(self.fields.aux.extent(1) != 0,
-      //                  "Domain::fields.aux(1) failed",
-      //                  HERE);
-      //   raise::ErrorIf(self.fields.cur0.extent(0) != 0,
-      //                  "Domain::fields.cur0(0) failed",
-      //                  HERE);
-      //   raise::ErrorIf(self.fields.cur0.extent(1) != 0,
-      //                  "Domain::fields.cur0(1) failed",
-      //                  HERE);
+        // check em0 allocations (should be unallocated)
+        raise::ErrorIf(self.fields.em0.extent(0) != 0,
+                       "Domain::fields.em0(0) failed",
+                       HERE);
+        raise::ErrorIf(self.fields.em0.extent(1) != 0,
+                       "Domain::fields.em0(1) failed",
+                       HERE);
+        raise::ErrorIf(self.fields.aux.extent(0) != 0,
+                       "Domain::fields.aux(0) failed",
+                       HERE);
+        raise::ErrorIf(self.fields.aux.extent(1) != 0,
+                       "Domain::fields.aux(1) failed",
+                       HERE);
+        raise::ErrorIf(self.fields.cur0.extent(0) != 0,
+                       "Domain::fields.cur0(0) failed",
+                       HERE);
+        raise::ErrorIf(self.fields.cur0.extent(1) != 0,
+                       "Domain::fields.cur0(1) failed",
+                       HERE);
 
-      //   // check boundaries and offsets
-      //   if (idx % 2 == 0) {
-      //     raise::ErrorIf(self.offset_ndomains()[0] != 0,
-      //                    "Domain::offset_ndomains() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.flds_bc_in({ -1, 0 }) != FldsBC::ATMOSPHERE,
-      //                    "Mesh::flds_bc_in() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.prtl_bc_in({ -1, 0 }) != PrtlBC::ATMOSPHERE,
-      //                    "Mesh::prtl_bc_in() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.flds_bc_in({ +1, 0 }) != FldsBC::SYNC,
-      //                    "Mesh::flds_bc_in() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.prtl_bc_in({ +1, 0 }) != PrtlBC::SYNC,
-      //                    "Mesh::prtl_bc_in() failed",
-      //                    HERE);
-      //   } else {
-      //     raise::ErrorIf(self.offset_ndomains()[0] != 1,
-      //                    "Domain::offset_ndomains() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.flds_bc_in({ +1, 0 }) != FldsBC::ABSORB,
-      //                    "Mesh::flds_bc_in() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.prtl_bc_in({ +1, 0 }) != PrtlBC::ABSORB,
-      //                    "Mesh::prtl_bc_in() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.flds_bc_in({ -1, 0 }) != FldsBC::SYNC,
-      //                    "Mesh::flds_bc_in() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.prtl_bc_in({ -1, 0 }) != PrtlBC::SYNC,
-      //                    "Mesh::prtl_bc_in() failed",
-      //                    HERE);
-      //   }
-      //   if (idx < 2) {
-      //     raise::ErrorIf(self.offset_ndomains()[1] != 0,
-      //                    "Domain::offset_ndomains() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.flds_bc_in({ 0, -1 }) != FldsBC::AXIS,
-      //                    "Mesh::flds_bc_in() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.prtl_bc_in({ 0, -1 }) != PrtlBC::AXIS,
-      //                    "Mesh::prtl_bc_in() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.flds_bc_in({ 0, +1 }) != FldsBC::SYNC,
-      //                    "Mesh::flds_bc_in() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.prtl_bc_in({ 0, +1 }) != PrtlBC::SYNC,
-      //                    "Mesh::prtl_bc_in() failed",
-      //                    HERE);
-      //   } else {
-      //     raise::ErrorIf(self.offset_ndomains()[1] != 1,
-      //                    "Domain::offset_ndomains() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.flds_bc_in({ 0, +1 }) != FldsBC::AXIS,
-      //                    "Mesh::flds_bc_in() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.prtl_bc_in({ 0, +1 }) != PrtlBC::AXIS,
-      //                    "Mesh::prtl_bc_in() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.flds_bc_in({ 0, -1 }) != FldsBC::SYNC,
-      //                    "Mesh::flds_bc_in() failed",
-      //                    HERE);
-      //     raise::ErrorIf(self.mesh.prtl_bc_in({ 0, -1 }) != PrtlBC::SYNC,
-      //                    "Mesh::prtl_bc_in() failed",
-      //                    HERE);
-      //   }
-      //   for (auto& direction : dir::Directions<Dim::_2D>::all) {
-      //     raise::ErrorIf(
-      //       self.neighbor_in(direction)->neighbor_in(direction)->index() !=
-      //         self.index(),
-      //       "Domain::neighbor_in() failed",
-      //       HERE);
-      //     raise::ErrorIf(
-      //       self.neighbor_in(direction)->neighbor_in(-direction)->index() !=
-      //         self.index(),
-      //       "Domain::neighbor_in() failed",
-      //       HERE);
-      //   }
-      // }
-      // raise::ErrorIf(nx1 != 2 * res[0], "Mesh::n_active() failed", HERE);
-      // raise::ErrorIf(nx2 != 2 * res[1], "Mesh::n_active() failed", HERE);
+        // check boundaries and offsets
+        if (idx % 2 == 0) {
+          raise::ErrorIf(self.offset_ndomains()[0] != 0,
+                         "Domain::offset_ndomains() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.flds_bc_in({ -1, 0 }) != FldsBC::ATMOSPHERE,
+                         "Mesh::flds_bc_in() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.prtl_bc_in({ -1, 0 }) != PrtlBC::ATMOSPHERE,
+                         "Mesh::prtl_bc_in() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.flds_bc_in({ +1, 0 }) != FldsBC::SYNC,
+                         "Mesh::flds_bc_in() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.prtl_bc_in({ +1, 0 }) != PrtlBC::SYNC,
+                         "Mesh::prtl_bc_in() failed",
+                         HERE);
+        } else {
+          raise::ErrorIf(self.offset_ndomains()[0] != 1,
+                         "Domain::offset_ndomains() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.flds_bc_in({ +1, 0 }) != FldsBC::ABSORB,
+                         "Mesh::flds_bc_in() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.prtl_bc_in({ +1, 0 }) != PrtlBC::ABSORB,
+                         "Mesh::prtl_bc_in() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.flds_bc_in({ -1, 0 }) != FldsBC::SYNC,
+                         "Mesh::flds_bc_in() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.prtl_bc_in({ -1, 0 }) != PrtlBC::SYNC,
+                         "Mesh::prtl_bc_in() failed",
+                         HERE);
+        }
+        if (idx < 2) {
+          raise::ErrorIf(self.offset_ndomains()[1] != 0,
+                         "Domain::offset_ndomains() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.flds_bc_in({ 0, -1 }) != FldsBC::AXIS,
+                         "Mesh::flds_bc_in() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.prtl_bc_in({ 0, -1 }) != PrtlBC::AXIS,
+                         "Mesh::prtl_bc_in() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.flds_bc_in({ 0, +1 }) != FldsBC::SYNC,
+                         "Mesh::flds_bc_in() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.prtl_bc_in({ 0, +1 }) != PrtlBC::SYNC,
+                         "Mesh::prtl_bc_in() failed",
+                         HERE);
+        } else {
+          raise::ErrorIf(self.offset_ndomains()[1] != 1,
+                         "Domain::offset_ndomains() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.flds_bc_in({ 0, +1 }) != FldsBC::AXIS,
+                         "Mesh::flds_bc_in() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.prtl_bc_in({ 0, +1 }) != PrtlBC::AXIS,
+                         "Mesh::prtl_bc_in() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.flds_bc_in({ 0, -1 }) != FldsBC::SYNC,
+                         "Mesh::flds_bc_in() failed",
+                         HERE);
+          raise::ErrorIf(self.mesh.prtl_bc_in({ 0, -1 }) != PrtlBC::SYNC,
+                         "Mesh::prtl_bc_in() failed",
+                         HERE);
+        }
+        for (auto& direction : dir::Directions<Dim::_2D>::all) {
+          const auto& neighbor1 = metadomain.idx2subdomain(
+            self.neighbor_idx_in(direction));
+          const auto& neighbor2 = metadomain.idx2subdomain(
+            neighbor1.neighbor_idx_in(direction));
+          const auto& neighbor3 = metadomain.idx2subdomain(
+            neighbor1.neighbor_idx_in(-direction));
+          raise::ErrorIf(neighbor2.index() != self.index(),
+                         "Domain::neighbor_in() failed",
+                         HERE);
+          raise::ErrorIf(neighbor3.index() != self.index(),
+                         "Domain::neighbor_in() failed",
+                         HERE);
+        }
+      }
+      raise::ErrorIf(nx1 != 2 * res[0], "Mesh::n_active() failed", HERE);
+      raise::ErrorIf(nx2 != 2 * res[1], "Mesh::n_active() failed", HERE);
     }
   } catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
