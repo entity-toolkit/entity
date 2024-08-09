5eeb23d85cbe6928f3f2b36b66934a11bd47c04b
jmahlmann <mahlmann@princeton.edu>
2024-08-08 23:30:52 -0400

    Ongoing.

--- setups/srpic/magnetar/pgen.hpp
+++ setups/srpic/magnetar/pgen.hpp
@@ -1057,15 +1057,10 @@ namespace user {
 
         }
   
-void CustomFieldOutput(const std::string& name, ndfield_t<M::Dim, 6> buffer, std::size_t index, const range_t<M::Dim> range) {
+void CustomFieldOutput(const std::string& name, ndfield_t<M::Dim, 6> buffer, std::size_t index, const Domain<S, M> range) {
   if (name == "pploc") {
         Kokkos::deep_copy(Kokkos::subview(buffer, Kokkos::ALL, Kokkos::ALL, index),
                           cbuff);
-    // Kokkos::parallel_for("CustomFieldOutput", range, KOKKOS_LAMBDA(index_t i1, index_t i2) {
-      // buffer(i1, i2, index) = cbuff(i1, i2);
-    // });
-  // } else {
-    // raise::Error("Custom output not provided", HERE);
   } 
   if (name == "pplocsec") {
         Kokkos::deep_copy(Kokkos::subview(buffer, Kokkos::ALL, Kokkos::ALL, index),
