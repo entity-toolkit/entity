1df0cce978d8da8e00a1f0c7d3154a8cb091585a
Alisa Galishnikova <agalishnikova@worker6021.cm.cluster>
2024-09-24 12:20:06 -0400

    compilable example for grpic

--- setups/grpic/pgen_grpic_example.hpp
+++ setups/grpic/pgen_grpic_example.hpp
@@ -12,7 +12,7 @@ namespace user {
   using namespace ntt;
 
   template <SimEngine::type S, class M>
-  struct PGen : public ProblemGenerator<S, M> {
+  struct PGen : public arch::ProblemGenerator<S, M> {
     // compatibility traits for the problem generator
     static constexpr auto engines { traits::compatible_with<SimEngine::GRPIC>::value };
     static constexpr auto metrics {
@@ -21,13 +21,12 @@ namespace user {
     static constexpr auto dimensions { traits::compatible_with<Dim::_2D>::value };
 
     // for easy access to variables in the child class
-    using ProblemGenerator<S, M>::D;
-    using ProblemGenerator<S, M>::C;
-    using ProblemGenerator<S, M>::params;
-    using ProblemGenerator<S, M>::domain;
+    using arch::ProblemGenerator<S, M>::D;
+    using arch::ProblemGenerator<S, M>::C;
+    using arch::ProblemGenerator<S, M>::params;
 
     inline PGen(SimulationParams& p, const Metadomain<S, M>&) :
-      ProblemGenerator<S, M>(p) {}
+      arch::ProblemGenerator<S, M>(p) {}
 
     inline PGen() {}
   };
