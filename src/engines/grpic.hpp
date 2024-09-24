fc126af8cf4dca5419b346d31bbdb58de5085d0f
Alisa Galishnikova <55898700+alisagk@users.noreply.github.com>
2024-09-24 13:46:21 -0400

    engines/grpic: added relevant libraries

--- src/engines/grpic.hpp
+++ src/engines/grpic.hpp
@@ -13,12 +13,36 @@
 #define ENGINES_GRPIC_GRPIC_H
 
 #include "enums.h"
+#include "global.h"
 
+#include "arch/kokkos_aliases.h"
+#include "arch/traits.h"
+#include "utils/log.h"
+#include "utils/numeric.h"
 #include "utils/timer.h"
 
+#include "archetypes/particle_injector.h"
 #include "framework/domain/domain.h"
+#include "framework/parameters.h"
 
 #include "engines/engine.hpp"
+
+#include "kernels/ampere_gr.hpp"
+#include "kernels/aux_fields_gr.hpp"
+#include "kernels/currents_deposit.hpp"
+#include "kernels/digital_filter.hpp"
+#include "kernels/faraday_gr.hpp"
+#include "kernels/fields_bcs.hpp"
+#include "kernels/particle_moments.hpp"
+#include "kernels/particle_pusher_gr.hpp"
+
+#include "pgen.hpp"
+
+#include <Kokkos_Core.hpp>
+#include <Kokkos_ScatterView.hpp>
+
+#include <string>
+#include <utility>
 
 namespace ntt {
 
@@ -26,6 +50,7 @@ namespace ntt {
   class GRPICEngine : public Engine<SimEngine::GRPIC, M> {
     using Engine<SimEngine::GRPIC, M>::m_params;
     using Engine<SimEngine::GRPIC, M>::m_metadomain;
+
 
   public:
     static constexpr auto S { SimEngine::SRPIC };
