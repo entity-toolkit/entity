77b162f697edb8fc9bfc0f176a027a1622be9824
Alisa Galishnikova <55898700+alisagk@users.noreply.github.com>
2024-10-01 14:00:57 -0400

    parameters: changed to requiring n=2 for GRPIC

--- src/framework/parameters.cpp
+++ src/framework/parameters.cpp
@@ -686,10 +686,10 @@ namespace ntt {
           prtl_bc_enum.push_back({ PrtlBC::PERIODIC, PrtlBC::PERIODIC });
         }
       } else {
-        raise::ErrorIf(flds_bc[0].size() != 1,
+        raise::ErrorIf(flds_bc[0].size() != 2,
                        "invalid `grid.boundaries.fields`",
                        HERE);
-        raise::ErrorIf(prtl_bc[0].size() != 1,
+        raise::ErrorIf(prtl_bc[0].size() != 2,
                        "invalid `grid.boundaries.particles`",
                        HERE);
         flds_bc_enum.push_back(
