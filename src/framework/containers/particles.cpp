ef705b573e6e87264ba9dfae0fc36ab4e6eca018
jmahlmann <mahlmann@princeton.edu>
2024-10-09 15:45:44 -0400

    MPI update.

--- src/framework/containers/particles.cpp
+++ src/framework/containers/particles.cpp
@@ -92,20 +92,13 @@ namespace ntt {
       });
     Kokkos::Experimental::contribute(npart_tag, npart_tag_scatter);
 
-    // auto npart_tag_host = Kokkos::create_mirror_view(npart_tag);
-    // Kokkos::deep_copy(npart_tag_host, npart_tag);
+    auto npart_tag_host = Kokkos::create_mirror_view(npart_tag);
+    Kokkos::deep_copy(npart_tag_host, npart_tag);
 
     // std::vector<std::size_t> npart_tag_vec;
     // for (std::size_t t { 0 }; t < ntags(); ++t) {
     //   npart_tag_vec.push_back(npart_tag_host(t));
     // }
-
-    std::vector<std::size_t> npart_tag_vec;
-    for (std::size_t t { 0 }; t < ntags(); ++t) {
-      npart_tag_vec.push_back(npart_tag(t));
-    }
-    Kokkos::create_mirror_view(npart_tag_vec);
-    Kokkos::deep_copy(npart_tag_vec, npart_tag);
 
     return npart_tag_vec;
   }
