85e6d77f952b0f07e11d377551ab273a84fd38c7
jmahlmann <mahlmann@princeton.edu>
2024-10-15 10:37:47 -0400

    Magnetar update.

--- setups/srpic/magnetar/pgen.hpp
+++ setups/srpic/magnetar/pgen.hpp
@@ -396,6 +396,8 @@ namespace user {
       auto bq_            = this->bq;
       auto dt_            = this->dt;
       auto fid_freq_      = this->fid_freq;
+      auto cbuff2_sc = Kokkos::Experimental::create_scatter_view(cbuff2);
+      auto inv_n0_      = this->inv_n0;
 
          for (std::size_t s { 0 }; s < 6; ++s) {
             if (s == 1 || s == 2 || s == 3) {
@@ -862,6 +864,12 @@ namespace user {
                   weight_perp(ph_p + ph_offset_perp) = weight(p);
                   tag_perp(ph_p + ph_offset_perp) = ParticleTag::alive;
                 }
+
+                auto cbuff2_acc     = cbuff2_sc.access();
+                cbuff2_acc(static_cast<int>(i1(p)), static_cast<int>(i2(p))) += weight(p) * inv_n0_ /
+                    metric.sqrt_det_h({ static_cast<real_t>(i1(p)) + HALF,
+                                        static_cast<real_t>(i2(p)) + HALF });
+
               }
 
             }
@@ -880,6 +888,7 @@ namespace user {
 
          }
 
+        Kokkos::Experimental::contribute(cbuff2, cbuff2_sc);
       } // Resonant scattering kernel
 
    // Pair production kernel (threshold)
