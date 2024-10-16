88a67646e28ebf2bc47dbb52c35acdd99e450f87
StaticObserver <cyysouldancer@gmail.com>
2024-10-16 14:25:52 +0800

    update grpic_1d.hpp

--- src/engines/grpic_1d.hpp
+++ src/engines/grpic_1d.hpp
@@ -46,8 +46,24 @@ namespace ntt {
 
   template <class M>
   class GRPICEngine_1D : public Engine<SimEngine::GRPIC, M> {
-    using Engine<SimEngine::GRPIC, M>::m_params;
-    using Engine<SimEngine::GRPIC, M>::m_metadomain;
+
+    using base_t   = Engine<SimEngine::GRPIC, M>;
+    using pgen_t   = user::PGen<SimEngine::GRPIC, M>;
+    using domain_t = Domain<SimEngine::GRPIC, M>;
+    // constexprs
+    using base_t::pgen_is_ok;
+    // contents
+    using base_t::m_metadomain;
+    using base_t::m_params;
+    using base_t::m_pgen;
+    // methods
+    using base_t::init;
+    // variables
+    using base_t::dt;
+    using base_t::max_steps;
+    using base_t::runtime;
+    using base_t::step;
+    using base_t::time;
 
     auto constexpr D { M::Dim };
 
@@ -55,12 +71,14 @@ namespace ntt {
     static constexpr auto S { SimEngine::GRPIC };
 
     GRPICEngine_1D(SimulationParams& params)
-      : Engine<SimEngine::GRPIC, M> { params } {}
+      : Engine<SimEngine::GRPIC, M> { params } {
+        raise::ErrorIf(D != Dim::_1D, "GRPICEngine_1D only works in 1D", HERE);
+        raise::ErrorIf(M.Lable != "boyer_lindq_tp", "GRPICEngine_1D only works with BoyerLindqTP metric", HERE);
+      }
 
     ~GRPICEngine_1D() = default;
 
     void step_forward(timer::Timers&, Domain<SimEngine::GRPIC, M>&) override {
-        static_assert(D == Dim::1D, "This is 1D GRPIC simulation engine");
         const auto sort_interval = m_params.template get<std::size_t>(
           "particles.sort_interval");
 
@@ -92,6 +110,9 @@ namespace ntt {
 
           timers.start("CurrentFiltering");
           CurrentsFilter(dom);
+          /**  
+           * !CurrentsFilter: Special version for 1D GRPIC needed
+           */
           timers.stop("CurrentFiltering");
 
           timers.start("Communications");
@@ -114,7 +135,7 @@ namespace ntt {
 
         {
           timers.start("Injector");
-          ParticleInjector(dom);
+          DischargeInjector(dom);
           timers.stop("Injector");
         }
     }
@@ -134,8 +155,8 @@ namespace ntt {
         const auto q_ovr_m = species.mass() > ZERO
                                 ? species.charge() / species.mass()
                                 : ZERO;
-        //  coeff = q / m (dt / 2) omegaB0
-        const auto coeff   = q_ovr_m * HALF * dt *
+        //  coeff = q / m dt omegaB0
+        const auto coeff   = q_ovr_m * dt *
                             m_params.template get<real_t>("scales.omegaB0");
         PrtlPusher::type pusher;
         if (species.pusher() == PrtlPusher::FORCEFREE) {
@@ -160,7 +181,7 @@ namespace ntt {
                                                       domain.mesh.n_active(in::x1),
                                                       1e-2,
                                                       30,
-                                                      domain.mesh.n_active(in::x1));
+                                                      domain.mesh.n_active(in::x1)));
       }
     }
 
@@ -178,6 +199,7 @@ namespace ntt {
         if (species.npart() == 0 || cmp::AlmostZero(species.charge())) {
           continue;
         }
+        // clang-format off
         /**  
            * !DepositCurrents_kernel: Special version for 1D GRPIC needed
            */
@@ -205,6 +227,7 @@ namespace ntt {
       const auto n0    = m_params.template get<real_t>("scales.n0");
       const auto B0    = m_params.template get<real_t>("scales.B0");
       const auto coeff = -dt * q0 * n0 / B0;
+      // clang-format off
       Kokkos::parallel_for(
         "Ampere",
         domain.mesh.rangeActiveCells(),
@@ -215,7 +238,7 @@ namespace ntt {
     }
 
 
-};
+}; // class GRPICEngine_1D
 
 } // namespace ntt
 
