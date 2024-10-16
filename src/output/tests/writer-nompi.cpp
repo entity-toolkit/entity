cbeca51456c0717e08cfb00106d053db7a79fe15
hayk <haykh.astro@gmail.com>
2024-10-15 23:19:47 -0400

    proper output readwrite test

--- src/output/tests/writer-nompi.cpp
+++ src/output/tests/writer-nompi.cpp
@@ -16,6 +16,8 @@
 #include <string>
 #include <vector>
 
+using namespace ntt;
+
 void cleanup() {
   namespace fs = std::filesystem;
   fs::path tempfile_path { "test.h5" };
@@ -26,86 +28,128 @@ auto main(int argc, char* argv[]) -> int {
   Kokkos::initialize(argc, argv);
 
   try {
+    constexpr auto nx1    = 10;
+    constexpr auto nx1_gh = nx1 + 2 * N_GHOSTS;
+    constexpr auto nx2    = 10;
+    constexpr auto nx2_gh = nx2 + 2 * N_GHOSTS;
+    constexpr auto nx3    = 10;
+    constexpr auto nx3_gh = nx3 + 2 * N_GHOSTS;
+    constexpr auto i1min  = N_GHOSTS;
+    constexpr auto i2min  = N_GHOSTS;
+    constexpr auto i3min  = N_GHOSTS;
+    constexpr auto i1max  = nx1 + N_GHOSTS;
+    constexpr auto i2max  = nx2 + N_GHOSTS;
+    constexpr auto i3max  = nx3 + N_GHOSTS;
+
+    ndfield_t<Dim::_3D, 3>   field { "fld", nx1_gh, nx2_gh, nx3_gh };
+    std::vector<std::string> field_names;
+
+    {
+      // fill data
+      Kokkos::parallel_for(
+        "fill",
+        CreateRangePolicy<Dim::_3D>({ i1min, i2min, i3min },
+                                    { i1max, i2max, i3max }),
+        Lambda(index_t i1, index_t i2, index_t i3) {
+          field(i1, i2, i3, 0) = i1 + i2 + i3;
+          field(i1, i2, i3, 1) = i1 * i2 / i3;
+          field(i1, i2, i3, 2) = i1 / i2 * i3;
+        });
+    }
+
     adios2::ADIOS adios;
 
-    using namespace ntt;
-    auto writer = out::Writer();
-    writer.init(&adios, "hdf5", "test");
-    writer.defineMeshLayout({ 10, 10, 10 },
-                            { 0, 0, 0 },
-                            { 10, 10, 10 },
-                            false,
-                            Coord::Cart);
-    writer.defineFieldOutputs(SimEngine::SRPIC, { "E", "B", "Rho_1_3", "N_2" });
+    {
+      // write
+      auto writer = out::Writer();
+      writer.init(&adios, "hdf5", "test");
+      writer.defineMeshLayout({ nx1, nx2, nx3 },
+                              { 0, 0, 0 },
+                              { nx1, nx3, nx3 },
+                              false,
+                              Coord::Cart);
+      writer.defineFieldOutputs(SimEngine::SRPIC, { "E", "B", "Rho_1_3", "N_2" });
 
-    ndfield_t<Dim::_3D, 3> field { "fld",
-                                   10 + 2 * N_GHOSTS,
-                                   10 + 2 * N_GHOSTS,
-                                   10 + 2 * N_GHOSTS };
-    Kokkos::parallel_for(
-      "fill",
-      CreateRangePolicy<Dim::_3D>({ N_GHOSTS, N_GHOSTS, N_GHOSTS },
-                                  { 10 + N_GHOSTS, 10 + N_GHOSTS, 10 + N_GHOSTS }),
-      Lambda(index_t i1, index_t i2, index_t i3) {
-        field(i1, i2, i3, 0) = i1 + i2 + i3;
-        field(i1, i2, i3, 1) = i1 * i2 / i3;
-        field(i1, i2, i3, 2) = i1 / i2 * i3;
-      });
-    std::vector<std::string> names;
-    std::vector<std::size_t> addresses;
-    for (auto i = 0; i < 3; ++i) {
-      names.push_back(writer.fieldWriters()[0].name(i));
-      addresses.push_back(i);
+      std::vector<std::size_t> addresses;
+      for (auto i = 0; i < 3; ++i) {
+        field_names.push_back(writer.fieldWriters()[0].name(i));
+        addresses.push_back(i);
+      }
+      writer.beginWriting(10, 123.0);
+      writer.writeField<Dim::_3D, 3>(field_names, field, addresses);
+      writer.endWriting();
+
+      writer.beginWriting(20, 123.4);
+      writer.writeField<Dim::_3D, 3>(field_names, field, addresses);
+      writer.endWriting();
     }
-    writer.beginWriting(0, 0.0);
-    writer.writeField<Dim::_3D, 3>(names, field, addresses);
-    writer.endWriting();
-
-    writer.beginWriting(1, 0.1);
-    writer.writeField<Dim::_3D, 3>(names, field, addresses);
-    writer.endWriting();
 
     {
       // read
-      adios2::ADIOS adios;
-      adios2::IO    io = adios.DeclareIO("read-test");
+      adios2::IO io = adios.DeclareIO("read-test");
       io.SetEngine("hdf5");
       adios2::Engine reader = io.Open("test.h5", adios2::Mode::Read);
 
-      std::size_t step { 0 };
-      long double time { 0.0 };
-      reader.Get(io.InquireVariable<std::size_t>("Step"), step);
-      reader.Get(io.InquireVariable<long double>("Time"), time);
-      raise::ErrorIf(step != 0, "Step is not 0", HERE);
-      raise::ErrorIf(time != 0.0, "Time is not 0.0", HERE);
+      raise::ErrorIf(io.InquireAttribute<unsigned int>("NGhosts").Data()[0] != 0,
+                     "NGhosts is not correct",
+                     HERE);
+      raise::ErrorIf(io.InquireAttribute<std::size_t>("Dimension").Data()[0] != 3,
+                     "Dimension is not correct",
+                     HERE);
 
       for (std::size_t step = 0; reader.BeginStep() == adios2::StepStatus::OK;
            ++step) {
-        std::size_t                   step_read;
-        adios2::Variable<std::size_t> stepVar = io.InquireVariable<std::size_t>(
-          "Step");
-        reader.Get(stepVar, step_read);
-
+        std::size_t step_read;
         long double time_read;
+
+        reader.Get(io.InquireVariable<std::size_t>("Step"), step_read);
         reader.Get(io.InquireVariable<long double>("Time"), time_read);
-        raise::ErrorIf(step_read != step, "Step is not correct", HERE);
-        raise::ErrorIf((float)time_read != (float)step / 10.0f,
+        raise::ErrorIf(step_read != (step + 1) * 10, "Step is not correct", HERE);
+        raise::ErrorIf((float)time_read != 123 + (float)step * 0.4f,
                        "Time is not correct",
                        HERE);
 
-        for (const auto& name : names) {
-          auto data = io.InquireVariable<real_t>(name);
-          raise::ErrorIf(data.Shape().size() != 3,
-                         fmt::format("%s is not 3D", name.c_str()),
-                         HERE);
+        array_t<real_t***> field_read { "fld_read", nx1, nx2, nx3 };
+        auto field_read_h = Kokkos::create_mirror_view(field_read);
 
-          auto        dims = data.Shape();
-          std::size_t nx1  = dims[0];
-          std::size_t nx2  = dims[1];
-          std::size_t nx3  = dims[2];
-          raise::ErrorIf((nx1 != 10) || (nx2 != 10) || (nx3 != 10),
-                         fmt::format("%s is not 10x10x10", name.c_str()),
-                         HERE);
+        int cntr = 0;
+        for (const auto& name : field_names) {
+          auto fieldVar = io.InquireVariable<real_t>(name);
+          if (fieldVar) {
+            raise::ErrorIf(fieldVar.Shape().size() != 3,
+                           fmt::format("%s is not 3D", name.c_str()),
+                           HERE);
+
+            auto        dims  = fieldVar.Shape();
+            std::size_t nx1_r = dims[0];
+            std::size_t nx2_r = dims[1];
+            std::size_t nx3_r = dims[2];
+            raise::ErrorIf(
+              (nx1_r != 10) || (nx2_r != 10) || (nx3_r != 10),
+              fmt::format("%s is not %dx%dx%d", name.c_str(), nx1_r, nx2_r, nx3_r),
+              HERE);
+            fieldVar.SetSelection(
+              adios2::Box<adios2::Dims>({ 0, 0, 0 }, { nx1_r, nx2_r, nx3_r }));
+            reader.Get(fieldVar, field_read_h.data(), adios2::Mode::Sync);
+            Kokkos::deep_copy(field_read, field_read_h);
+
+            Kokkos::parallel_for(
+              "check",
+              CreateRangePolicy<Dim::_3D>({ 0, 0, 0 }, { nx1_r, nx2_r, nx3_r }),
+              Lambda(index_t i1, index_t i2, index_t i3) {
+                if (not cmp::AlmostEqual(
+                      field_read(i1, i2, i3),
+                      field(i1 + i1min, i2 + i2min, i3 + i3min, cntr))) {
+                  printf("%e %e\n",
+                         field_read(i1, i2, i3),
+                         field(i1 + i1min, i2 + i2min, i3 + i3min, cntr));
+                  raise::KernelError(HERE, "Field is not read correctly");
+                }
+              });
+            ++cntr;
+          } else {
+            raise::Error("Field not found", HERE);
+          }
         }
         reader.EndStep();
       }
