50e59305c5161096167e7ab77cc60e8c262d311b
jmahlmann <mahlmann@princeton.edu>
2024-10-18 18:27:21 -0400

    Magnetar update.

--- setups/srpic/magnetar/pgen.hpp
+++ setups/srpic/magnetar/pgen.hpp
@@ -867,8 +867,7 @@ namespace user {
                   tag_perp(ph_p + ph_offset_perp) = ParticleTag::alive;
                 }
 
-                sandcheck = true;
-
+                sendcheck = true;
 
                 auto cbuff2_acc     = cbuff2_sc.access();
                 cbuff2_acc(static_cast<int>(i1(p)), static_cast<int>(i2(p))) += weight(p) * inv_n0_ /
