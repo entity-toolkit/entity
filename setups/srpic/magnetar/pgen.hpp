974c9c74a4e339a607abc3cfdc5864ababd4d32c
jmahlmann <mahlmann@princeton.edu>
2024-10-18 18:03:59 -0400

    Magnetar update.

--- setups/srpic/magnetar/pgen.hpp
+++ setups/srpic/magnetar/pgen.hpp
@@ -839,7 +839,7 @@ namespace user {
                            kph_z);
 
               // Inject the scattered photon
-              if ((eph > 2.0) && (!photon_sent)) {
+              if ((eph > 2.0) && (!photon_sent_)) {
                 if (pol_par) {
                   auto ph_p = Kokkos::atomic_fetch_add(&ph_ind_par(), 1);
                   i1_par(ph_p + ph_offset_par) = i1(p);
