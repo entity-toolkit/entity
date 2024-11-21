2c49cc70ef5d5811d0b12ac3fd680195be58e88e
jmahlmann <mahlmann@princeton.edu>
2024-11-20 22:05:29 -0500

    Magnetar update.

--- setups/srpic/magnetar_ring/pgen.hpp
+++ setups/srpic/magnetar_ring/pgen.hpp
@@ -839,13 +839,26 @@ namespace user {
             auto tpeak = fid_freq_ / 2.821;
 
             if (true) {
-              auto pnorm { 1.0 / NORM(px, py, pz) };
+              
+              auto pnorm { NORM(px, py, pz) };
               auto f_drag = 1.41631 * math::pow(10.0, 18) * SQR(Rstar_/xPh[0]) * (rand_costheta_RF - math::sqrt(beta_sq)) * CUBE(eph_LF) / (math::exp(eph_LF/tpeak) - 1.0);
               auto dp = dt_ * f_drag;
 
-              ux1(p) += dp * ux1(p);
-              ux2(p) += dp * ux2(p);
-              ux3(p) += dp * ux3(p);
+              auto pplus = pnorm * ( ONE + dp );
+              auto beta_plus = pplus / math::sqrt(1.0 + SQR(pplus));
+
+              if ((rand_costheta_RF - math::sqrt(beta_sq)) * (rand_costheta_RF - beta_plus) < 0.0) {
+                auto beta_star = rand_costheta_RF;
+                auto gamma_star = 1.0 / math::sqrt(1.0 - SQR(beta_star));
+                auto p_star = gamma_star * beta_star;
+                ux1(p) = px * p_star / pnorm;
+                ux2(p) = py * p_star / pnorm;
+                ux3(p) = pz * p_star / pnorm;
+              } else {
+                ux1(p) += dp * ux1(p);
+                ux2(p) += dp * ux2(p);
+                ux3(p) += dp * ux3(p);
+              }
               
               return;
             }
