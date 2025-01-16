abdc37e005f28d42bb2bef77efdfd4e313a77512
jmahlmann <mahlmann@princeton.edu>
2025-01-16 11:30:52 -0500

    PP update.

--- setups/srpic/magnetar_ring/pgen.hpp
+++ setups/srpic/magnetar_ring/pgen.hpp
@@ -915,31 +915,31 @@ namespace user {
 
             auto tpeak = fid_freq_ / 2.821;
 
-            if (gamma < 2.0) {
+            // if (gamma < 2.0) {
             // if (true) {
               
-              auto pnorm { NORM(px, py, pz) };
-              auto f_drag = 1.41631 * math::pow(10.0, 18) * SQR(Rstar_/xPh[0]) * (rand_costheta_RF - math::sqrt(beta_sq)) * CUBE(eph_LF) / (math::exp(eph_LF/tpeak) - 1.0);
-              auto dp = dt_ * f_drag;
+            //   auto pnorm { NORM(px, py, pz) };
+            //   auto f_drag = 1.41631 * math::pow(10.0, 18) * SQR(Rstar_/xPh[0]) * (rand_costheta_RF - math::sqrt(beta_sq)) * CUBE(eph_LF) / (math::exp(eph_LF/tpeak) - 1.0);
+            //   auto dp = dt_ * f_drag;
 
-              auto pplus = pnorm * ( ONE + dp );
-              auto beta_plus = pplus / math::sqrt(1.0 + SQR(pplus));
+            //   auto pplus = pnorm * ( ONE + dp );
+            //   auto beta_plus = pplus / math::sqrt(1.0 + SQR(pplus));
 
-              if ((rand_costheta_RF - math::sqrt(beta_sq)) * (rand_costheta_RF - beta_plus) < 0.0) {
-                auto beta_star = rand_costheta_RF;
-                auto gamma_star = 1.0 / math::sqrt(1.0 - SQR(beta_star));
-                auto p_star = gamma_star * beta_star;
-                ux1(p) = px * p_star / pnorm;
-                ux2(p) = py * p_star / pnorm;
-                ux3(p) = pz * p_star / pnorm;
-              } else {
-                ux1(p) += dp * px;
-                ux2(p) += dp * py;
-                ux3(p) += dp * pz;
-              }
+            //   if ((rand_costheta_RF - math::sqrt(beta_sq)) * (rand_costheta_RF - beta_plus) < 0.0) {
+            //     auto beta_star = rand_costheta_RF;
+            //     auto gamma_star = 1.0 / math::sqrt(1.0 - SQR(beta_star));
+            //     auto p_star = gamma_star * beta_star;
+            //     ux1(p) = px * p_star / pnorm;
+            //     ux2(p) = py * p_star / pnorm;
+            //     ux3(p) = pz * p_star / pnorm;
+            //   } else {
+            //     ux1(p) += dp * px;
+            //     ux2(p) += dp * py;
+            //     ux3(p) += dp * pz;
+            //   }
               
-              return;
-            }
+            //   return;
+            // }
 
             auto ndot = 1.41631 * math::pow(10.0, 18) * SQR(Rstar_/xPh[0]) * 1.0 / gamma
                           * SQR(eph_LF) / (math::exp(eph_LF/tpeak) - 1.0);
