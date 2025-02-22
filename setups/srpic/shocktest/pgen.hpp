d6b2b1c266a127763a60ee6706f59f64214f7795
jmahlmann <mahlmann@princeton.edu>
2025-02-22 11:16:58 -0500

    Ongoing.

---
+++ setups/srpic/shocktest/pgen.hpp
@@ -0,0 +1,179 @@
+#ifndef PROBLEM_GENERATOR_H
+#define PROBLEM_GENERATOR_H
+
+#include "enums.h"
+#include "global.h"
+
+#include "arch/traits.h"
+#include "utils/error.h"
+#include "utils/numeric.h"
+
+#include "archetypes/energy_dist.h"
+#include "archetypes/particle_injector.h"
+#include "archetypes/problem_generator.h"
+#include "framework/domain/metadomain.h"
+
+#include <utility>
+
+namespace user {
+  using namespace ntt;
+
+  template <Dimension D>
+  struct InitFields {
+    /*
+      Sets up magnetic and electric field components for the simulation.
+      Must satisfy E = -v x B for Lorentz Force to be zero.
+
+      @param bmag: magnetic field scaling
+      @param btheta: magnetic field polar angle
+      @param bphi: magnetic field azimuthal angle
+      @param drift_ux: drift velocity in the x direction
+    */
+    InitFields(real_t bmag, real_t btheta, real_t bphi, real_t drift_ux)
+      : Bmag { bmag }
+      , Btheta { btheta * static_cast<real_t>(convert::deg2rad) }
+      , Bphi { bphi * static_cast<real_t>(convert::deg2rad) }
+      , Vx { drift_ux } {}
+
+    // magnetic field components
+    Inline auto bx1(const coord_t<D>& x_Ph) const -> real_t {
+      return x_Ph[0];
+    }
+
+    Inline auto bx2(const coord_t<D>&) const -> real_t {
+      return x_Ph[0];
+    }
+
+    Inline auto bx3(const coord_t<D>&) const -> real_t {
+      return x_Ph[0];
+    }
+
+    // electric field components
+    Inline auto ex1(const coord_t<D>&) const -> real_t {
+      return x_Ph[0];
+    }
+
+    Inline auto ex2(const coord_t<D>&) const -> real_t {
+      return x_Ph[0];
+    }
+
+    Inline auto ex3(const coord_t<D>&) const -> real_t {
+      return x_Ph[0];
+    }
+
+  private:
+    const real_t Btheta, Bphi, Vx, Bmag;
+  };
+
+  template <SimEngine::type S, class M>
+  struct PGen : public arch::ProblemGenerator<S, M> {
+    // compatibility traits for the problem generator
+    static constexpr auto engines { traits::compatible_with<SimEngine::SRPIC>::value };
+    static constexpr auto metrics { traits::compatible_with<Metric::Minkowski>::value };
+    static constexpr auto dimensions {
+      traits::compatible_with<Dim::_1D, Dim::_2D, Dim::_3D>::value
+    };
+
+    // for easy access to variables in the child class
+    using arch::ProblemGenerator<S, M>::D;
+    using arch::ProblemGenerator<S, M>::C;
+    using arch::ProblemGenerator<S, M>::params;
+
+    const real_t drift_ux, temperature;
+
+    const real_t  Btheta, Bphi, Bmag;
+    InitFields<D> init_flds;
+
+    inline PGen(const SimulationParams& p, const Metadomain<S, M>& m)
+      : arch::ProblemGenerator<S, M> { p }
+      , drift_ux { p.template get<real_t>("setup.drift_ux") }
+      , temperature { p.template get<real_t>("setup.temperature") }
+      , Bmag { p.template get<real_t>("setup.Bmag", ZERO) }
+      , Btheta { p.template get<real_t>("setup.Btheta", ZERO) }
+      , Bphi { p.template get<real_t>("setup.Bphi", ZERO) }
+      , init_flds { Bmag, Btheta, Bphi, drift_ux } {}
+
+    inline PGen() {}
+
+    auto FixFieldsConst(const bc_in&, const em& comp) const
+      -> std::pair<real_t, bool> {
+      if (comp == em::ex2) {
+        return { init_flds.ex2({ ZERO }), true };
+      } else if (comp == em::ex3) {
+        return { init_flds.ex3({ ZERO }), true };
+      } else {
+        return { ZERO, false };
+      }
+    }
+
+    
+    auto PerfectConductorFieldsConst(const bc_in&, const em& comp) const -> real_t{
+
+      // electric field components
+      if (comp == em::ex1) {
+          return ONE;
+      } else if (comp == em::ex2) {
+        return -ONE;
+      } else if (comp == em::ex3) {
+          return -ONE; } 
+      // magentic field components
+      else if (comp == em::bx1) {
+        return -ONE;
+      } else if (comp == em::bx2) {
+        return ONE;
+      } else if (comp == em::bx3) {
+        return ONE;}
+      // should never be the case
+      else
+      {
+          return ZERO;
+      }
+    }
+
+    // auto PerfectConductorCurrentsConst(const bc_in &, const cur &comp) const
+    //     -> std::pair<real_t, bool>
+    // {
+    //     // ToDo
+    //     if (comp == cur::jx1)
+    //     {
+    //         return ZERO;
+    //     }
+    //     else if (comp == cur::jx2)
+    //     {
+    //         return ZERO;
+    //     }
+    //     else if (comp == cur::jx3)
+    //     {
+    //         return ZERO;
+    //     }
+    //     else
+    //     {
+    //         return ZERO;
+    //     }
+    // }
+
+    auto MatchFields(real_t time) const -> InitFields<D> {
+      return init_flds;
+    }
+
+    inline void InitPrtls(Domain<S, M>& local_domain) {
+      const auto energy_dist = arch::Maxwellian<S, M>(local_domain.mesh.metric,
+                                                      local_domain.random_pool,
+                                                      temperature,
+                                                      -drift_ux,
+                                                      in::x1);
+
+      const auto injector = arch::UniformInjector<S, M, arch::Maxwellian>(
+        energy_dist,
+        { 1, 2 });
+      // arch::InjectUniform<S, M, arch::UniformInjector<S, M, arch::Maxwellian>>(
+      //   params,
+      //   local_domain,
+      //   injector,
+      //   1.0);
+    }
+  };
+
+} // namespace user
+
+#endif
