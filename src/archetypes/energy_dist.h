1447a60f324efe4c6d8cadd509fe3b4882885b76
Alisa Galishnikova <55898700+alisagk@users.noreply.github.com>
2025-04-11 13:00:08 -0400

    hack for using coordinate-dependent boost for maxw

--- src/archetypes/energy_dist.h
+++ src/archetypes/energy_dist.h
@@ -263,6 +263,130 @@ namespace arch {
     const bool   zero_current;
   };
 
+template <SimEngine::type S, class M>
+  struct Maxwellian_coord : public EnergyDistribution<S, M> {
+    using EnergyDistribution<S, M>::metric;
+
+    Maxwellian_coord(const M&              metric,
+               random_number_pool_t& pool,
+               real_t                temperature,
+               in                    boost_direction = in::x1,
+               bool                  zero_current    = true)
+      : EnergyDistribution<S, M> { metric }
+      , pool { pool }
+      , temperature { temperature }
+      , boost_direction { boost_direction }
+      , zero_current { zero_current } {
+      raise::ErrorIf(temperature < ZERO,
+                     "Maxwellian_coord: Temperature must be non-negative",
+                     HERE);
+    }
+
+    // Juttner-Synge distribution
+    Inline void JS(vec_t<Dim::_3D>& v, const real_t& temp) const {
+      auto   rand_gen = pool.get_state();
+      real_t randX1, randX2;
+      if (temp < static_cast<real_t>(0.5)) {
+        // Juttner-Synge distribution using the Box-Muller method - non-relativistic
+        randX1 = Random<real_t>(rand_gen);
+        while (cmp::AlmostZero(randX1)) {
+          randX1 = Random<real_t>(rand_gen);
+        }
+        randX1 = math::sqrt(-TWO * math::log(randX1));
+        randX2 = constant::TWO_PI * Random<real_t>(rand_gen);
+        v[0]   = randX1 * math::cos(randX2) * math::sqrt(temp);
+
+        randX1 = Random<real_t>(rand_gen);
+        while (cmp::AlmostZero(randX1)) {
+          randX1 = Random<real_t>(rand_gen);
+        }
+        randX1 = math::sqrt(-TWO * math::log(randX1));
+        randX2 = constant::TWO_PI * Random<real_t>(rand_gen);
+        v[1]   = randX1 * math::cos(randX2) * math::sqrt(temp);
+
+        randX1 = Random<real_t>(rand_gen);
+        while (cmp::AlmostZero(randX1)) {
+          randX1 = Random<real_t>(rand_gen);
+        }
+        randX1 = math::sqrt(-TWO * math::log(randX1));
+        randX2 = constant::TWO_PI * Random<real_t>(rand_gen);
+        v[2]   = randX1 * math::cos(randX2) * math::sqrt(temp);
+      } else {
+        // Juttner-Synge distribution using the Sobol method - relativistic
+        auto randu   = ONE;
+        auto randeta = Random<real_t>(rand_gen);
+        while (SQR(randeta) <= SQR(randu) + ONE) {
+          randX1 = Random<real_t>(rand_gen) * Random<real_t>(rand_gen) *
+                   Random<real_t>(rand_gen);
+          while (cmp::AlmostZero(randX1)) {
+            randX1 = Random<real_t>(rand_gen) * Random<real_t>(rand_gen) *
+                     Random<real_t>(rand_gen);
+          }
+          randu  = -temp * math::log(randX1);
+          randX2 = Random<real_t>(rand_gen);
+          while (cmp::AlmostZero(randX2)) {
+            randX2 = Random<real_t>(rand_gen);
+          }
+          randeta = -temp * math::log(randX1 * randX2);
+        }
+        randX1 = Random<real_t>(rand_gen);
+        randX2 = Random<real_t>(rand_gen);
+        v[0]   = randu * (TWO * randX1 - ONE);
+        v[2]   = TWO * randu * math::sqrt(randX1 * (ONE - randX1));
+        v[1]   = v[2] * math::cos(constant::TWO_PI * randX2);
+        v[2]   = v[2] * math::sin(constant::TWO_PI * randX2);
+      }
+      pool.free_state(rand_gen);
+    }
+
+    // Boost a symmetric distribution to a relativistic speed using flipping
+    // method https://arxiv.org/pdf/1504.03910.pdf
+    Inline void boost(vec_t<Dim::_3D>& v, real_t boost_velocity) const {
+      const auto boost_dir = static_cast<unsigned short>(boost_direction);
+      const auto boost_beta { boost_velocity /
+                              math::sqrt(ONE + SQR(boost_velocity)) };
+      const auto gamma { U2GAMMA(v[0], v[1], v[2]) };
+      auto       rand_gen = pool.get_state();
+      if (-boost_beta * v[boost_dir] > gamma * Random<real_t>(rand_gen)) {
+        v[boost_dir] = -v[boost_dir];
+      }
+      pool.free_state(rand_gen);
+      v[boost_dir] = math::sqrt(ONE + SQR(boost_velocity)) *
+                     (v[boost_dir] + boost_beta * gamma);
+    }
+
+    Inline void operator()(const coord_t<M::Dim>&,
+                           vec_t<Dim::_3D>& v,
+                           unsigned short   s = 0) const override {
+      const real_t boost_velocity = v[1]; // set coordinate-dependent boost via vy
+      if (cmp::AlmostZero(temperature)) {
+        v[0] = ZERO;
+        v[1] = ZERO;
+        v[2] = ZERO;
+      } else {
+        JS(v, temperature);
+      }
+      if constexpr (M::CoordType == Coord::Cart) {
+        // boost only when using cartesian coordinates
+        if (not cmp::AlmostZero(boost_velocity)) {
+          boost(v, boost_velocity);
+          if (not zero_current and s % 2 == 0) {
+            v[0] = -v[0];
+            v[1] = -v[1];
+            v[2] = -v[2];
+          }
+        }
+      }
+    }
+
+  private:
+    random_number_pool_t pool;
+
+    const real_t temperature;
+    const in     boost_direction;
+    const bool   zero_current;
+  };
+
 } // namespace arch
 
 #endif // ARCHETYPES_ENERGY_DIST_HPP
