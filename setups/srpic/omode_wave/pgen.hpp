2129cdc4177760ade9cd95b5a9ca4c58ba9ad4b2
jmahlmann <jens.mahlmann@gmail.com>
2025-04-22 08:21:18 -0400

    Merging turbulence infrastructure

--- setups/srpic/omode_wave/pgen.hpp
+++ setups/srpic/omode_wave/pgen.hpp
@@ -20,6 +20,45 @@ namespace user {
   template <SimEngine::type S, class M>
   struct PGen : public arch::ProblemGenerator<S, M> {
 
+    // Simplified external current driver: single x1-directional mode with spatial/temporal variation
+    template <Dimension D>
+    struct ExternalCurrent {
+        ExternalCurrent(real_t amplitude, int num_waves_x, int num_waves_y, int num_waves_z,
+                        real_t frequency, real_t Lx, real_t Ly, real_t Lz)
+            : A(amplitude), omega(frequency), Lx(Lx), Ly(Ly), Lz(Lz) {
+
+            // Calculate wavevector components based on number of desired waves
+            if constexpr (D == Dim::_2D) {
+                kx = constant::TWO_PI * num_waves_x / Lx;
+                ky = constant::TWO_PI * num_waves_y / Ly;
+            }
+            if constexpr (D == Dim::_3D) {
+                kx = constant::TWO_PI * num_waves_x / Lx;
+                ky = constant::TWO_PI * num_waves_y / Ly;
+                kz = constant::TWO_PI * num_waves_z / Lz;
+            }
+        }
+
+        Inline auto jx1(const coord_t<D>& x_Ph, real_t time) const -> real_t {
+            if constexpr (D == Dim::_2D) {
+                auto phase = kx * x_Ph[0] + ky * x_Ph[1] - omega * time;
+                return A * math::cos(phase);
+            }
+            if constexpr (D == Dim::_3D) {
+                auto phase = kx * x_Ph[0] + ky * x_Ph[1] + kz * x_Ph[2] - omega * time;
+                return A * math::cos(phase);
+            }
+        }
+
+        Inline auto jx2(const coord_t<D>&) const -> real_t { return ZERO; }
+        Inline auto jx3(const coord_t<D>&) const -> real_t { return ZERO; }
+
+    private:
+        real_t A, omega, kx, ky, kz;
+        real_t Lx, Ly, Lz;
+    };
+
+
     // compatibility traits for the problem generator
     static constexpr auto engines = traits::compatible_with<SimEngine::SRPIC>::value;
     static constexpr auto metrics = traits::compatible_with<Metric::Minkowski>::value;
@@ -32,8 +71,10 @@ namespace user {
     using arch::ProblemGenerator<S, M>::params;
 
     const real_t sx1, sx2, sx3;
-    const real_t temp;
+    const real_t temp, amplitude;
+    const real_t nwave_x, nwave_y, nwave_z, frequency;
 
+    ExternalCurrent<D> ExternalCurrent;
 
     inline PGen(const SimulationParams& p, const Metadomain<S, M>& global_domain)
       : arch::ProblemGenerator<S, M> { p }
@@ -44,6 +85,13 @@ namespace user {
       , sx3 { global_domain.mesh().extent(in::x3).second -
               global_domain.mesh().extent(in::x3).first }
       , temp { p.template get<real_t>("setup.temp") }
+      , amplitude { p.template get<real_t>("setup.amplitude") }
+      , nwave_x { p.template get<real_t>("setup.nwave_x") }
+      , nwave_y { p.template get<real_t>("setup.nwave_y") }
+      , nwave_z { p.template get<real_t>("setup.nwave_z") }
+      , frequency { p.template get<real_t>("setup.frequency") }
+      , ExternalCurrent {  amplitude, nwave_x, nwave_y, nwave_z, frequency, sx1, sx2, sx3 }
+
       {}
 
     inline void InitPrtls(Domain<S, M>& local_domain) {
