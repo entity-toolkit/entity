fc01e8d4602357252a5c9ca64c38a418d7b84e24
haykh <haykh.astro@gmail.com>
2026-03-19 14:41:30 -0400

    new extfields + example usage

--- pgens/examples/README.md
+++ pgens/examples/README.md
@@ -5,3 +5,5 @@ Problem generations in this directory are just examples demonstrating how to use
 - `custom_emission`: simple example where two particles are initialized on gyrating trajectories probabilistically emitting photons while the total energy is conserved.
 
   https://github.com/user-attachments/assets/6c5c399a-bbab-4b93-be5f-35fed1959fcc
+
+- `external_fields`: example of using external fields for supplying a time-varying and species-dependent magnetic/electric fields and/or force-field imposed on particles.
--- src/kernels/particle_pusher_sr.hpp
+++ src/kernels/particle_pusher_sr.hpp
@@ -126,7 +126,6 @@ namespace kernel::sr {
 
     const randacc_ndfield_t<D, 6> EB;
 
-    const spidx_t      sp;
     array_t<int*>      i1, i2, i3;
     array_t<int*>      i1_prev, i2_prev, i3_prev;
     array_t<prtldx_t*> dx1, dx2, dx3;
@@ -140,8 +139,7 @@ namespace kernel::sr {
 
     const E emission_policy;
 
-    const simtime_t time;
-    const real_t    coeff, dt;
+    const real_t coeff, dt;
 
     const int ni1, ni2, ni3;
     bool      is_absorb_i1min { false }, is_absorb_i1max { false };
@@ -173,7 +171,6 @@ namespace kernel::sr {
       : pusher_flags { pusher_params.pusher_flags }
       , radiative_drag_flags { pusher_params.radiative_drag_flags }
       , EB { EB }
-      , sp { pusher_arrays.sp }
       , i1 { pusher_arrays.i1 }
       , i2 { pusher_arrays.i2 }
       , i3 { pusher_arrays.i3 }
@@ -195,7 +192,6 @@ namespace kernel::sr {
       , metric { metric }
       , external_fields { external_fields }
       , emission_policy { emission_policy }
-      , time { pusher_params.time }
       , coeff { HALF * (pusher_params.charge / pusher_params.mass) *
                 pusher_params.omegaB0 * pusher_params.dt }
       , dt { pusher_params.dt }
@@ -372,13 +368,13 @@ namespace kernel::sr {
           vec_t<Dim::_3D> ext_e_Ph { ZERO };
           vec_t<Dim::_3D> ext_e_Cart { ZERO };
           if constexpr (::traits::external::HasEx1<F, D>) {
-            ext_e_Ph[0] = external_fields.ex1(sp, time, xp_Ph);
+            ext_e_Ph[0] = external_fields.ex1(xp_Ph);
           }
           if constexpr (::traits::external::HasEx2<F, D>) {
-            ext_e_Ph[1] = external_fields.ex2(sp, time, xp_Ph);
+            ext_e_Ph[1] = external_fields.ex2(xp_Ph);
           }
           if constexpr (::traits::external::HasEx3<F, D>) {
-            ext_e_Ph[2] = external_fields.ex3(sp, time, xp_Ph);
+            ext_e_Ph[2] = external_fields.ex3(xp_Ph);
           }
           metric.template transform_xyz<Idx::T, Idx::XYZ>(xp_Cd, ext_e_Ph, ext_e_Cart);
           ei_Cart[0] += ext_e_Cart[0];
@@ -389,14 +385,14 @@ namespace kernel::sr {
         if constexpr (HasExtBfield) {
           vec_t<Dim::_3D> ext_b_Ph { ZERO };
           vec_t<Dim::_3D> ext_b_Cart { ZERO };
-          if constexpr (::traits::external::HasEx1<F, D>) {
-            ext_b_Ph[0] = external_fields.bx1(sp, time, xp_Ph);
+          if constexpr (::traits::external::HasBx1<F, D>) {
+            ext_b_Ph[0] = external_fields.bx1(xp_Ph);
           }
-          if constexpr (::traits::external::HasEx2<F, D>) {
-            ext_b_Ph[1] = external_fields.bx2(sp, time, xp_Ph);
+          if constexpr (::traits::external::HasBx2<F, D>) {
+            ext_b_Ph[1] = external_fields.bx2(xp_Ph);
           }
-          if constexpr (::traits::external::HasEx3<F, D>) {
-            ext_b_Ph[2] = external_fields.bx3(sp, time, xp_Ph);
+          if constexpr (::traits::external::HasBx3<F, D>) {
+            ext_b_Ph[2] = external_fields.bx3(xp_Ph);
           }
           metric.template transform_xyz<Idx::T, Idx::XYZ>(xp_Cd, ext_b_Ph, ext_b_Cart);
           bi_Cart[0] += ext_b_Cart[0];
@@ -421,13 +417,13 @@ namespace kernel::sr {
         if constexpr (HasExtForce or Atm) {
           real_t f_x1 = ZERO, f_x2 = ZERO, f_x3 = ZERO;
           if constexpr (::traits::external::HasFx1<F, D>) {
-            f_x1 = external_fields.fx1(sp, time, xp_Ph);
+            f_x1 = external_fields.fx1(xp_Ph);
           }
           if constexpr (::traits::external::HasFx2<F, D>) {
-            f_x2 = external_fields.fx2(sp, time, xp_Ph);
+            f_x2 = external_fields.fx2(xp_Ph);
           }
           if constexpr (::traits::external::HasFx3<F, D>) {
-            f_x3 = external_fields.fx3(sp, time, xp_Ph);
+            f_x3 = external_fields.fx3(xp_Ph);
           }
           if constexpr (Atm) {
             if constexpr (D == Dim::_1D or D == Dim::_2D or D == Dim::_3D) {
