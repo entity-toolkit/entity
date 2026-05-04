9f3c8237c01fdee067758caf60f2098129c4e015
haykh <haykh.astro@gmail.com>
2026-05-04 09:11:03 -0400

    2bodyint

---
+++ src/archetypes/twobody_interactions.h
@@ -0,0 +1,20 @@
+#ifndef ARCHETYPES_TWO_BODY_INTERACTIONS_H
+#define ARCHETYPES_TWO_BODY_INTERACTIONS_H
+
+#include "traits/metric.h"
+
+#include "framework/containers/particles.h"
+
+#include <vector>
+
+namespace arch {
+
+  template <MetricClass M>
+  class TwoBodyComptonScattering {
+    // std::vector<spidx_t> lepton_species_indices;
+    // std::vector<spidx_t> photon_species_indices;
+  };
+
+} // namespace arch
+
+#endif
