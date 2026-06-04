50ce4488fdc8f0fc3ba23b64a7d40141fc2d22a3
hayk <haykh.astro@gmail.com>
2026-06-04 11:48:32 -0400

    updated EMF call

--- src/engines/hybrid/fieldsolvers.h
+++ src/engines/hybrid/fieldsolvers.h
@@ -10,6 +10,7 @@
 #include "metrics/minkowski.h"
 
 #include "framework/domain/domain.h"
+#include "framework/parameters/parameters.h"
 #include "kernels/hybrid/EMF.hpp"
 #include "kernels/hybrid/faraday.hpp"
 
@@ -20,6 +21,11 @@ namespace ntt {
       push1,
       push2,
       push3,
+    };
+
+    enum class emf : uint8_t {
+      push0,
+      push12,
     };
 
     template <Dimension D>
@@ -67,38 +73,53 @@ namespace ntt {
 
     template <Dimension D>
     void EMF(Domain<SimEngine::HYBRID, metric::Minkowski<D>>& domain,
-             const prm::Parameters&                           engine_params) {
+             const prm::Parameters&                           engine_params,
+             const SimulationParams&                          params,
+             const emf&                                       flag) {
       const auto dt       = engine_params.get<real_t>("dt");
-      const auto gamma_ad = engine_params.get<real_t>("gamma_ad");
-      const auto theta    = engine_params.get<real_t>("theta");
-      const auto d0       = engine_params.get<real_t>("skindepth0");
-      const auto rho0     = engine_params.get<real_t>("larmor0");
-      Kokkos::parallel_for(
-        "EMFPush",
-        domain.mesh.rangeActiveCells(),
-        kernel::hybrid::EMF_kernel<D, 3>(domain.fields.aux,  // P
-                                         domain.fields.aux,  // N
-                                         domain.fields.em,   // Ee_in
-                                         domain.fields.em,   // Bf
-                                         domain.fields.em0,  // Ec
-                                         domain.fields.cur,  // Bfs
-                                         domain.fields.em0,  // Ee_out
-                                         domain.fields.bckp, // Ec_out
-                                         domain.fields.bckp, // Bc_out
-                                         0,                  // P
-                                         3,                  // N
-                                         0,                  // Ee_in
-                                         3,                  // Bf
-                                         0,                  // Ec
-                                         0,                  // Bfs
-                                         3,                  // Ee_out
-                                         0,                  // Ec_out
-                                         3,                  // Bc_out
-                                         dt,
-                                         gamma_ad,
-                                         theta,
-                                         d0,
-                                         rho0));
+      const auto gamma_ad = params.get<real_t>("scales.gamma_ad");
+      const auto theta    = params.get<real_t>("scales.theta0");
+      const auto d0       = params.get<real_t>("scales.skindepth0");
+      const auto rho0     = params.get<real_t>("scales.larmor0");
+      if (flag == emf::push0) {
+        // clang-format off
+        Kokkos::parallel_for(
+          "EMFPush",
+          domain.mesh.rangeActiveCells(),
+          kernel::hybrid::EMF_kernel<D, true>(domain.fields.aux,  // P
+                                              domain.fields.aux,  // N
+                                              domain.fields.em0,  // Ee_in
+                                              domain.fields.cur,  // Bf
+                                              domain.fields.bckp, // Ec
+                                              domain.fields.em,   // Bfs
+                                              domain.fields.em,   // Ee_out
+                                              domain.fields.em0,  // Ec_out
+                                              domain.fields.bckp, // Bc_out
+                                              0, 3, 3,            // P, N, Ee_in
+                                              0, 0, 3,            // Bf, Ec, Bfs
+                                              0, 0, 3,            // Ee_out, Ec_out, Bc_out
+                                              dt, gamma_ad, theta, d0, rho0));
+        // clang-format on
+      } else {
+        // clang-format off
+        Kokkos::parallel_for(
+          "EMFPush",
+          domain.mesh.rangeActiveCells(),
+          kernel::hybrid::EMF_kernel<D, false>(domain.fields.aux,  // P
+                                               domain.fields.aux,  // N
+                                               domain.fields.em,   // Ee_in
+                                               domain.fields.em,   // Bf
+                                               domain.fields.em0,  // Ec
+                                               domain.fields.cur,  // Bfs
+                                               domain.fields.em0,  // Ee_out
+                                               domain.fields.bckp, // Ec_out
+                                               domain.fields.bckp, // Bc_out
+                                               0, 3, 0,            // P, N, Ee_in
+                                               3, 0, 0,            // Bf, Ec, Bfs
+                                               3, 0, 3,            // Ee_out, Ec_out, Bc_out
+                                               dt, gamma_ad, theta, d0, rho0));
+        // clang-format on
+      }
     }
 
   } // namespace hybrid
